261c1ffcffa6406db4918d33e7df36b83fef03df
Charlie Varley <cvarley100@gmail.com>
2017-09-01 02:29:00 +0100

    Initial commit

---
+++ containers/receive.js
@@ -0,0 +1,145 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ListView,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import QRCode from 'react-native-qrcode';
+import { connect } from 'react-redux';
+import { generateNewAddress } from '../actions/iotaActions';
+import { getFromKeychain } from '../libs/cryptography'
+import TransactionRow from '../components/transactionRow';
+
+const { height, width } = Dimensions.get('window');
+
+class Receive extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([
+      ]),
+    };
+  }
+
+  onGeneratePress(props) {
+    getFromKeychain(this.props.iota.password, function(value){
+      if(typeof value !== 'undefined'){
+         generate(value);
+      } else {
+         error();
+      }
+   });
+   function generate(value){
+      props.generateNewAddress(value);
+   };
+   function error(){
+     this.dropdown.alertWithType('error', 'Something went wrong', 'Please restart the app.');
+   };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ paddingBottom: height / 40 }}>
+          <View style={styles.receiveAddressContainer}>
+            <Text style={styles.receiveAddressText}>{this.props.iota.addresses[this.props.iota.addresses.length - 1]}</Text>
+          </View>
+        </View>
+        <View style={{ paddingBottom: height / 40 }}>
+          <QRCode
+            value={this.props.iota.addresses[this.props.iota.addresses.length - 1]}
+            size={width / 2.5}
+            bgColor="#000"
+            fgColor="#FFF"
+          />
+        </View>
+        <TouchableWithoutFeedback onPress={event => this.onGeneratePress(this.props)}>
+          <View style={styles.generateButton}>
+            <Image style={styles.generateImage} source={require('../images/plus.png')} />
+            <Text style={styles.generateText}>GENERATE NEW ADDRESS</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <View style={{ paddingTop: height / 20 }}>
+          {/* <ListView
+                dataSource={this.state.dataSource}
+                renderRow={(data) => <TransactionRow rowData={data} />}
+                renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+              /> */}
+        </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingTop: height / 20 
+  },
+  receiveAddressContainer: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    width: width / 1.3,
+    height: height / 10,
+    justifyContent: 'center',
+  },
+  receiveAddressText: {
+    fontFamily: 'Lato-Regular',
+    fontSize: width / 33.7,
+    color: 'white',
+    backgroundColor: 'transparent',
+    paddingHorizontal: width / 14,
+    textAlign: 'center',
+  },
+  generateButton: {
+    flexDirection: 'row',
+    borderColor: 'rgba(255,255,255,0.6)',
+    borderWidth: 1.5,
+    borderRadius: 8,
+    width: width / 2,
+    height: height / 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#009f3f',
+  },
+  generateText: {
+    color: 'white',
+    fontFamily: 'Lato-Bold',
+    fontSize: width / 40.5,
+    backgroundColor: 'transparent',
+    paddingLeft: 6,
+  },
+  generateImage: {
+    height: width / 30,
+    width: width / 30,
+  },
+  separator: {
+    flex: 1,
+    height: 15,
+  },
+
+
+});
+
+const mapStateToProps = state => ({
+  marketData: state.marketData,
+  iota: state.iota,
+  account: state.account,
+});
+
+const mapDispatchToProps = dispatch => ({
+  generateNewAddress: (seed) => {
+    dispatch(generateNewAddress(seed));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Receive);
---
+++ reducers/accountReducer.js
@@ -0,0 +1,16 @@
+const accountReducer = (state = {
+  firstUse: true,
+}, action) => {
+  switch (action.type) {
+
+    case 'SET_FIRSTUSE':
+      state = {
+        ...state,
+        firstUse: action.payload,
+      };
+      break;
+  }
+  return state;
+};
+
+export default accountReducer;
