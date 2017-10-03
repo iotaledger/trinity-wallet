4b7386160af73687b224b07f1b11d8c0f05719a9
Manuel <github@manuelbieh.de>
2017-10-03 23:08:28 +0200

    more setup stuff

--- src/desktop/src/index.js
+++ src/desktop/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from 'components/App';
+import { Provider as Redux } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18next from './libs/i18next';
+import store from './store';
+import App from './components/App';
 
 render(
-    <App name='World'/>,
+    <Redux store={store}>
+        <I18nextProvider i18n={i18next}>
+            <App name='IOTA'/>
+        </I18nextProvider>
+    </Redux>,
     document.getElementById('root')
 );
