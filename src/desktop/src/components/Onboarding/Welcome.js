a8e300117c9872f9c77d8e056159fefbdced9eab
Manuel <github@manuelbieh.de>
2017-10-07 18:40:51 +0200

    fixed broken paths in electron production build

--- src/desktop/src/components/Onboarding/Welcome.js
+++ src/desktop/src/components/Onboarding/Welcome.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import Header from './Header';
 import Button from '../UI/Button';
+import ButtonLink from '../UI/ButtonLink';
 import LanguageSelect from 'components/UI/LanguageSelect';
 import css from '../Layout/Onboarding.css';
 
@@ -24,6 +25,9 @@ export default translate('setLanguage')(class Welcome extends React.PureComponen
                     </div>
                 </main>
                 <footer>
+                    <ButtonLink to="/test">
+                        TEST
+                    </ButtonLink>
                     <Button variant="success">
                         {t('button1')}
                     </Button>
