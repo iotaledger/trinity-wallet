510c36cf097d779c30755fc2ad888cde4cb3bc57
Rajiv Shah <rajivshah1@icloud.com>
2017-10-06 23:41:12 -0400

    Fix dependencies and babelrc presets

--- src/desktop/webpack.config.js/config.base.js
+++ src/desktop/webpack.config.js/config.base.js
@@ -17,7 +17,10 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: [{ loader: 'babel-loader' }]
+                loader: 'babel-loader',
+                query: {
+                   presets: ['es2015', 'react', 'stage-0']
+                 }
             },
             {
                 test: /\.css$/,
