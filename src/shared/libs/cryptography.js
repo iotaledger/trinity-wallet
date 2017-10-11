9dad4348ad17a00e223b65f5fc6e28f1ec2d1678
Charlie Varley <cvarley100@gmail.com>
2017-10-11 19:29:38 +0100

    Remove seed console log

--- src/shared/libs/cryptography.js
+++ src/shared/libs/cryptography.js
@@ -3,14 +3,14 @@ import { SInfo } from '../../mobile/exports';
 export function storeInKeychain(key, value) {
     SInfo.setItem(key, value, {
         sharedPreferencesName: 'mySharedPrefs',
-        keychainService: 'myKeychain'
-    }).then(value => console.log(value));
+        keychainService: 'myKeychain',
+    });
 }
 
 export function getFromKeychain(key, fn) {
     SInfo.getItem(key, {
         sharedPreferencesName: 'mySharedPrefs',
-        keychainService: 'myKeychain'
+        keychainService: 'myKeychain',
     }).then(value => {
         fn(value);
     });
