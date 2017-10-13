9720b528927ff2e828c967043f6a2fe2d8d82785
Umair Sarfraz <aquadestructor@icloud.com>
2017-10-13 05:35:18 +0500

    Import inline styles to separate CSS files - Bring flexibility to Steps component

--- src/desktop/src/components/UI/Steps.js
+++ src/desktop/src/components/UI/Steps.js
@@ -1,29 +1,36 @@
+import each from 'lodash/each';
+import startCase from 'lodash/startCase';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import css from './Steps.css';
 
-export default class Steps extends React.PureComponent {
+export default class Steps extends PureComponent {
+    static propTypes = {
+        steps: PropTypes.array,
+        currentStep: PropTypes.oneOf(['manual', 'clipboard', 'paper']).isRequired,
+    };
+
+    static defaultProps = {
+        steps: ['manual_copy', 'paper_wallet', 'copy_to_clipboard'],
+    };
+
+    renderSteps(steps, currentStep) {
+        const asUiElements = [];
+        const pattern = value => new RegExp(value);
+        const isSelected = value => pattern(currentStep).test(value);
+
+        each(steps, (step, i) => {
+            asUiElements.push(<span {...isSelected(step) && { className: css.colorOverride }}>{startCase(step)}</span>);
+            if (i < steps.length - 1) {
+                asUiElements.push(<div className={css.separator} />);
+            }
+        });
+
+        return asUiElements;
+    }
+
     render() {
-        return (
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: '100%',
-                    marginBottom: '10px',
-                    justifyContent: 'center',
-                }}
-            >
-                <span>Manual Copy</span>
-                <div
-                    style={{ borderBottom: '1px solid gray', width: '150px', marginRight: '20px', marginLeft: '20px' }}
-                />
-                <span>Paper Wallet</span>
-                <div
-                    style={{ borderBottom: '1px solid gray', width: '150px', marginRight: '20px', marginLeft: '20px' }}
-                />
-                <span>Copy to clipboard</span>
-            </div>
-        );
+        const { steps, currentStep } = this.props;
+        return <div className={css.wrapper}>{this.renderSteps(steps, currentStep)}</div>;
     }
 }
