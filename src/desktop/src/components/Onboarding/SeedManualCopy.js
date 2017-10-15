5f7083054cfc5748eac972c8751f1ba871767077
Manuel <github@manuelbieh.de>
2017-10-15 17:42:04 +0200

    simplyfied BoxedSeed and made it copyable

--- src/desktop/src/components/Onboarding/SeedManualCopy.js
+++ src/desktop/src/components/Onboarding/SeedManualCopy.js
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
-import BoxedSeed from './BoxedSeed';
 import Header from './Header';
+import BoxedSeed from '../UI/BoxedSeed';
 import Button from '../UI/Button';
 import Steps from '../UI/Steps';
 
