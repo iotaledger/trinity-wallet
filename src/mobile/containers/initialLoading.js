939f379cae0ecaecab68011140475c8da72d6d7c
Rajiv Shah <rajivshah1@icloud.com>
2017-10-15 14:59:48 -0400

    Add StatusBar back in to initialLoading.js

--- src/mobile/containers/initialLoading.js
+++ src/mobile/containers/initialLoading.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Dimensions, Image, ImageBackground, Text } from 'react-native';
+import { StyleSheet, View, Dimensions, Image, ImageBackground, Text, StatusBar } from 'react-native';
 import { getCurrentYear } from '../../shared/libs/util';
 import store from '../../shared/store';
 
