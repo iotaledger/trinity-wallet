ba41061550aca4aee7b391734544a8f3c79a9173
Umair Sarfraz <aquadestructor@icloud.com>
2017-10-16 03:05:42 +0500

    Add babel presets - fix routing links

--- src/desktop/src/components/Onboarding/SeedCopyToClipboard.js
+++ src/desktop/src/components/Onboarding/SeedCopyToClipboard.js
@@ -46,7 +46,7 @@ class SeedCopyToClipboard extends React.PureComponent {
                     </div>
                 </main>
                 <footer>
-                    <Button to="/" variant="warning">
+                    <Button to="/onboarding/seed/save" variant="warning">
                         {t('button2')}
                     </Button>
                 </footer>
