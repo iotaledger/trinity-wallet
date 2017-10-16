42194414b08dd1b578777f315639b05285a71c2c
Charlie Varley <cvarley100@gmail.com>
2017-10-16 13:32:42 +0100

    Fix tx sending and add receive wait spinner

--- src/shared/libs/iota.js
+++ src/shared/libs/iota.js
@@ -1,7 +1,7 @@
 import IOTA from 'iota.lib.js';
 
-const defaultNode = 'http://node01.iotatoken.nl:14265';
+const defaultNode = 'http://node.iotawallet.info:14265/';
 
 export var iota = new IOTA({
-    provider: defaultNode
+    provider: defaultNode,
 });
