1ee69d7f90e1e29a009baa677e359cd2806a9a2e
Rajiv Shah <rajivshah1@icloud.com>
2017-10-15 22:08:27 -0400

    Take out references to DropdownHolder

--- src/mobile/containers/newSeedSetup.js
+++ src/mobile/containers/newSeedSetup.js
@@ -19,10 +19,10 @@ import { connect } from 'react-redux';
 import { randomiseSeed, setSeed } from '../../shared/actions/iotaActions';
 import { randomBytes } from 'react-native-randombytes';
 import DropdownAlert from 'react-native-dropdownalert';
-import DropdownHolder from './dropdownHolder';
+//import DropdownHolder from './dropdownHolder';
 
 const { height, width } = Dimensions.get('window');
-const dropdown = DropdownHolder.getDropDown();
+//const dropdown = DropdownHolder.getDropDown();
 
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
@@ -255,7 +255,7 @@ class NewSeedSetup extends Component {
                 animated: false,
             });
         } else {
-            dropdown.alertWithType(
+            this.dropdown.alertWithType(
                 'error',
                 'Seed has not been generated',
                 'Please click the Generate New Seed button.',
@@ -360,7 +360,7 @@ class NewSeedSetup extends Component {
                     </View>
                 </View>
                 <DropdownAlert
-                    ref={ref => DropdownHolder.setDropDown(ref)}
+                    ref={ref => (this.dropdown = ref)}
                     successColor="#009f3f"
                     errorColor="#A10702"
                     titleStyle={styles.dropdownTitle}
