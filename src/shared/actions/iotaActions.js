9099fc0cbe2a4bc545cbc82b0e582120522b553a
Rajiv Shah <rajivshah1@icloud.com>
2017-10-17 02:13:19 -0400

    Insert dropdown

--- src/shared/actions/iotaActions.js
+++ src/shared/actions/iotaActions.js
@@ -204,7 +204,7 @@ export function sendTransaction(seed, address, value, message) {
     filterSpentAddresses(outputsToCheck).then(filtered => {
         if (filtered.length !== expectedOutputsLength) {
             console.log('You cannot send to an already used address');
-            return;
+            return false;
         } else {
             // Send transfer with depth 4 and minWeightMagnitude 18
             iota.api.sendTransfer(seed, 4, 14, transfer, function(error, success) {
