6db910eb9437e9418648150e450ee25d5c979a0e
Charlie Varley <cvarley100@gmail.com>
2017-10-17 16:16:52 +0000

    Update initialLoading.js

--- src/mobile/containers/initialLoading.js
+++ src/mobile/containers/initialLoading.js
@@ -51,7 +51,7 @@ export default class InitialLoading extends Component {
             });
         } else {
             this.props.navigator.push({
-                screen: 'home',
+                screen: 'login',
                 navigatorStyle: { navBarHidden: true, screenBackgroundImageName: 'bg-green.png' },
                 animated: false,
             });
