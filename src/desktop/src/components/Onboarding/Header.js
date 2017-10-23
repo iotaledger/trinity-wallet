8d84231d23d42fea48632658e45d4fe07ba7f09d
Manuel <github@manuelbieh.de>
2017-10-23 21:21:25 +0200

    prettier formatting

--- src/desktop/src/components/Onboarding/Header.js
+++ src/desktop/src/components/Onboarding/Header.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Logo from '../UI/Logo';
 
 export default class Header extends React.PureComponent {
-
     static propTypes = {
         headline: PropTypes.string.isRequired,
     };
@@ -17,5 +16,4 @@ export default class Header extends React.PureComponent {
             </header>
         );
     }
-
 }
