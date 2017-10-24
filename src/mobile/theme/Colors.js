be0e1d3c1c497b2dd69f00779e060559f3fe8cf3
Umair Sarfraz <aquadestructor@icloud.com>
2017-10-24 21:51:48 +0500

    Add basic setup for change password

---
+++ src/mobile/theme/Colors.js
@@ -0,0 +1,12 @@
+const BaseColors = {
+    blueDarker: '#102E36',
+    white: '#FFFFFF',
+    orangeDark: '#F7D002',
+};
+
+export default {
+    ...BaseColors,
+    brand: {
+        primary: BaseColors.blueDarker,
+    },
+};
---
+++ src/mobile/theme/Fonts.js
@@ -0,0 +1,5 @@
+export default {
+    primary: 'Lato-Bold',
+    secondary: 'Lato-Regular',
+    tertiary: 'Lato-Light',
+};
