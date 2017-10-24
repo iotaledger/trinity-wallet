a3b8849b105cab6056f5a39439d0bf91dd2b3ffa
Umair Sarfraz <aquadestructor@icloud.com>
2017-10-24 22:23:39 +0500

    Clean up changePassword

--- src/mobile/theme/Colors.js
+++ src/mobile/theme/Colors.js
@@ -1,12 +1,18 @@
 const BaseColors = {
     blueDarker: '#102E36',
-    white: '#FFFFFF',
+    greenLight: '#009F3F',
     orangeDark: '#F7D002',
+    red: '#A10702',
+    white: '#FFFFFF',
 };
 
 export default {
     ...BaseColors,
     brand: {
         primary: BaseColors.blueDarker,
+    },
+    dropdown: {
+        success: BaseColors.greenLight,
+        error: BaseColors.red,
     },
 };
