ddddd853607a16caed1c7d19b181495a64afae3c
Charlie Varley <cvarley100@gmail.com>
2017-10-26 14:56:54 +0100

    Add multiseed keychain functions

--- src/shared/libs/cryptography.js
+++ src/shared/libs/cryptography.js
@@ -1,10 +1,37 @@
 import { SInfo } from '../../mobile/exports';
 
-export function storeInKeychain(key, value) {
-    SInfo.setItem(key, value, {
-        sharedPreferencesName: 'mySharedPrefs',
-        keychainService: 'myKeychain',
+export function storeInKeychain(key, seed, name) {
+    getFromKeychain(key, value => {
+        if (typeof value == 'undefined') {
+            var newSeedArray = [{ name: name, seed: seed }];
+            newSeedArray = JSON.stringify(newSeedArray);
+            store(newSeedArray);
+        } else {
+            var seedArray = JSON.parse(value);
+            for (var item of seedArray) {
+                if (item.name == name) {
+                    console.log('Error: Same name');
+                    return true;
+                }
+                if (item.seed == seed) {
+                    console.log('Error: Same seed');
+                    return false;
+                }
+                return;
+            }
+            var newSeed = { name: name, seed: seed };
+            seedArray.push(newSeed);
+            seedArray = JSON.stringify(seedArray);
+            store(seedArray);
+        }
     });
+
+    function store(seeds) {
+        SInfo.setItem(key, seeds, {
+            sharedPreferencesName: 'mySharedPrefs',
+            keychainService: 'myKeychain',
+        });
+    }
 }
 
 export function getFromKeychain(key, fn) {
