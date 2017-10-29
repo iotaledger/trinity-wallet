c78ed58c5a127cecb7e660c1cafbcd29d808b4c6
Manuel <github@manuelbieh.de>
2017-10-29 19:39:48 +0100

    small adjustments, improvements and fixes

--- src/desktop/src/components/Onboarding/Welcome.js
+++ src/desktop/src/components/Onboarding/Welcome.js
@@ -3,32 +3,35 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import Template, { Main, Footer } from './Template';
 import Button from '../UI/Button';
+// import Modal from '../UI/Modal';
 import LanguageSelect from '../UI/LanguageSelect';
 import css from '../Layout/Onboarding.css';
 
-export default translate('setLanguage')(
-    class Welcome extends React.PureComponent {
-        static propTypes = {
-            t: PropTypes.func.isRequired,
-        };
+class Welcome extends React.PureComponent {
+    static propTypes = {
+        t: PropTypes.func.isRequired,
+    };
 
-        render() {
-            const { t } = this.props;
-            return (
-                <Template headline="Hello / Salut / Hola / Hallo">
-                    <Main>
-                        <div className={css.formGroup}>
-                            <label>{t('dropdown_title')}</label>
-                            <LanguageSelect />
-                        </div>
-                    </Main>
-                    <Footer>
-                        <Button to="/instructions" variant="success">
-                            {t('button1')}
-                        </Button>
-                    </Footer>
-                </Template>
-            );
-        }
-    },
-);
+    state = { modalOpen: false };
+
+    render() {
+        const { t } = this.props;
+        return (
+            <Template headline="Hello / Salut / Hola / Hallo">
+                <Main>
+                    <div className={css.formGroup}>
+                        <label>{t('dropdown_title')}</label>
+                        <LanguageSelect />
+                    </div>
+                </Main>
+                <Footer>
+                    <Button to="/instructions" variant="success">
+                        {t('button1')}
+                    </Button>
+                </Footer>
+            </Template>
+        );
+    }
+}
+
+export default translate('setLanguage')(Welcome);
