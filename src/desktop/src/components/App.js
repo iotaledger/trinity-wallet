0fd960c97c100b176a104731f0434bd91da90c3e
Manuel <github@manuelbieh.de>
2017-11-12 00:58:47 +0100

    some basic styling for header and the list of seed

--- src/desktop/src/components/App.js
+++ src/desktop/src/components/App.js
@@ -49,17 +49,17 @@ class App extends React.Component {
 
     componentWillMount() {
         persistStore(store, { blacklist: ['tempAccount', 'notifications', 'seeds'] }, () => {
-            setTimeout(
-                () =>
-                    this.setState(() => ({
-                        initialized: true,
-                    })),
-                2500,
-            );
+            // setTimeout(
+            //     () =>
+            //         this.setState(() => ({
+            //             initialized: true,
+            //         })),
+            //     2500,
+            // );
             // TODO: re-add timeout to avoid flashes of the loading spinner. temporarily disabled for easier debugging
-            // this.setState(() => ({
-            //     initialized: true,
-            // }));
+            this.setState(() => ({
+                initialized: true,
+            }));
         });
     }
 
--- src/desktop/src/components/Main/Header/index.js
+++ src/desktop/src/components/Main/Header/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Logo from 'components/UI/Logo';
+import css from 'components/Layout/Main.css';
 
 export default class Header extends React.PureComponent {
     render() {
         return (
             <header>
-                <Logo width={48} />
-                {/* Seeds */}
+                <div className={css.logo}>
+                    <Logo width={48} />
+                </div>
+                <div className={css.seedsList}>
+                    {/* Seeds */}
+                    <ul>
+                        <li>
+                            <h1>My main wallet</h1>
+                            <h2>326 Ti</h2>
+                        </li>
+                        <li>
+                            <h1>Small change</h1>
+                            <h2>16 Mi</h2>
+                        </li>
+                        <li>
+                            <h1>For bad times</h1>
+                            <h2>18 Gi</h2>
+                        </li>
+                    </ul>
+                </div>
             </header>
         );
     }
