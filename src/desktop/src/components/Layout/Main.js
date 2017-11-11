ec9aaa545d1f43dc3d78a4ef5e53487f9d8f8332
Manuel <github@manuelbieh.de>
2017-11-12 00:11:03 +0100

    moved loading from layouts to UI, added pseudo login screen, made some minor css tweaks, added custom css styled scrollbar

--- src/desktop/src/components/Layout/Main.js
+++ src/desktop/src/components/Layout/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 // import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Balance from '../Main/Balance';
 import Login from '../Main/Login';
@@ -20,11 +20,12 @@ export default withRouter(
             return (
                 <Switch location={location}>
                     <Route path="/done" component={Done} />
-                    <Route exact path="/" component={Login} />
+                    <Route path="/login" component={Login} />
                     <Route path="/balance" component={Balance} />
                     <Route path="/send" component={Balance} />
                     <Route path="/receive" component={Balance} />
                     <Route path="/settings" component={Balance} />
+                    <Redirect from="/" to="/login" />
                 </Switch>
             );
         }
--- src/desktop/src/components/Main/Balance.js
+++ src/desktop/src/components/Main/Balance.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
 import Template, { Content } from './Template';
-
+import Loading from '../UI/Loading';
 import css from '../Layout/Main.css';
 
 class Balance extends React.Component {
-    static propTypes = {};
+    static propTypes = {
+        t: PropTypes.func.isRequired,
+    };
 
     render() {
         // const { t } = this.props;
         return (
             <Template>
-                <Content>Test</Content>
+                <Content>
+                    <Loading />
+                </Content>
             </Template>
         );
     }
