4e1f87665eccd35e204982d1b336c276331788fe
Umair Sarfraz <aquadestructor@icloud.com>
2017-11-13 04:03:28 +0500

    Setup configuration for i18next

--- src/mobile/routes/entry.js
+++ src/mobile/routes/entry.js
@@ -1,10 +1,13 @@
 import '../shim';
 import { Navigation } from 'react-native-navigation';
+import { translate } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { registerScreens } from './navigation';
 import store from '../store';
+import i18 from '../i18next';
 
 registerScreens(store, Provider);
+translate.setI18n(i18);
 
 Navigation.startSingleScreenApp({
     screen: {
