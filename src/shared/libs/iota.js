233ba3e7fe3fa0e94438d181415c55a32d2167d0
Charlie Varley <cvarley100@gmail.com>
2017-11-14 16:52:35 +0000

    Update node

--- src/shared/libs/iota.js
+++ src/shared/libs/iota.js
@@ -1,6 +1,6 @@
 import IOTA from 'iota.lib.js';
 
-const defaultNode = 'https://www.veriti.io:443';
+const defaultNode = 'http://titan.iota.community:14442';
 
 export const iota = new IOTA({
     provider: defaultNode,
