c634cab61f0b665ed54c9883fea907cd4cdce9e9
Umair Sarfraz <aquadestructor@icloud.com>
2017-11-18 19:38:57 +0500

    Revert android build changes

--- src/mobile/theme/androidSoftKeys.js
+++ src/mobile/theme/androidSoftKeys.js
@@ -1,21 +1,26 @@
 'use strict';
 
-import {NativeModules, Platform} from 'react-native';
-const {RNDetectNavbarAndroid} = NativeModules;
+// TODO: Relocate this file
+// /theme should contain UI specific stuff
+import { NativeModules, Platform } from 'react-native';
+const { RNDetectNavbarAndroid } = NativeModules;
 
 function unSupportedError() {
-  throw new Error('[react-native-detect-navbar-android] does not support iOS');
+    throw new Error('[react-native-detect-navbar-android] does not support iOS');
 }
 
-const DetectNavbar = Platform.OS === 'android' ? {
-  hasSoftKeys() {
-    return RNDetectNavbarAndroid.hasSoftKeys();
-  }
-} : {
-  hasSoftKeys() {
-    return false;
-  }
-};
+const DetectNavbar =
+    Platform.OS === 'android'
+        ? {
+              hasSoftKeys() {
+                  return RNDetectNavbarAndroid.hasSoftKeys();
+              },
+          }
+        : {
+              hasSoftKeys() {
+                  return false;
+              },
+          };
 
-export {DetectNavbar};
+export { DetectNavbar };
 export default DetectNavbar;
