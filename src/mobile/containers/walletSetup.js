ac7960fa8f71c778f823af5fa557efe9d4fc3f2b
Charlie Varley <cvarley100@gmail.com>
2017-11-22 15:35:40 +0000

    Initial loading

--- src/mobile/containers/walletSetup.js
+++ src/mobile/containers/walletSetup.js
@@ -55,8 +55,7 @@ class WalletSetup extends React.Component {
                     <View style={styles.infoTextContainer}>
                         <Image source={require('../../shared/images/info.png')} style={styles.infoIcon} />
                         <Text style={styles.infoText}>
-                            The IOTA seed is like a username and password to your account, combined into one string of
-                            81 characters.
+                            The IOTA seed is like a master key to your account. It is 81 characters long.
                         </Text>
                         <Text style={styles.infoText}>
                             <Text style={styles.infoTextLight}>You can use it to access your funds from</Text>
