1c3fd36fbe16d628505998635f8f03f314aef3c7
Vincent Taverna <vinny@getlua.com>
2017-12-05 14:29:55 -0500

    hoist onboardingComplete inline requires

--- src/mobile/containers/onboardingComplete.js
+++ src/mobile/containers/onboardingComplete.js
@@ -3,6 +3,9 @@ import { translate } from 'react-i18next';
 import { StyleSheet, View, Text, TouchableOpacity, Image, ImageBackground, StatusBar } from 'react-native';
 
 import { width, height } from '../util/dimensions';
+import balloonsImagePath from 'iota-wallet-shared-modules/images/balloons.png';
+import blueBackgroundImagePath from 'iota-wallet-shared-modules/images/bg-blue.png';
+import iotaGlowImagePath from 'iota-wallet-shared-modules/images/iota-glow.png';
 
 class OnboardingComplete extends React.Component {
     onNextPress() {
@@ -21,13 +24,10 @@ class OnboardingComplete extends React.Component {
         const { t } = this.props;
 
         return (
-            <ImageBackground source={require('iota-wallet-shared-modules/images/bg-blue.png')} style={styles.container}>
+            <ImageBackground source={blueBackgroundImagePath} style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <View style={styles.topContainer}>
-                    <Image
-                        source={require('iota-wallet-shared-modules/images/iota-glow.png')}
-                        style={styles.iotaLogo}
-                    />
+                    <Image source={iotaGlowImagePath} style={styles.iotaLogo} />
                 </View>
                 <View style={styles.midContainer}>
                     <View style={styles.infoTextContainer}>
@@ -36,7 +36,7 @@ class OnboardingComplete extends React.Component {
                             look in the Settings menu.
                         </Text>
                     </View>
-                    <Image source={require('iota-wallet-shared-modules/images/balloons.png')} style={styles.party} />
+                    <Image source={balloonsImagePath} style={styles.party} />
                 </View>
                 <View style={styles.bottomContainer}>
                     <TouchableOpacity onPress={event => this.onNextPress()}>
