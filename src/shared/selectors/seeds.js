c191dcf177c474b3d925891e5daacf5b283e6c2d
Manuel Bieh <info@manuelbieh.de>
2017-12-06 14:33:31 +0100

    minor stuff

--- src/shared/selectors/seeds.js
+++ src/shared/selectors/seeds.js
@@ -7,9 +7,18 @@ export const getSelectedSeed = createSelector(
     ({ items, selectedSeedIndex }) => items[selectedSeedIndex] || {},
 );
 
+export const selectedSeedSelector = createSelector(
+    seedsSelector,
+    ({ items, selectedSeedIndex }) => items[selectedSeedIndex] || {},
+);
+
 export const getSelectedIndex = createSelector(seedsSelector, ({ selectedSeedIndex }) => selectedSeedIndex);
 
+export const selectedSeedIndexSelector = createSelector(seedsSelector, ({ selectedSeedIndex }) => selectedSeedIndex);
+
 export const getSeedItems = createSelector(seedsSelector, ({ items }) => items || []);
+
+export const seedItemsSelector = createSelector(seedsSelector, ({ items }) => items || []);
 
 export const getMostRecentSeedIndex = createSelector(
     getSeedItems,
