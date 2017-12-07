c5456735e289321c1f96bfc747c7f236cf441937
Charlie Varley <cvarley100@gmail.com>
2017-12-07 18:15:35 +0000

    UI fixes. Remove language support

--- src/mobile/containers/initialLoading.js
+++ src/mobile/containers/initialLoading.js
@@ -47,7 +47,7 @@ export default class InitialLoading extends Component {
         if (!state.account.onboardingComplete) {
             this.clearKeychain();
             this.props.navigator.push({
-                screen: 'languageSetup',
+                screen: 'welcome',
                 navigatorStyle: { navBarHidden: true, navBarTransparent: true },
                 animated: false,
                 overrideBackPress: true,
