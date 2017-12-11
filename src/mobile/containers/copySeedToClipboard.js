844d1d71175ddeb4a991595bb2d6c9ea1755fe58
Rajiv Shah <rajivshah1@icloud.com>
2017-12-11 08:55:49 -0500

    Add translate HOC

--- src/mobile/containers/copySeedToClipboard.js
+++ src/mobile/containers/copySeedToClipboard.js
@@ -73,7 +73,7 @@ class CopySeedToClipboard extends React.Component {
                     <Seedbox seed={this.props.tempAccount.seed} />
                     <TouchableOpacity onPress={event => this.onCopyPress()} style={{ marginTop: height / 22 }}>
                         <View style={styles.copyButton}>
-                            <Text style={styles.copyText}>COPY TO CLIPBOARD</Text>
+                            <Text style={styles.copyText}>{t('global:copyToClipboard').toUpperCase()}</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
