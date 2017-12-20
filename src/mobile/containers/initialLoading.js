4e74440f759a0c693c4a8a65816e9e23accd3226
Charlie Varley <charlie@charlies-mbp-2.mynet>
2017-12-20 13:02:46 +0000

    Fix new seed setup UI

--- src/mobile/containers/initialLoading.js
+++ src/mobile/containers/initialLoading.js
@@ -44,14 +44,14 @@ export default class InitialLoading extends Component {
         if (!state.account.onboardingComplete) {
             this.clearKeychain();
             this.props.navigator.push({
-                screen: 'welcome',
+                screen: 'newSeedSetup',
                 navigatorStyle: { navBarHidden: true, navBarTransparent: true },
                 animated: false,
                 overrideBackPress: true,
             });
         } else {
             this.props.navigator.push({
-                screen: 'login',
+                screen: 'newSeedSetup',
                 navigatorStyle: { navBarHidden: true, navBarTransparent: true },
                 animated: false,
                 overrideBackPress: true,
