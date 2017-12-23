04a0304cc9d99a91271d07acec00ccf50715684f
Rajiv Shah <rajivshah1@icloud.com>
2017-12-23 15:30:32 -0500

    Reconfigure i18next in shared

--- src/shared/i18next.js
+++ src/shared/i18next.js
@@ -4,41 +4,54 @@ const I18N_DEFAULT_LANGUAGE = 'en';
 
 i18next
     //.use(LanguageDetector)
-    .init({
-        fallbackLng: I18N_DEFAULT_LANGUAGE,
-        fallbackNS: 'Common',
-        order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
-        resources: {
-            ar: { Common: require('./locales/ar/translation.json') },
-            da: { Common: require('./locales/da/translation.json') },
-            de: { Common: require('./locales/de/translation.json') },
-            el: { Common: require('./locales/el/translation.json') },
-            en: { Common: require('./locales/en/translation.json') },
-            es_ES: { Common: require('./locales/es-ES/translation.json') },
-            es_LA: { Common: require('./locales/es-LA/translation.json') },
-            fi: { Common: require('./locales/fi/translation.json') },
-            fr: { Common: require('./locales/fr/translation.json') },
-            he: { Common: require('./locales/he/translation.json') },
-            hi: { Common: require('./locales/hi/translation.json') },
-            id: { Common: require('./locales/id/translation.json') },
-            it: { Common: require('./locales/it/translation.json') },
-            ja: { Common: require('./locales/ja/translation.json') },
-            ko: { Common: require('./locales/ko/translation.json') },
-            lv: { Common: require('./locales/lv/translation.json') },
-            nl: { Common: require('./locales/nl/translation.json') },
-            no: { Common: require('./locales/no/translation.json') },
-            pl: { Common: require('./locales/pl/translation.json') },
-            pt_BR: { Common: require('./locales/pt-BR/translation.json') },
-            pt_PT: { Common: require('./locales/pt-PT/translation.json') },
-            ro: { Common: require('./locales/ro/translation.json') },
-            ru: { Common: require('./locales/ru/translation.json') },
-            sl: { Common: require('./locales/sl/translation.json') },
-            sv_SE: { Common: require('./locales/sv-SE/translation.json') },
-            tr: { Common: require('./locales/tr/translation.json') },
-            ur: { Common: require('./locales/ur/translation.json') },
-            zh_CN: { Common: require('./locales/zh-CN/translation.json') },
-            zh_TW: { Common: require('./locales/zh-TW/translation.json') },
+    .init(
+        {
+            fallbackLng: I18N_DEFAULT_LANGUAGE,
+            fallbackNS: 'global',
+            resources: {
+                ar: require('../shared/locales/ar/translation.json'),
+                da: require('../shared/locales/da/translation.json'),
+                de: require('../shared/locales/de/translation.json'),
+                el: require('../shared/locales/el/translation.json'),
+                en: require('../shared/locales/en/translation.json'),
+                es_ES: require('../shared/locales/es-ES/translation.json'),
+                es_LA: require('../shared/locales/es-LA/translation.json'),
+                fi: require('../shared/locales/fi/translation.json'),
+                fr: require('../shared/locales/fr/translation.json'),
+                he: require('../shared/locales/he/translation.json'),
+                hi: require('../shared/locales/hi/translation.json'),
+                id: require('../shared/locales/id/translation.json'),
+                it: require('../shared/locales/it/translation.json'),
+                ja: require('../shared/locales/ja/translation.json'),
+                ko: require('../shared/locales/ko/translation.json'),
+                lv: require('../shared/locales/lv/translation.json'),
+                nl: require('../shared/locales/nl/translation.json'),
+                no: require('../shared/locales/no/translation.json'),
+                pl: require('../shared/locales/pl/translation.json'),
+                pt_BR: require('../shared/locales/pt-BR/translation.json'),
+                pt_PT: require('../shared/locales/pt-PT/translation.json'),
+                ro: require('../shared/locales/ro/translation.json'),
+                ru: require('../shared/locales/ru/translation.json'),
+                sl: require('../shared/locales/sl/translation.json'),
+                sv_SE: require('../shared/locales/sv-SE/translation.json'),
+                tr: require('../shared/locales/tr/translation.json'),
+                ur: require('../shared/locales/ur/translation.json'),
+                zh_CN: require('../shared/locales/zh-CN/translation.json'),
+                zh_TW: require('../shared/locales/zh-TW/translation.json'),
+            },
+            interpolation: {
+                escapeValue: false,
+                format: function(value, format, lng) {
+                    if (format === 'uppercase') return value.toUpperCase();
+                    return value;
+                },
+            },
         },
-    });
+        function(err, t) {
+            if (err) {
+                console.error(err);
+            }
+        },
+    );
 
 export default i18next;
