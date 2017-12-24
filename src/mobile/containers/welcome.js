29a430259601f41b667ea94851c1be5293a28706
Umair Sarfraz <aquadestructor@icloud.com>
2017-12-24 06:22:01 +0500

    Get rid of callbacks

--- src/mobile/containers/welcome.js
+++ src/mobile/containers/welcome.js
@@ -1,27 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import { StyleSheet, View, Text, TouchableOpacity, Image, StatusBar } from 'react-native';
-import OnboardingButtons from '../components/onboardingButtons.js';
 import COLORS from '../theme/Colors';
 import GENERAL from '../theme/general';
 
-import blueBackgroundImagePath from 'iota-wallet-shared-modules/images/bg-blue.png';
 import iotaGlowImagePath from 'iota-wallet-shared-modules/images/iota-glow.png';
 import { width, height } from '../util/dimensions';
 
-class Welcome extends React.Component {
+class Welcome extends Component {
     constructor(props) {
         super(props);
-    }
-
-    onBackPress() {
-        this.props.navigator.pop({
-            navigatorStyle: {
-                navBarHidden: true,
-                navBarTransparent: true,
-            },
-            animated: false,
-        });
     }
 
     onNextPress() {
@@ -126,22 +114,6 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent',
         paddingTop: height / 30,
         textAlign: 'center',
-    },
-    nextButton: {
-        borderColor: '#9DFFAF',
-        borderWidth: 1.2,
-        borderRadius: GENERAL.borderRadius,
-        width: width / 3,
-        height: height / 14,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginBottom: height / 20,
-    },
-    nextText: {
-        color: '#9DFFAF',
-        fontFamily: 'Lato-Light',
-        fontSize: width / 24.4,
-        backgroundColor: 'transparent',
     },
 });
 
