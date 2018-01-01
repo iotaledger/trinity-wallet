8c2e21fe73f9b58aa89732df6f185d9598a6160a
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-02 02:09:44 +0500

    Fix dropdown usage for currencySelection

--- src/mobile/components/currencySelection.js
+++ src/mobile/components/currencySelection.js
@@ -73,12 +73,18 @@ class CurrencySelection extends Component {
     render() {
         const { currency, currencies, backPress, t } = this.props;
         return (
-            <TouchableWithoutFeedback onPress={() => this.dropdown.closeDropdown()}>
+            <TouchableWithoutFeedback
+                onPress={() => {
+                    if (this.dropdown) {
+                        this.dropdown.closeDropdown();
+                    }
+                }}
+            >
                 <View style={styles.container}>
                     <View style={styles.topContainer}>
                         <View style={{ flex: 0.2 }} />
                         <Dropdown
-                            ref={c => {
+                            onRef={c => {
                                 this.dropdown = c;
                             }}
                             title={t('currency')}
