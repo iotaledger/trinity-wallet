90ee530eee0a80d09323c7b8996bb60520249244
Michel Machado <michelpm1@gmail.com>
2018-01-06 14:14:14 -0200

    wip windows and mac deep linking working

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -2,23 +2,15 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const url = require('url');
-const path = require('path');
 
 let mainWindow;
 let deeplinkingUrl;
-let win = false;
 
 const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
     if (process.platform == 'win32') {
-        win = true;
         deeplinkingUrl = argv.slice(1);
     }
     logEverywhere('app.makeSingleInstance# ' + deeplinkingUrl);
-
-    if (win) {
-        if (win.isMinimized()) win.restore();
-        win.focus();
-    }
 });
 
 if (shouldQuit) {
@@ -32,8 +24,8 @@ function createWindow() {
         height: 768,
     });
 
-    mainWindow.loadURL('http://localhost:8080/index.html');
-    //mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+    //mainWindow.loadURL('http://localhost:8080/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
     mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', () => {
