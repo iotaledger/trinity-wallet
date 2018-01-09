dfac033d903ac8fba23c807d4e8c7de0ce9e7c81
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-01-09 12:04:12 +0000

    Login UI fixes

--- src/mobile/components/enterPasswordOnLogin.js
+++ src/mobile/components/enterPasswordOnLogin.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { StyleSheet, View, Text, TouchableWithoutFeedback, TouchableOpacity, Image, Keyboard } from 'react-native';
 import { TextField } from 'react-native-material-textfield';
-import iotaGlowImagePath from 'iota-wallet-shared-modules/images/iota-glow.png';
+import whiteIotaImagePath from 'iota-wallet-shared-modules/images/iota-white.png';
+import blackIotaImagePath from 'iota-wallet-shared-modules/images/iota-black.png';
 import THEMES from '../theme/themes';
 import { width, height } from '../util/dimensions';
 import OnboardingButtons from '../components/onboardingButtons';
@@ -13,7 +14,7 @@ const styles = StyleSheet.create({
         flex: 1.2,
         alignItems: 'center',
         justifyContent: 'flex-start',
-        paddingTop: height / 22,
+        paddingTop: height / 16,
     },
     midContainer: {
         flex: 4.8,
@@ -30,7 +31,7 @@ const styles = StyleSheet.create({
     titleContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: height / 15,
+        paddingTop: height / 8,
     },
     title: {
         fontFamily: 'Lato-Regular',
@@ -39,8 +40,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent',
     },
     iotaLogo: {
-        height: width / 5,
-        width: width / 5,
+        height: width / 7,
+        width: width / 7,
     },
     loginButton: {
         borderWidth: 1.2,
@@ -79,12 +80,13 @@ class EnterPasswordOnLogin extends Component {
     render() {
         const { password } = this.state;
         const { t, textColor, secondaryBackgroundColor, negativeColor } = this.props;
+        const iotaLogoImagePath = secondaryBackgroundColor === 'white' ? whiteIotaImagePath : blackIotaImagePath;
 
         return (
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View>
                     <View style={styles.topContainer}>
-                        <Image source={iotaGlowImagePath} style={styles.iotaLogo} />
+                        <Image source={iotaLogoImagePath} style={styles.iotaLogo} />
                         <View style={styles.titleContainer}>
                             <Text style={[styles.title, textColor]}>{t('enterPassword')}</Text>
                         </View>
