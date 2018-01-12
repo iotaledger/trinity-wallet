887a81382e68425f32ba4325a1d561a4d241b813
iotadance <merch.iota.dance@gmail.com>
2018-01-12 11:20:22 +0200

    Native menu initial implementation

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -1,25 +1,52 @@
 const electron = require('electron');
+const initMenu = require('./lib/Menu.js');
+const path = require('path');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const devMode = process.env.NODE_ENV === 'development';
 
-let mainWindow;
+const windows = {
+    main: null,
+};
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
+    windows.main = new BrowserWindow({
         width: 1024,
         height: 768,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'lib/window.js'),
+        },
     });
 
-    // mainWindow.loadURL('http://localhost:8080/index.html');
-    // mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
-    mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+    const url = devMode ? 'http://localhost:1074/' : 'file://' + __dirname + '/dist/index.html';
 
-    mainWindow.webContents.openDevTools();
+    windows.main.loadURL(url);
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
+    if (devMode) {
+        windows.main.webContents.openDevTools();
+
+        const {
+            default: installExtension,
+            REACT_DEVELOPER_TOOLS,
+            REDUX_DEVTOOLS,
+        } = require('electron-devtools-installer');
+
+        installExtension(REACT_DEVELOPER_TOOLS);
+        installExtension(REDUX_DEVTOOLS);
+    }
+
+    windows.main.on('closed', () => {
+        windows.main = null;
     });
 }
+
+const getWindow = function(windowName) {
+    return windows[windowName];
+};
+
+initMenu(app, getWindow);
 
 app.on('ready', createWindow);
 
@@ -30,7 +57,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
+    if (windows.main === null) {
         createWindow();
     }
 });
