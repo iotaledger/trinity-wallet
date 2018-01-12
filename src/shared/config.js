33de07dbfb4e546e5f5f5a5b2245a267c31208f0
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-12 21:06:01 +0500

    Initialize IOTA instance with a default node

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,1 +1,10 @@
 export const defaultNode = 'https://www.veriti.io';
+export const nodes = [
+    'https://iri2-api.iota.fm:443',
+    'https://iri3-api.iota.fm:443',
+    'https://node.tangle.works:443',
+    'https://iotanode.us:443',
+    'http://astra2261.startdedicated.net:14265',
+    'http://iota.nck.nz:14265',
+    'http://www.veriti.io',
+];
