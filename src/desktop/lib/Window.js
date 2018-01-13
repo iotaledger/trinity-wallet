2d372cdecd052a6f3d7fa174401fbc84888b11c1
iotadance <merch.iota.dance@gmail.com>
2018-01-13 23:19:48 +0200

    Native menu localisation

--- src/desktop/lib/Window.js
+++ src/desktop/lib/Window.js
@@ -1,10 +1,47 @@
 const { ipcRenderer: ipc } = require('electron');
 
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
 const Electron = {
     updateMenu: (attribute, value) => {
         ipc.send('menu.update', {
             attribute: attribute,
             value: value,
+        });
+    },
+
+    changeLanguage: (t) => {
+        ipc.send('menu.language', {
+            about: 'About',
+            settings: capitalize(t('home:settings')),
+            language: t('languageSetup:language'),
+            currency: t('settings:currency'),
+            theme: t('settings:theme'),
+            twoFA: t('settings:twoFA'),
+            changePassword: t('settings:hangePassword'),
+            advanced: t('settings:advanced'),
+            hide: 'Hide',
+            hideOthers: 'Hide Others',
+            showAll: 'Show All',
+            quit: 'Quit',
+            edit: 'Edit',
+            undo: 'Undo',
+            redo: 'Redo',
+            cut: 'Cut',
+            copy: 'Copy',
+            paste: 'Paste',
+            selectAll: 'Select All',
+            account: t(''),
+            balance: capitalize(t('home:balance')),
+            send: capitalize(t('home:send')),
+            receive: capitalize(t('home:receive')),
+            history: capitalize(t('home:history')),
+            logout: t('settings:logout'),
+            logoutConfirm: t('logoutConfirmationModal:logoutConfirmation'),
+            yes: t('global:no'),
+            no: t('global:yes'),
         });
     },
 
