7deecbc8bfa450d65a4884feed5260738e68c8f5
Rajiv Shah <rajivshah1@icloud.com>
2018-01-13 19:00:08 -0500

    Update nodes

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,10 +1,10 @@
-export const defaultNode = 'http://trinity.iota.fm';
+export const defaultNode = 'http://trinity.iota.fm:80';
 export const nodes = [
-    'http://trinity.iota.fm/',
+    'http://trinity.iota.fm:80',
     'http://iri2.iota.fm:80',
-    'http://iri3.iota.fm:80',
     'https://node.neffware.com:443',
     'https://iotanode.us:443',
     'http://astra2261.startdedicated.net:14265',
     'https://www.veriti.io',
+    'http://148.251.181.105:14265',
 ];
