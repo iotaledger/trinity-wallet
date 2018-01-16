cee2c12568b7a72b7a3e3c9fba4debb9a4c480a9
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-17 00:09:55 +0500

    Detect app upgrades on login screen

--- src/shared/actions/app.js
+++ src/shared/actions/app.js
@@ -1,3 +1,7 @@
+import noop from 'lodash/noop';
+import { getStoredState, purgeStoredState } from 'redux-persist';
+import { updatePersistedState } from '../libs/util';
+
 export const ActionTypes = {
     SET_ONBOARDING_COMPLETED: 'IOTA/APP/ONBOARDING/COMPLETE',
     WALLET_LOGOUT: 'IOTA/APP/WALLET/LOGOUT',
@@ -28,3 +32,30 @@ export function resetWallet() {
         type: ActionTypes.WALLET_RESET,
     };
 }
+
+export const migrate = (versions, config, persistor) => (dispatch, getState) => {
+    let restoredState = {};
+    console.log('Gonna login');
+    getStoredState(config)
+        .then(persistedState => {
+            console.log('Persisted state', persistedState);
+            restoredState = persistedState;
+
+            if (persistor) {
+                console.log('Purging');
+                return persistor.purge();
+            }
+
+            throw new Error('persistor not defined.');
+        })
+        .then(() => {
+            console.log('Before', getState());
+            dispatch(resetWallet());
+            console.log('After', getState());
+            dispatch(setAppVersions(versions));
+
+            const updatedState = updatePersistedState(getState(), restoredState);
+            persistor.rehydrate(updatedState);
+        })
+        .catch(err => console.error(err));
+};
