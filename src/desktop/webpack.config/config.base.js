197d37a3f2ab0a33b69cb9cb4761bbdf70fd68dc
Michel Machado <michelpm1@gmail.com>
2018-01-18 20:46:22 -0200

    changed regex to deal with real address

--- src/desktop/webpack.config/config.base.js
+++ src/desktop/webpack.config/config.base.js
@@ -6,7 +6,7 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
-    // target: 'electron',
+     target: 'electron',
     output: {
         path: path.join(__dirname, '..', 'dist'),
         pathinfo: true,
