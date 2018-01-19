87eaf44b64c214f9ff4ae945d1bdc5d0f4bf6348
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-19 06:18:46 +0500

    Make sure we check for confirmation states during polling

--- src/shared/libs/promoter.js
+++ src/shared/libs/promoter.js
@@ -1,10 +1,11 @@
+import assign from 'lodash/assign';
 import get from 'lodash/get';
 import each from 'lodash/each';
 import transform from 'lodash/transform';
 import { isMinutesAgo, convertUnixTimeToJSDate, isValid } from '../libs/dateUtils';
 import { iota } from '../libs/iota';
 
-export const isAboveMaxDepth = timestamp => {
+export const isAboveMaxDepth = (timestamp) => {
     return timestamp < Date.now() && Date.now() - parseInt(timestamp) < 11 * 60 * 1000;
 };
 
@@ -15,7 +16,7 @@ export const getFirstConsistentTail = (tails, idx) => {
 
     return iota.api
         .isPromotable(get(tails[idx], 'hash'))
-        .then(state => {
+        .then((state) => {
             if (state && isAboveMaxDepth(get(tails[idx], 'attachmentTimestamp'))) {
                 return tails[idx];
             }
@@ -26,7 +27,7 @@ export const getFirstConsistentTail = (tails, idx) => {
         .catch(() => false);
 };
 
-export const isWithinADayAndTenMinutesAgo = timestamp => {
+export const isWithinADayAndTenMinutesAgo = (timestamp) => {
     const dateObject = convertUnixTimeToJSDate(timestamp);
 
     if (isValid(dateObject.format())) {
@@ -39,7 +40,7 @@ export const isWithinADayAndTenMinutesAgo = timestamp => {
     );
 };
 
-export const isWithinADay = timestamp => {
+export const isWithinADay = (timestamp) => {
     const dateObject = convertUnixTimeToJSDate(timestamp);
 
     if (isValid(dateObject.format())) {
@@ -49,7 +50,7 @@ export const isWithinADay = timestamp => {
     return !isMinutesAgo(convertUnixTimeToJSDate(timestamp / 1000), 1440);
 };
 
-export const isTenMinutesAgo = timestamp => {
+export const isTenMinutesAgo = (timestamp) => {
     const dateObject = convertUnixTimeToJSDate(timestamp);
 
     if (isValid(dateObject.format())) {
@@ -59,7 +60,7 @@ export const isTenMinutesAgo = timestamp => {
     return isMinutesAgo(convertUnixTimeToJSDate(timestamp / 1000), 10);
 };
 
-export const isADayAgo = timestamp => {
+export const isADayAgo = (timestamp) => {
     const dateObject = convertUnixTimeToJSDate(timestamp);
 
     if (isValid(dateObject.format())) {
@@ -69,23 +70,24 @@ export const isADayAgo = timestamp => {
     return isMinutesAgo(convertUnixTimeToJSDate(timestamp / 1000), 1440);
 };
 
-export const getBundleTailsForSentTransfers = (transfers, addresses) => {
+export const getBundleTailsForSentTransfers = (transfers, addresses, account) => {
     const categorizedTransfers = iota.utils.categorizeTransfers(transfers, addresses);
     const sentTransfers = get(categorizedTransfers, 'sent');
 
     const grabTails = (payload, val) => {
-        each(val, v => {
+        each(val, (v) => {
             const attachmentTimestamp = get(v, 'attachmentTimestamp');
 
             // Pick all those transaction that were replayed within twenty-four hours
             const hasMadeReattachmentWithinADay = isWithinADay(attachmentTimestamp);
             if (!v.persistence && v.currentIndex === 0 && v.value > 0 && hasMadeReattachmentWithinADay) {
                 const bundle = v.bundle;
+                const withAccount = assign({}, v, { account });
 
                 if (bundle in payload) {
-                    payload[bundle] = [...payload[bundle], v];
+                    payload[bundle] = [...payload[bundle], withAccount];
                 } else {
-                    payload[bundle] = [v];
+                    payload[bundle] = [withAccount];
                 }
             }
         });
