b40fbb95cc6f4a567888c99d0b16ce512c930e18
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-01-22 19:28:59 +0000

    Fix iPhone X tx status display bug

--- src/mobile/components/transactionRow.js
+++ src/mobile/components/transactionRow.js
@@ -290,7 +290,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent',
         fontFamily: 'Lato-Regular',
         fontSize: width / 31.8,
-        paddingRight: width / 20,
+        paddingRight: width / 25,
     },
     modalValue: {
         backgroundColor: 'transparent',
