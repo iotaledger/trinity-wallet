c2cebadfb7b23cff2dc6704b3c692179dfd7c285
iotadance <merch.iota.dance@gmail.com>
2018-01-23 23:25:49 +0200

    Theme naming, bugfixes, icons

--- src/desktop/src/guide/pages/Icons.js
+++ src/desktop/src/guide/pages/Icons.js
@@ -7,17 +7,22 @@ import css from './icons.css';
 
 const Colors = () => {
     return (
-        <div className={css.icons}>
-            {Object.keys(icons).map((icon, key) => {
-                return (
-                    <div key={key}>
-                        <Icon icon={icon} size={64} />
-                        <Icon icon={icon} size={32} />
-                        <Icon icon={icon} size={16} />
-                        <p>{icon}</p>
-                    </div>
-                );
-            })}
+        <div>
+            <h1>Icons</h1>
+            <p>Where possible, vector icons should be used. Preferred icon sizes - 16, 32, 64.</p>
+            <hr />
+            <div className={css.icons}>
+                {Object.keys(icons).map((icon, key) => {
+                    return (
+                        <div key={key}>
+                            <Icon icon={icon} size={64} />
+                            <Icon icon={icon} size={32} />
+                            <Icon icon={icon} size={16} />
+                            <p>{icon}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
