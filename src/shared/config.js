389e4bc36422f858c187c6f73b3716adc700248d
Rajiv Shah <rajivshah1@icloud.com>
2018-01-31 09:46:49 -0500

    Fix tag

--- src/shared/config.js
+++ src/shared/config.js
@@ -12,5 +12,5 @@ export const nodes = [
 
 export const DEFAULT_DEPTH = 4;
 export const DEFAULT_MIN_WEIGHT_MAGNITUDE = 14;
-export const DEFAULT_TAG = 'IOTA';
+export const DEFAULT_TAG = 'TRINITY';
 export const DEFAULT_SECURITY = 2;
