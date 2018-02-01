cd5ffc988570b5e4ee53c633f026979857a618d0
iotadance <merch.iota.dance@gmail.com>
2018-02-02 00:53:10 +0200

    Theme color naming update

--- src/shared/themes/themes.js
+++ src/shared/themes/themes.js
@@ -2,16 +2,18 @@ import tinycolor from 'tinycolor2';
 
 import Default from './themes/Default';
 import GreyDark from './themes/GreyDark';
-import Grey from './themes/Grey';
+import Mint from './themes/Mint';
 import MonochromeLight from './themes/MonochromeLight';
 import MonochromeDark from './themes/MonochromeDark';
 
 export const themes = {
     Default: Default,
-    Grey: Grey,
+    Mint: Mint,
+    /*,
     'Grey dark': GreyDark,
     'Monochrome Light': MonochromeLight,
     'Monochrome Dark': MonochromeDark,
+    */
 };
 
 export const getHSL = (color) => {
