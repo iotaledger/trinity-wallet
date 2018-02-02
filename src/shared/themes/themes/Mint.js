095a5c639430d1247ee107691e733f94ee147668
iotadance <merch.iota.dance@gmail.com>
2018-02-02 12:50:04 +0200

    Mint theme update

--- src/shared/themes/themes/Mint.js
+++ src/shared/themes/themes/Mint.js
@@ -5,7 +5,7 @@ export default {
     secondary: { color: '#5CBAFE', hover: '#4996CE', body: '#1E1E1E' },
     positive: { color: '#44C27F', hover: '#409568', body: '#1E1E1E' },
     negative: { color: '#CF6666', hover: '#B94006', body: '#1E1E1E' },
-    highlight: { color: '#DFD386', hover: '#D4C363', body: '#1E1E1E' },
+    highlight: { color: '#DFD386', hover: '#BEAB40', body: '#1E1E1E' },
     extra: { color: '#88D4FF', hover: '#669FBF', body: '#1E1E1E' },
     label: { color: '#F0F2F6', hover: '#66CF72', active: '#09F9BA' },
     input: { color: '#1E1E1E', bg: '#FFFFFF', alt: '#727272' },
