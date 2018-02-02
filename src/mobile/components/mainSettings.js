326a1f5a5cdecb62456fc11cd0c824a75fcca703
Rajiv Shah <rajivshah1@icloud.com>
2018-02-02 12:52:06 -0500

    Implement fingerprint auth

--- src/mobile/components/mainSettings.js
+++ src/mobile/components/mainSettings.js
@@ -156,12 +156,12 @@ const MainSettings = props => (
         </View>
         <View style={styles.itemContainer}>
             <TouchableOpacity
-                onPress={() => props.on2FASetupPress()}
+                onPress={() => props.setSetting('securitySettings')}
                 hitSlop={{ top: height / 55, bottom: height / 55, left: width / 55, right: width / 55 }}
             >
                 <View style={styles.item}>
                     <Image source={props.twoFactorAuthImagePath} style={styles.icon} />
-                    <Text style={[styles.titleText, props.textColor]}>{props.t('twoFA')}</Text>
+                    <Text style={[styles.titleText, props.textColor]}>{props.t('securitySettings')}</Text>
                 </View>
             </TouchableOpacity>
         </View>
@@ -212,7 +212,6 @@ MainSettings.propTypes = {
     onLanguagePress: PropTypes.func.isRequired,
     setSetting: PropTypes.func.isRequired,
     setModalContent: PropTypes.func.isRequired,
-    on2FASetupPress: PropTypes.func.isRequired,
     onThemePress: PropTypes.func.isRequired,
     t: PropTypes.func.isRequired,
 };
---
+++ src/mobile/components/securitySettings.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+import { Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import whiteNodeImagePath from 'iota-wallet-shared-modules/images/node-white.png';
+import whiteSyncImagePath from 'iota-wallet-shared-modules/images/sync-white.png';
+import whiteCrossImagePath from 'iota-wallet-shared-modules/images/cross-white.png';
+import blackNodeImagePath from 'iota-wallet-shared-modules/images/node-black.png';
+import blackSyncImagePath from 'iota-wallet-shared-modules/images/sync-black.png';
+import blackCrossImagePath from 'iota-wallet-shared-modules/images/cross-black.png';
+import { width, height } from '../util/dimensions';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    topContainer: {
+        flex: 10,
+        justifyContent: 'flex-end',
+    },
+    bottomContainer: {
+        flex: 1,
+        width,
+        paddingHorizontal: width / 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+    },
+    itemContainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        width,
+        paddingHorizontal: width / 15,
+    },
+    icon: {
+        width: width / 22,
+        height: width / 22,
+        marginRight: width / 25,
+    },
+    backIcon: {
+        width: width / 28,
+        height: width / 28,
+        marginRight: width / 20,
+    },
+    titleText: {
+        fontFamily: 'Lato-Regular',
+        fontSize: width / 23,
+        backgroundColor: 'transparent',
+    },
+    separator: {
+        borderBottomColor: 'white',
+        borderBottomWidth: height / 1500,
+        width: width / 1.16,
+        alignSelf: 'center',
+    },
+    separatorContainer: {
+        flex: 0.5,
+        justifyContent: 'center',
+    },
+    settingText: {
+        fontFamily: 'Lato-Light',
+        fontSize: width / 23,
+        marginLeft: width / 12,
+        width: width / 2.4,
+        backgroundColor: 'transparent',
+    },
+});
+
+/* eslint-disable react/prefer-stateless-function */
+class SecuritySettings extends Component {
+    static propTypes = {
+        setSetting: PropTypes.func.isRequired,
+        secondaryBackgroundColor: PropTypes.string.isRequired,
+        textColor: PropTypes.object.isRequired,
+        borderColor: PropTypes.object.isRequired,
+        arrowLeftImagePath: PropTypes.number.isRequired,
+        addImagePath: PropTypes.number.isRequired,
+        t: PropTypes.func.isRequired,
+        on2FASetupPress: PropTypes.func.isRequired,
+        onFingerSetup: PropTypes.func.isRequired,
+    };
+
+    render() {
+        const { t, textColor, borderColor, secondaryBackgroundColor, arrowLeftImagePath, addImagePath } = this.props;
+        const nodeImagePath = secondaryBackgroundColor === 'white' ? whiteNodeImagePath : blackNodeImagePath;
+        const syncImagePath = secondaryBackgroundColor === 'white' ? whiteSyncImagePath : blackSyncImagePath;
+        const crossImagePath = secondaryBackgroundColor === 'white' ? whiteCrossImagePath : blackCrossImagePath;
+
+        return (
+            <View style={styles.container}>
+                <View style={{ flex: 4.5 }}>
+                    <View style={styles.itemContainer}>
+                        <TouchableOpacity
+                            onPress={() => this.props.on2FASetupPress()}
+                            hitSlop={{ top: height / 55, bottom: height / 55, left: width / 55, right: width / 55 }}
+                        >
+                            <View style={styles.item}>
+                                <Image source={nodeImagePath} style={styles.icon} />
+                                <Text style={[styles.titleText, textColor]}>{t('twoFA')}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.itemContainer}>
+                        <TouchableOpacity
+                            onPress={() => this.props.onFingerSetup()}
+                            hitSlop={{ top: height / 55, bottom: height / 55, left: width / 55, right: width / 55 }}
+                        >
+                            <View style={styles.item}>
+                                <Image source={addImagePath} style={styles.icon} />
+                                <Text style={[styles.titleText, textColor]}>{t('fingerprint')}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 5.5, justifyContent: 'flex-end' }}>
+                        <View style={{ flex: 4.5 }} />
+                        <View style={styles.itemContainer}>
+                            <TouchableOpacity
+                                onPress={() => this.props.setSetting('mainSettings')}
+                                hitSlop={{ top: height / 55, bottom: height / 55, left: width / 55, right: width / 55 }}
+                            >
+                                <View style={styles.item}>
+                                    <Image source={arrowLeftImagePath} style={styles.backIcon} />
+                                    <Text style={[styles.titleText, textColor]}>{t('global:backLowercase')}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default translate(['settings', 'global'])(SecuritySettings);
