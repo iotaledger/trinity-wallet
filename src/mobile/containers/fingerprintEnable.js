0dc0e3bddf0f075ec95987c371ac0d85ea9c5486
Rajiv Shah <rajivshah1@icloud.com>
2018-02-02 15:37:20 -0500

    Disable AppState listeners for now

--- src/mobile/containers/fingerprintEnable.js
+++ src/mobile/containers/fingerprintEnable.js
@@ -83,7 +83,7 @@ class FingerprintEnable extends Component {
             this.goBack();
             return true;
         });
-        AppState.addEventListener('change', this.handleAppStateChange);
+        //    AppState.addEventListener('change', this.handleAppStateChange);
         this.activateFingerPrintScanner();
     }
     componentWillReceiveProps(newProps) {
