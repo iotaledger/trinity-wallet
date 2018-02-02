ae6d63faefd744d4e78b59ac00829a97f8accf9f
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-02-02 00:05:25 +0000

    Remove need to enter password third time during onboarding + general clean up

--- src/mobile/containers/initialLoading.js
+++ src/mobile/containers/initialLoading.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { AsyncStorage, StyleSheet, View, Text, BackHandler } from 'react-native';
+import { StyleSheet, View, Text, BackHandler } from 'react-native';
 import { getVersion, getBuildNumber } from 'react-native-device-info';
 import LottieView from 'lottie-react-native';
-import DynamicStatusBar from '../components/dynamicStatusBar';
+import i18next from 'i18next';
+import { getLocaleFromLabel } from 'iota-wallet-shared-modules/libs/i18n';
 import whiteWelcomeAnimation from 'iota-wallet-shared-modules/animations/welcome-white.json';
 import blackWelcomeAnimation from 'iota-wallet-shared-modules/animations/welcome-black.json';
+import DynamicStatusBar from '../components/dynamicStatusBar';
 import keychain from '../util/keychain';
 import { width, height } from '../util/dimensions';
 import { isIOS } from '../util/device';
 import THEMES from '../theme/themes';
-import i18next from 'i18next';
-import { getLocaleFromLabel } from 'iota-wallet-shared-modules/libs/i18n';
 
 const version = getVersion();
 const build = getBuildNumber();
 
 const FULL_VERSION = `v ${version}  (${build})`;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logoContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    text: {
+        backgroundColor: 'transparent',
+        fontFamily: 'Lato-Regular',
+        fontSize: width / 33.75,
+    },
+    textContainer: {
+        justifyContent: 'flex-end',
+        paddingBottom: height / 15,
+    },
+    logo: {
+        width: width / 4,
+        height: width / 4,
+    },
+    animation: {
+        justifyContent: 'center',
+        width: width * 1.5,
+        height: width / 1.77 * 1.5,
+    },
+    animationContainer: {
+        paddingTop: height / 40,
+    },
+});
 
 class InitialLoading extends Component {
     static propTypes = {
@@ -33,6 +67,12 @@ class InitialLoading extends Component {
         Text.defaultProps.allowFontScaling = false;
     }
 
+    componentWillMount() {
+        const { language } = this.props;
+        i18next.changeLanguage(getLocaleFromLabel(language));
+        BackHandler.removeEventListener('backPress');
+    }
+
     componentDidMount() {
         this.animation.play();
         this.timeout = setTimeout(this.onLoaded.bind(this), 2000);
@@ -42,11 +82,7 @@ class InitialLoading extends Component {
             return true;
         });
     }
-    componentWillMount() {
-        const { language } = this.props;
-        i18next.changeLanguage(getLocaleFromLabel(language));
-        BackHandler.removeEventListener('backPress');
-    }
+
     onLoaded() {
         if (!this.props.onboardingComplete) {
             this.clearKeychain();
@@ -106,40 +142,6 @@ class InitialLoading extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logoContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-    },
-    text: {
-        backgroundColor: 'transparent',
-        fontFamily: 'Lato-Regular',
-        fontSize: width / 33.75,
-    },
-    textContainer: {
-        justifyContent: 'flex-end',
-        paddingBottom: height / 15,
-    },
-    logo: {
-        width: width / 4,
-        height: width / 4,
-    },
-    animation: {
-        justifyContent: 'center',
-        width: width * 1.5,
-        height: width / 1.77 * 1.5,
-    },
-    animationContainer: {
-        paddingTop: height / 40,
-    },
-});
 
 const mapStateToProps = state => ({
     onboardingComplete: state.account.onboardingComplete,
