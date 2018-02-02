e2591b23bc0941f48d5c4a96d9b11c176ffc2eb3
iotadance <merch.iota.dance@gmail.com>
2018-02-02 14:37:55 +0200

    Node settings component initial implementation

--- src/desktop/src/components/UI/input/Select.js
+++ src/desktop/src/components/UI/input/Select.js
@@ -5,15 +5,16 @@ import css from './Input.css';
 export default class Select extends React.PureComponent {
     static propTypes = {
         children: PropTypes.node,
+        disabled: PropTypes.bool,
         label: PropTypes.string,
     };
 
     render() {
-        const { children, label } = this.props;
+        const { children, label, disabled } = this.props;
 
         return (
             <div className={css.input}>
-                <fieldset>
+                <fieldset disabled={disabled}>
                     <a className={css.down}>&shy;</a>
                     <select {...this.props}>{children}</select>
                     <small>{label}</small>
