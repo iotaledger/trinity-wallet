5e99fe6c1635412c95f1e4ad8ddb65fa25ea619f
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-06 02:24:43 +0500

    Update keychain data structure

--- src/mobile/util/keychain.js
+++ src/mobile/util/keychain.js
@@ -1,7 +1,9 @@
+import assign from 'lodash/assign';
 import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
 import some from 'lodash/some';
 import map from 'lodash/map';
+import isString from 'lodash/isString';
 import filter from 'lodash/filter';
 import { serialize, parse } from '../../shared/libs/util';
 import * as Keychain from 'react-native-keychain';
@@ -44,36 +46,64 @@ const keychain = {
 
 export const storeSeedInKeychain = async (password, seed, name) => {
     const data = await keychain.get();
-    const info = { seed, name };
+    const info = { accounts: [{ seed, name }], shared: { twoFactorAuthKey: null } };
 
     if (isEmpty(data)) {
-        const seedArray = [info];
-        return keychain.set(password, serialize(seedArray));
+        return keychain.set(password, serialize(info));
     }
 
-    const existingSeedArray = parse(get(data, 'data'));
-    const updatedKeychainInfo = [...existingSeedArray, info]; // Might need to check type
+    const existingInfo = parse(get(data, 'data'));
+    const updatedKeychainInfo = assign({}, existingInfo, {
+        accounts: [...get(existingInfo, 'accounts'), { seed, name }],
+    });
+
     return keychain.set(password, serialize(updatedKeychainInfo));
+};
+
+export const storeTwoFactorAuthKeyInKeychain = async key => {
+    const info = await keychain.get();
+
+    const { password, data } = info;
+
+    // Should only allow storing two factor authentication key
+    // if a user has an account and is logged in.
+    const shouldNotAllow = !password || !data;
+
+    if (!isString(key)) {
+        throw new Error('Invalid two factor authentication key.');
+    } else if (shouldNotAllow) {
+        throw new Error('Cannot store two factor authentication key. No account information found in keychain.');
+    }
+
+    const parsed = parse(data);
+    const withTwoFactorAuthKey = assign({}, parsed, {
+        shared: {
+            twoFactorAuthKey: key,
+        },
+    });
+
+    return keychain.set(password, serialize(withTwoFactorAuthKey));
 };
 
 export const hasDuplicateAccountName = (data, name) => {
     const parsed = parse(data);
     const hasDuplicate = d => get(d, 'name') === name;
 
-    return some(parsed, hasDuplicate);
+    return some(get(parsed, 'accounts'), hasDuplicate);
 };
 
 export const hasDuplicateSeed = (data, seed) => {
     const parsed = parse(data);
     const hasDuplicate = d => get(d, 'seed') === seed;
 
-    return some(parsed, hasDuplicate);
+    return some(get(parsed, 'accounts'), hasDuplicate);
 };
 
 export const getSeed = (value, index) => {
     const parsed = parse(value);
+    const accounts = get(parsed, 'accounts');
 
-    return parsed[index] ? parsed[index].seed : '';
+    return accounts[index] ? accounts[index].seed : '';
 };
 
 export const updateAccountNameInKeychain = async (replaceAtIndex, newAccountName, password) => {
@@ -93,8 +123,16 @@ export const updateAccountNameInKeychain = async (replaceAtIndex, newAccountName
             return d;
         };
 
-        const updatedAccountInfo = map(parsed, replace);
-        return keychain.set(password, serialize(updatedAccountInfo));
+        const updatedAccountInfo = map(get(parsed, 'accounts'), replace);
+
+        return keychain.set(
+            password,
+            serialize(
+                assign({}, accountInformation, {
+                    accounts: updatedAccountInfo,
+                }),
+            ),
+        );
     }
 
     return Promise.reject(new Error('Something went wrong while updating account name.'));
@@ -107,9 +145,16 @@ export const deleteFromKeychain = async (deleteFromIndex, password) => {
     if (accountInformation) {
         const parsed = parse(accountInformation);
         const remove = (d, i) => i !== deleteFromIndex;
-        const updatedAccountInfo = filter(parsed, remove);
+        const updatedAccountInfo = filter(get(parsed, 'accounts'), remove);
 
-        return keychain.set(password, serialize(updatedAccountInfo));
+        return keychain.set(
+            password,
+            serialize(
+                assign({}, updatedAccountInfo, {
+                    accounts: updatedAccountInfo,
+                }),
+            ),
+        );
     }
 
     return Promise.reject(new Error('Something went wrong while deleting from keychain.'));
