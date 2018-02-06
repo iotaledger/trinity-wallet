064a308fbcc962129820feb8327d3b482ee0402a
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-06 05:26:38 +0500

    Add coverage for storeTwoFactorAuthKeyInKeychain

--- src/mobile/__tests__/util/keychain.spec.js
+++ src/mobile/__tests__/util/keychain.spec.js
@@ -1,11 +1,10 @@
-import assign from 'lodash/assign';
-import noop from 'lodash/noop';
-import each from 'lodash/each';
 import React from 'react';
 import keychain from 'react-native-keychain';
-import keychainWrapper, { storeSeedInKeychain } from '../../util/keychain';
-import { shallow } from 'enzyme';
-import { Poll } from '../../containers/poll';
+import keychainWrapper, {
+    storeSeedInKeychain,
+    storeTwoFactorAuthKeyInKeychain,
+    getTwoFactorAuthKeyFromKeychain,
+} from '../../util/keychain';
 
 jest.mock('react-native-keychain', () => ({
     setGenericPassword: jest.fn((username, password) => Promise.resolve({})),
@@ -57,6 +56,11 @@ describe('Testing keychain util', () => {
     });
 
     describe('#storeSeedInKeychain', () => {
+        afterEach(() => {
+            keychain.setGenericPassword.mockClear();
+            keychain.getGenericPassword.mockClear();
+        });
+
         describe('when data prop is empty', () => {
             it('should call setGenericPassword with first argument and info consisting of seed and name', () => {
                 return storeSeedInKeychain('foo', 'SEED', 'account').then(() => {
@@ -65,6 +69,99 @@ describe('Testing keychain util', () => {
                         JSON.stringify({
                             accounts: [{ seed: 'SEED', name: 'account' }],
                             shared: { twoFactorAuthKey: null },
+                        }),
+                    );
+                });
+            });
+        });
+
+        describe('when data prop is not empty', () => {
+            it('should call setGenericPassword with first argument and accounts array updated with info consisting of seed and name', () => {
+                keychain.getGenericPassword.mockImplementation(() =>
+                    Promise.resolve({
+                        username: '',
+                        password: {
+                            accounts: [{ seed: 'FOO', name: 'ACCOUNT_ONE' }],
+                            shared: { twoFactorAuthKey: null },
+                        },
+                        service: 'bundleId',
+                    }),
+                );
+
+                return storeSeedInKeychain('foo', 'SEED', 'new account').then(() => {
+                    expect(keychain.setGenericPassword).toHaveBeenCalledWith(
+                        'foo',
+                        JSON.stringify({
+                            accounts: [{ seed: 'FOO', name: 'ACCOUNT_ONE' }, { seed: 'SEED', name: 'new account' }],
+                            shared: { twoFactorAuthKey: null },
+                        }),
+                    );
+                });
+            });
+        });
+    });
+
+    describe('#storeTwoFactorAuthKeyInKeychain', () => {
+        afterEach(() => {
+            keychain.setGenericPassword.mockClear();
+            keychain.getGenericPassword.mockClear();
+        });
+
+        describe('when key passed as an argument is not a string', () => {
+            it('should throw with error "Invalid two factor authentication key."', () => {
+                return storeTwoFactorAuthKeyInKeychain(null).catch(err => {
+                    expect(err.message).toEqual('Invalid two factor authentication key.');
+                });
+            });
+        });
+
+        describe('when password prop from getGenericPassword is empty', () => {
+            it('should throw with error "Cannot store two factor authentication key. No account information found in keychain."', () => {
+                return storeTwoFactorAuthKeyInKeychain('foo').catch(err => {
+                    expect(err.message).toEqual(
+                        'Cannot store two factor authentication key. No account information found in keychain.',
+                    );
+                });
+            });
+        });
+
+        describe('when data prop from getGenericPassword is empty', () => {
+            keychain.getGenericPassword.mockImplementation(() =>
+                Promise.resolve({
+                    username: 'foo', // password -> username
+                    password: '', // data -> password
+                    service: 'bundleId',
+                }),
+            );
+
+            it('should throw with error "Cannot store two factor authentication key. No account information found in keychain."', () => {
+                return storeTwoFactorAuthKeyInKeychain('foo').catch(err => {
+                    expect(err.message).toEqual(
+                        'Cannot store two factor authentication key. No account information found in keychain.',
+                    );
+                });
+            });
+        });
+
+        describe('when data and password props are not empty and key is a string', () => {
+            it('should assign twoFactorAuthKey to argument', () => {
+                keychain.getGenericPassword.mockImplementation(() =>
+                    Promise.resolve({
+                        username: 'baz',
+                        password: {
+                            accounts: [{ seed: 'FOO', name: 'ACCOUNT_ONE' }],
+                            shared: { twoFactorAuthKey: null },
+                        },
+                        service: 'bundleId',
+                    }),
+                );
+
+                return storeTwoFactorAuthKeyInKeychain('foo').then(() => {
+                    expect(keychain.setGenericPassword).toHaveBeenCalledWith(
+                        'baz',
+                        JSON.stringify({
+                            accounts: [{ seed: 'FOO', name: 'ACCOUNT_ONE' }],
+                            shared: { twoFactorAuthKey: 'foo' },
                         }),
                     );
                 });
