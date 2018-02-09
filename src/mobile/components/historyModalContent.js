166c90168c11d7546477686280dd1eb1280ee70f
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-09 14:41:08 +0500

    Make sure styles are declared in stylesheet

--- src/mobile/components/historyModalContent.js
+++ src/mobile/components/historyModalContent.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { TouchableOpacity, View, Text, StyleSheet, FlatList, ScrollView } from 'react-native';
+import { formatModalTime, convertUnixTimeToJSDate } from 'iota-wallet-shared-modules/libs/dateUtils';
 import GENERAL from '../theme/general';
 import { width, height } from '../util/dimensions';
 
@@ -34,7 +35,7 @@ const styles = StyleSheet.create({
         fontSize: width / 29.6,
     },
     valueText: {
-        marginLeft: 8,
+        marginLeft: 2,
     },
     confirmationWrapper: {
         flexDirection: 'row',
@@ -43,6 +44,18 @@ const styles = StyleSheet.create({
     bundleWrapper: {
         flexDirection: 'row',
         alignItems: 'center',
+    },
+    bundleInnerWrapper: {
+        flex: 7,
+    },
+    bundleHash: {
+        backgroundColor: 'transparent',
+        fontFamily: 'Inconsolata-Regular',
+        fontSize: width / 31.8,
+        marginTop: 2,
+    },
+    bundleSeparator: {
+        flex: 1,
     },
     confirmation: {
         backgroundColor: 'transparent',
@@ -55,16 +68,33 @@ const styles = StyleSheet.create({
         fontFamily: 'Lato-Regular',
         fontSize: width / 31.8,
     },
-    bundleTitle: {
+    heading: {
         backgroundColor: 'transparent',
         fontFamily: 'Lato-Bold',
         fontSize: width / 31.8,
         paddingTop: height / 50,
     },
+    text: {
+        backgroundColor: 'transparent',
+        fontFamily: 'Inconsolata-Regular',
+        fontSize: width / 31.8,
+    },
     addressRowContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         paddingVertical: 2,
+    },
+    addressRowTopWrapper: {
+        flex: 4.7,
+    },
+    addressRowBottomWrapper: {
+        flex: 1.3,
+    },
+    addressRowValue: {
+        backgroundColor: 'transparent',
+        fontFamily: 'Lato-Bold',
+        fontSize: width / 27.6,
+        textAlign: 'right',
     },
 });
 
@@ -97,14 +127,13 @@ export default class HistoryModalContent extends PureComponent {
 
         return (
             <View style={styles.addressRowContainer}>
-                <TouchableOpacity onPress={() => {}} style={{ flex: 4.7 }}>
-                    <Text style={[styles.hash, style.defaultTextColor]} numberOfLines={2}>
+                <TouchableOpacity onPress={() => {}} style={styles.addressRowTopWrapper}>
+                    <Text style={[styles.text, style.defaultTextColor]} numberOfLines={2}>
                         {address}
                     </Text>
                 </TouchableOpacity>
-                <View style={{ flex: 1.3 }}>
-                    <Text style={[styles.modalValue, style.defaultTextColor]} numberOfLines={1}>
-                        {' '}
+                <View style={styles.addressRowBottomWrapper}>
+                    <Text style={[styles.addressRowValue, style.defaultTextColor]} numberOfLines={1}>
                         {value} {unit}
                     </Text>
                 </View>
@@ -134,30 +163,36 @@ export default class HistoryModalContent extends PureComponent {
                     <View style={[styles.content, style.borderColor, { backgroundColor: style.backgroundColor }]}>
                         <ScrollView>
                             <View style={styles.statusWrapper}>
-                                <Text style={[styles.statusText, { color: style.titleColor }]}>{status}</Text>
-                                <Text style={[styles.statusText, styles.valueText, { color: style.titleColor }]}>
-                                    {value} {unit}
+                                <Text style={[styles.statusText, { color: style.titleColor }]}>
+                                    <Text>
+                                        {status} {value} {unit}
+                                    </Text>
                                 </Text>
                                 <View style={styles.confirmationWrapper}>
                                     <Text style={[styles.confirmation, style.confirmationStatusColor]}>
                                         {confirmation}
                                     </Text>
-                                    <Text style={[styles.timestamp, style.defaultTextColor]}>{time}</Text>
+                                    <Text style={[styles.timestamp, style.defaultTextColor]}>
+                                        {formatModalTime(convertUnixTimeToJSDate(time))}
+                                    </Text>
                                 </View>
                             </View>
-                            <Text style={[styles.bundleTitle, style.defaultTextColor]}>{t('bundleHash')}:</Text>
+                            <Text style={[styles.heading, style.defaultTextColor]}>{t('bundleHash')}:</Text>
                             <View style={styles.bundleWrapper}>
-                                <TouchableOpacity onPress={() => console.log('Copied')} style={{ flex: 7 }}>
+                                <TouchableOpacity
+                                    onPress={() => console.log('Copied')}
+                                    style={styles.bundleInnerWrapper}
+                                >
                                     <Text style={[styles.bundleHash, style.defaultTextColor]} numberOfLines={2}>
                                         {bundle}
                                     </Text>
-                                    <View style={{ flex: 1 }} />
+                                    <View style={styles.bundleSeparator} />
                                 </TouchableOpacity>
                             </View>
-                            <Text style={[styles.bundleTitle, style.defaultTextColor]}>{t('addresses')}:</Text>
+                            <Text style={[styles.heading, style.defaultTextColor]}>{t('addresses')}:</Text>
                             {this.renderAddresses(addresses)}
-                            <Text style={[styles.bundleTitle, style.defaultTextColor]}>{t('send:message')}:</Text>
-                            <Text style={[styles.hash, style.defaultTextColor]}>{message}</Text>
+                            <Text style={[styles.heading, style.defaultTextColor]}>{t('send:message')}:</Text>
+                            <Text style={[styles.text, style.defaultTextColor]}>{message}</Text>
                         </ScrollView>
                     </View>
                 </View>
