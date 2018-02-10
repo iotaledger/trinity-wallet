8d040b1ab5174f89fe0dd5e6d4d439d6c1dd4a35
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-10 19:41:45 +0500

    Update coverage for viewAddresses component

--- src/mobile/__tests__/containers/viewAddresses.spec.js
+++ src/mobile/__tests__/containers/viewAddresses.spec.js
@@ -1,6 +1,7 @@
 import assign from 'lodash/assign';
 import noop from 'lodash/noop';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Clipboard } from 'react-native';
 import { shallow } from 'enzyme';
 import { ViewAddresses } from '../../containers/viewAddresses';
@@ -20,6 +21,32 @@ const getProps = overrides =>
     );
 
 describe('Testing ViewAddresses component', () => {
+    describe('propTypes', () => {
+        it('should require an addressData object as a prop', () => {
+            expect(ViewAddresses.propTypes.addressData).toEqual(PropTypes.object.isRequired);
+        });
+
+        it('should require a secondaryBackgroundColor string as a prop', () => {
+            expect(ViewAddresses.propTypes.secondaryBackgroundColor).toEqual(PropTypes.string.isRequired);
+        });
+
+        it('should require an arrowLeftImagePath number as a prop', () => {
+            expect(ViewAddresses.propTypes.arrowLeftImagePath).toEqual(PropTypes.number.isRequired);
+        });
+
+        it('should require a t function as a prop', () => {
+            expect(ViewAddresses.propTypes.t).toEqual(PropTypes.func.isRequired);
+        });
+
+        it('should require a backPress function as a prop', () => {
+            expect(ViewAddresses.propTypes.backPress).toEqual(PropTypes.func.isRequired);
+        });
+
+        it('should require a generateAlert function as a prop', () => {
+            expect(ViewAddresses.propTypes.generateAlert).toEqual(PropTypes.func.isRequired);
+        });
+    });
+
     describe('when renders', () => {
         it('should not explode', () => {
             const props = getProps();
@@ -50,6 +77,90 @@ describe('Testing ViewAddresses component', () => {
 
     describe('instance methods', () => {
         describe('when called', () => {
+            describe('#prepAddresses', () => {
+                it('should always return an array', () => {
+                    const props = getProps();
+
+                    const wrapper = shallow(<ViewAddresses {...props} />);
+
+                    const instance = wrapper.instance();
+                    let returnValue = instance.prepAddresses();
+
+                    // When addresses are an empty object
+                    expect(Array.isArray(returnValue)).toEqual(true);
+
+                    wrapper.setProps({ addressData: { ['U'.repeat(81)]: {} } });
+
+                    returnValue = instance.prepAddresses();
+
+                    expect(Array.isArray(returnValue)).toEqual(true);
+                });
+
+                it('should have atleast "balance", "unit" and "address" props in each item of the returned array', () => {
+                    const props = getProps({
+                        addressData: {
+                            ['U'.repeat(81)]: {},
+                        },
+                    });
+
+                    const wrapper = shallow(<ViewAddresses {...props} />);
+
+                    const instance = wrapper.instance();
+                    const returnValue = instance.prepAddresses();
+
+                    expect(returnValue.length).toEqual(1);
+
+                    const head = returnValue[0];
+
+                    expect('balance' in head).toEqual(true);
+                    expect('unit' in head).toEqual(true);
+                    expect('address' in head).toEqual(true);
+                });
+
+                it('should assign checksum to address prop in each item of the returned array', () => {
+                    const fakeAddress = 'U'.repeat(81);
+                    const props = getProps({
+                        addressData: {
+                            [fakeAddress]: {},
+                        },
+                    });
+
+                    const wrapper = shallow(<ViewAddresses {...props} />);
+
+                    const instance = wrapper.instance();
+                    const returnValue = instance.prepAddresses();
+
+                    expect(returnValue.length).toEqual(1);
+
+                    const head = returnValue[0];
+
+                    const checksum = 'NXELTUENX';
+                    const fakeAddressWithChecksum = `${fakeAddress}${checksum}`;
+                    expect(head.address).toEqual(fakeAddressWithChecksum);
+                });
+
+                it('should return an ordered array in descending by index prop', () => {
+                    const props = getProps({
+                        addressData: {
+                            ['U'.repeat(81)]: { index: 0 },
+                            ['A'.repeat(81)]: { index: 3 },
+                            ['B'.repeat(81)]: { index: 32 },
+                        },
+                    });
+
+                    const wrapper = shallow(<ViewAddresses {...props} />);
+
+                    const instance = wrapper.instance();
+                    const returnValue = instance.prepAddresses();
+
+                    expect(returnValue[0].index).not.toEqual(0);
+                    expect(returnValue[0].index).toEqual(32);
+
+                    expect(returnValue[1].index).toEqual(3);
+                    expect(returnValue[2].index).toEqual(0);
+                });
+            });
+
             describe('#copy', () => {
                 afterEach(() => {
                     if (Clipboard.setString.mockClear) {
