93854fae4661a7d6319c9ac103034806655b5655
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-11 11:03:59 +0500

    Add coverage for prepTransfers in balance component

--- src/mobile/__tests__/containers/balance.spec.js
+++ src/mobile/__tests__/containers/balance.spec.js
@@ -1,4 +1,5 @@
 import assign from 'lodash/assign';
+import map from 'lodash/map';
 import noop from 'lodash/noop';
 import React from 'react';
 import { FlatList } from 'react-native';
@@ -26,7 +27,16 @@ const getProps = overrides =>
             secondaryBackgroundColor: 'white',
             chartLineColorPrimary: 'white',
             chartLineColorSecondary: 'white',
-            t: noop,
+            t: arg => {
+                const translations = {
+                    received: 'Received',
+                    receiving: 'Receiving',
+                    sent: 'Sent',
+                    sending: 'Sending',
+                };
+
+                return translations[arg] ? translations[arg] : 'foo';
+            },
             closeTopBar: noop,
         },
         overrides,
@@ -160,6 +170,135 @@ describe('Testing Balance component', () => {
                         'ItemSeparatorComponent',
                         'ListEmptyComponent',
                     ].forEach(prop => expect(passedProps.includes(prop)).toEqual(true));
+                });
+            });
+
+            describe('#prepTransactions', () => {
+                let transfers;
+
+                beforeEach(() => {
+                    transfers = [
+                        [
+                            {
+                                currentIndex: 0,
+                                lastIndex: 3,
+                                timestamp: 1518327592290,
+                                value: 1,
+                                persistence: true,
+                                address: 'U'.repeat(81),
+                            },
+                            {
+                                currentIndex: 1,
+                                lastIndex: 3,
+                                timestamp: 1518327592290,
+                                value: 1,
+                                persistence: true,
+                                address: 'A'.repeat(81),
+                            },
+                            {
+                                currentIndex: 2,
+                                lastIndex: 3,
+                                timestamp: 1518327592290,
+                                value: 1,
+                                persistence: true,
+                                address: 'B'.repeat(81),
+                            },
+                            {
+                                currentIndex: 3,
+                                lastIndex: 3,
+                                timestamp: 1518327592290,
+                                value: 1,
+                                persistence: true,
+                                address: 'C'.repeat(81),
+                            },
+                        ],
+                    ];
+                });
+
+                it('should always return an array', () => {
+                    const props = getProps();
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValue = instance.prepTransactions();
+
+                    expect(Array.isArray(returnValue)).toEqual(true);
+                });
+
+                it('should have "time", "confirmationStatus", "value", "unit", "sign", "iconPath" and "style" props in each item in array', () => {
+                    const props = getProps({ transfers });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    ['time', 'confirmationStatus', 'value', 'unit', 'sign', 'iconPath', 'style'].forEach(prop =>
+                        expect(Object.keys(returnValueHead).includes(prop)).toEqual(true),
+                    );
+
+                    expect(Object.keys(returnValueHead).length).toEqual(7);
+                });
+
+                it('should have time prop equals timestamp prop in transfers object', () => {
+                    const props = getProps({ transfers });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    expect(returnValueHead.time).toEqual(1518327592290);
+                });
+
+                it('should have confirmationStatus prop equals "Received" if value prop is greater than one, addresses are part of transfers object and persistence is true', () => {
+                    const props = getProps({
+                        transfers,
+                        addresses: ['U'.repeat(81), 'A'.repeat(81), 'B'.repeat(81), 'C'.repeat(81)],
+                    });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    // Since addresses are part of transfers mock object and value > 0
+                    expect(returnValueHead.confirmationStatus).toEqual('Received');
+                });
+
+                it('should have confirmationStatus prop equals "Receiving" if value prop is greater than one, addresses are part of transfers object and persistence is false', () => {
+                    transfers = map(transfers, tx => map(tx, t => ({ ...t, persistence: false })));
+
+                    const props = getProps({
+                        transfers,
+                        addresses: ['U'.repeat(81), 'A'.repeat(81), 'B'.repeat(81), 'C'.repeat(81)],
+                    });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    expect(returnValueHead.confirmationStatus).toEqual('Receiving');
+                });
+
+                it('should have confirmationStatus prop equals "Sent" if value prop is less than one, addresses are part of transfers object and persistence is true', () => {
+                    transfers = map(transfers, tx => map(tx, t => ({ ...t, value: -1 })));
+
+                    const props = getProps({
+                        transfers,
+                        addresses: ['U'.repeat(81), 'A'.repeat(81), 'B'.repeat(81), 'C'.repeat(81)],
+                    });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    expect(returnValueHead.confirmationStatus).toEqual('Sent');
+                });
+
+                it('should have confirmationStatus prop equals "Sending" if value prop is less than one, addresses are part of transfers object and persistence is false', () => {
+                    transfers = map(transfers, tx => map(tx, t => ({ ...t, value: -1, persistence: false })));
+
+                    const props = getProps({
+                        transfers,
+                        addresses: ['U'.repeat(81), 'A'.repeat(81), 'B'.repeat(81), 'C'.repeat(81)],
+                    });
+
+                    const instance = shallow(<Balance {...props} />).instance();
+                    const returnValueHead = instance.prepTransactions()[0];
+
+                    expect(returnValueHead.confirmationStatus).toEqual('Sending');
                 });
             });
         });
