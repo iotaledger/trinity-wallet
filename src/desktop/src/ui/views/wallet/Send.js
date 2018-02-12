29de26c70a44f3773638495264c9f0ecaa07b9a9
iotadance <merch.iota.dance@gmail.com>
2018-02-12 19:36:44 +0200

    Send transaction container component

--- src/desktop/src/ui/views/wallet/Send.js
+++ src/desktop/src/ui/views/wallet/Send.js
@@ -1,181 +1,139 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from 'react-i18next';
-import { connect } from 'react-redux';
-import { VALID_SEED_REGEX, ADDRESS_LENGTH, formatValue, formatUnit } from 'libs/util';
-import { sendTransferRequest } from 'actions/tempAccount';
-import { iota } from 'libs/iota';
-import { showError } from 'actions/notifications';
-import List from 'ui/components/List';
+import { formatValue, formatUnit } from 'libs/util';
+
 import AddressInput from 'ui/components/input/Address';
 import AmountInput from 'ui/components/input/Amount';
 import MessageInput from 'ui/components/input/Message';
+import List from 'ui/components/List';
 import Button from 'ui/components/Button';
 import Modal from 'ui/components/modal/Modal';
-import { runTask } from 'worker';
+
+import withSendData from 'containers/wallet/Send';
 
 import css from './send.css';
 
+/**
+ * Send transaction component
+ */
 class Send extends React.PureComponent {
     static propTypes = {
-        t: PropTypes.func.isRequired,
-        account: PropTypes.object.isRequired,
-        tempAccount: PropTypes.object.isRequired,
+        /* Current send status */
+        isSending: PropTypes.bool.isRequired,
+        /* Current seed state value */
         seeds: PropTypes.object.isRequired,
-        settings: PropTypes.object.isRequired,
-        showError: PropTypes.func.isRequired,
-        sendTransferRequest: PropTypes.func.isRequired,
+        /* Total current account wallet ballance in iotas */
+        balance: PropTypes.number.isRequired,
+        /* Validate the transaction inputs
+         *  @param {string} address - receiver address 
+         *  @param {number} value - transaction value in iotas
+         */
+        /* Fiat currency settings
+         * @property {string} conversionRate - Active currency conversion rate to MIota
+         * @property (string) currency - Active currency name
+         */
+        settings: PropTypes.shape({
+            conversionRate: PropTypes.number.isRequired,
+            currency: PropTypes.string.isRequired,
+        }),
+        validateInputs: PropTypes.func.isRequired,
+        /* Send the transaction
+         *  @param {string} seed - seed to be used for the transaction signing
+         *  @param {string} address - receiver address 
+         *  @param {number} value - transaction value in iotas
+         *  @param {string} message - transaction message
+         */
+        sendTransfer: PropTypes.func.isRequired,
+        /* Translation helper
+         * @param {string} translationString - locale string identifier to be translated
+         * @ignore
+         */
+        t: PropTypes.func.isRequired,
     };
 
     state = {
         address: '',
-        amount: '0',
+        amount: 0,
         message: '',
         isModalVisible: false,
     };
 
-    onAddressChange = (value) => {
-        this.setState(() => ({
-            address: value.replace(/[^a-zA-Z9]*/g, '').toUpperCase(),
-        }));
-    };
-
-    onAmountChange = (value) => {
-        this.setState(() => ({
-            amount: value,
-        }));
-    };
-
-    onMessageChange = (value) => {
-        this.setState(() => ({
-            message: value,
-        }));
-    };
-
-    send = () => {
+    validateInputs = (e) => {
         const { address, amount } = this.state;
-        const { showError, account } = this.props;
+        const { validateInputs } = this.props;
 
-        if (address.length !== ADDRESS_LENGTH) {
-            return showError({
-                title: 'send:invalidAddress',
-                text: 'send:invalidAddressExplanation1',
-                translate: true,
-            });
-        }
+        e.preventDefault();
 
-        if (!address.match(VALID_SEED_REGEX)) {
-            return showError({
-                title: 'send:invalidAddress',
-                text: 'send:invalidAddressExplanation2',
-                translate: true,
-            });
-        }
-
-        if (!iota.utils.isValidChecksum(address)) {
-            return showError({
-                title: 'send:invalidAddress',
-                text: 'send:invalidAddressExplanation3',
-                translate: true,
-            });
-        }
-
-        if (parseFloat(amount) > account.balance) {
-            return showError({
-                title: 'send:notEnoughFunds',
-                text: 'send:notEnoughFundsExplanation',
-                translate: true,
-            });
-        }
-
-        this.toggleConfirmation();
-    };
-
-    toggleConfirmation = () => {
         this.setState({
-            isModalVisible: !this.state.isModalVisible,
+            isModalVisible: validateInputs(address, amount),
         });
     };
 
-    sendTransfer = () => {
+    confirmTransfer = () => {
         const { address, amount, message } = this.state;
-        const { sendTransferRequest, seeds, account } = this.props;
+        const { seeds, sendTransfer } = this.props;
 
-        this.toggleConfirmation();
-        sendTransferRequest();
+        this.setState({
+            isModalVisible: false,
+        });
 
         const seedInfo = seeds.items[seeds.selectedSeedIndex];
-        const accountInfo = account.accountInfo[seedInfo.name];
 
-        runTask('sendTransaction', [seedInfo.seed, accountInfo, seedInfo.name, address, parseInt(amount), message]);
+        sendTransfer(seedInfo.seed, address, amount, message);
     };
 
     render() {
-        const { t, settings, account, seeds, tempAccount } = this.props;
+        const { isSending, balance, settings, t } = this.props;
         const { address, amount, message, isModalVisible } = this.state;
-
-        const seedInfo = seeds.items[seeds.selectedSeedIndex];
-        const accountInfo = account.accountInfo[seedInfo.name];
 
         return (
             <main>
                 <section className={css.send}>
                     <div>
-                        <Modal variant="confirm" isOpen={isModalVisible} onClose={this.toggleConfirmation}>
-                            <h1>
-                                You are about to send <strong>{`${formatValue(amount)} ${formatUnit(amount)}`}</strong>{' '}
-                                to the address: <br />
-                                <strong>{address}</strong>
-                            </h1>
-                            <Button onClick={this.toggleConfirmation} variant="secondary">
-                                {t('global:no')}
+                        <form onSubmit={this.validateInputs}>
+                            <Modal variant="confirm" isOpen={isModalVisible} onClose={this.toggleConfirmation}>
+                                <h1>
+                                    You are about to send{' '}
+                                    <strong>{`${formatValue(amount)} ${formatUnit(amount)}`}</strong> to the address:{' '}
+                                    <br />
+                                    <strong>{address}</strong>
+                                </h1>
+                                <Button onClick={() => this.setState({ isModalVisible: false })} variant="secondary">
+                                    {t('global:no')}
+                                </Button>
+                                <Button onClick={this.confirmTransfer} variant="primary">
+                                    {t('global:yes')}
+                                </Button>
+                            </Modal>
+                            <AddressInput
+                                address={address}
+                                onChange={(value) => this.setState({ address: value })}
+                                label={t('send:recipientAddress')}
+                                closeLabel={t('global:back')}
+                            />
+                            <AmountInput
+                                amount={amount}
+                                settings={settings}
+                                label={t('send:amount')}
+                                labelMax={t('send:max')}
+                                balance={balance}
+                                onChange={(value) => this.setState({ amount: value })}
+                            />
+                            <MessageInput
+                                message={message}
+                                label={t('send:message')}
+                                onChange={(value) => this.setState({ message: value })}
+                            />
+                            <Button loading={isSending} variant="primary">
+                                {t('send:send')}
                             </Button>
-                            <Button onClick={this.sendTransfer} variant="primary">
-                                {t('global:yes')}
-                            </Button>
-                        </Modal>
-                        <AddressInput
-                            address={address}
-                            onChange={this.onAddressChange}
-                            label={t('send:recipientAddress')}
-                            closeLabel={t('global:back')}
-                        />
-                        <AmountInput
-                            amount={amount}
-                            settings={settings}
-                            label={t('send:amount')}
-                            labelMax={t('send:max')}
-                            balance={accountInfo.balance}
-                            onChange={this.onAmountChange}
-                        />
-                        <MessageInput message={message} label={t('send:message')} onChange={this.onMessageChange} />
-                        <Button loading={tempAccount.isSendingTransfer} onClick={this.send} variant="primary">
-                            {t('send:send')}
-                        </Button>
+                        </form>
                     </div>
                 </section>
-                <section>
-                    <List
-                        filter="sent"
-                        transfers={accountInfo.transfers.length ? accountInfo.transfers : []}
-                        addresses={Object.keys(accountInfo.addresses)}
-                    />
-                </section>
+                <section />
             </main>
         );
     }
 }
 
-const mapStateToProps = (state) => ({
-    tempAccount: state.tempAccount,
-    settings: state.settings,
-    account: state.account,
-    seeds: state.seeds,
-});
-
-const mapDispatchToProps = {
-    showError,
-    sendTransferRequest,
-};
-
-export default translate('send')(connect(mapStateToProps, mapDispatchToProps)(Send));
+export default withSendData(Send);
