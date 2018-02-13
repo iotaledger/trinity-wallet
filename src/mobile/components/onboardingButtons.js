29ade3f8adffffdad1dad661aa2120ef1392cfce
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-02-13 23:16:42 +0000

    Add complete android back button coverage

--- src/mobile/components/onboardingButtons.js
+++ src/mobile/components/onboardingButtons.js
@@ -11,14 +11,26 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         flexDirection: 'row',
     },
-    button: {
+    leftButton: {
         borderWidth: 1.2,
         borderRadius: GENERAL.borderRadius,
         height: height / 14,
         alignItems: 'center',
         justifyContent: 'space-around',
     },
-    text: {
+    rightButton: {
+        borderWidth: 1.2,
+        borderRadius: GENERAL.borderRadius,
+        height: height / 14,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    leftText: {
+        fontFamily: 'Lato-Light',
+        fontSize: width / 24.4,
+        backgroundColor: 'transparent',
+    },
+    rightText: {
         fontFamily: 'Lato-Light',
         fontSize: width / 24.4,
         backgroundColor: 'transparent',
@@ -37,23 +49,31 @@ class OnboardingButtons extends Component {
     };
 
     render() {
-        const { style, positiveColor, negativeColor, opacity, buttonWidth, containerWidth } = this.props;
-        const positiveTextColor = { color: positiveColor };
-        const positiveBorderColor = { borderColor: positiveColor };
-        const negativeTextColor = { color: negativeColor };
-        const negativeBorderColor = { borderColor: negativeColor };
+        const {
+            style,
+            positiveColor,
+            negativeColor,
+            opacity,
+            buttonWidth,
+            containerWidth,
+            onboardingButtonsOverride,
+        } = this.props;
+        const rightTextColor = { color: positiveColor };
+        const rightBorderColor = { borderColor: positiveColor };
+        const leftTextColor = { color: negativeColor };
+        const leftBorderColor = { borderColor: negativeColor };
         const rightButtonOpacity = { opacity };
 
         return (
             <View style={[styles.buttonsContainer, containerWidth]}>
                 <TouchableOpacity onPress={() => this.props.onLeftButtonPress()} testID={this.props.leftButtonTestID}>
-                    <View style={[styles.button, buttonWidth, negativeBorderColor]}>
-                        <Text style={[styles.text, negativeTextColor]}>{this.props.leftText}</Text>
+                    <View style={[styles.leftButton, buttonWidth, leftBorderColor]}>
+                        <Text style={[styles.leftText, leftTextColor]}>{this.props.leftText}</Text>
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => this.props.onRightButtonPress()} testID={this.props.rightButtonTestID}>
-                    <View style={[styles.button, buttonWidth, positiveBorderColor, rightButtonOpacity]}>
-                        <Text style={[styles.text, positiveTextColor]}>{this.props.rightText}</Text>
+                    <View style={[styles.rightButton, buttonWidth, rightBorderColor, rightButtonOpacity]}>
+                        <Text style={[styles.rightText, rightTextColor]}>{this.props.rightText}</Text>
                     </View>
                 </TouchableOpacity>
             </View>
