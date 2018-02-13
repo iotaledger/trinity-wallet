e4cddcf9ecb2476d9cf3a92da502945cdaa96231
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-02-13 16:51:12 +0000

    Fix modal UI cut-off

--- src/mobile/components/onboardingButtons.js
+++ src/mobile/components/onboardingButtons.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import GENERAL from '../theme/general';
 import { width, height } from '../util/dimensions';
@@ -9,12 +10,10 @@ const styles = StyleSheet.create({
         alignItems: 'flex-end',
         justifyContent: 'space-between',
         flexDirection: 'row',
-        width: width / 1.2,
     },
     button: {
         borderWidth: 1.2,
         borderRadius: GENERAL.borderRadius,
-        width: width / 2.7,
         height: height / 14,
         alignItems: 'center',
         justifyContent: 'space-around',
@@ -27,8 +26,18 @@ const styles = StyleSheet.create({
 });
 
 class OnboardingButtons extends Component {
+    static propTypes = {
+        buttonWidth: PropTypes.object,
+        containerWidth: PropTypes.object,
+    };
+
+    static defaultProps = {
+        buttonWidth: { width: width / 2.7 },
+        containerWidth: { width: width / 1.2 },
+    };
+
     render() {
-        const { style, positiveColor, negativeColor, opacity } = this.props;
+        const { style, positiveColor, negativeColor, opacity, buttonWidth, containerWidth } = this.props;
         const positiveTextColor = { color: positiveColor };
         const positiveBorderColor = { borderColor: positiveColor };
         const negativeTextColor = { color: negativeColor };
@@ -36,14 +45,14 @@ class OnboardingButtons extends Component {
         const rightButtonOpacity = { opacity };
 
         return (
-            <View style={styles.buttonsContainer}>
+            <View style={[styles.buttonsContainer, containerWidth]}>
                 <TouchableOpacity onPress={() => this.props.onLeftButtonPress()} testID={this.props.leftButtonTestID}>
-                    <View style={[styles.button, negativeBorderColor]}>
+                    <View style={[styles.button, buttonWidth, negativeBorderColor]}>
                         <Text style={[styles.text, negativeTextColor]}>{this.props.leftText}</Text>
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => this.props.onRightButtonPress()} testID={this.props.rightButtonTestID}>
-                    <View style={[styles.button, positiveBorderColor, rightButtonOpacity]}>
+                    <View style={[styles.button, buttonWidth, positiveBorderColor, rightButtonOpacity]}>
                         <Text style={[styles.text, positiveTextColor]}>{this.props.rightText}</Text>
                     </View>
                 </TouchableOpacity>
