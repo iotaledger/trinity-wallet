abd0e9403856a09a9e61298802fc18542a0137d3
Rajiv Shah <rajivshah1@icloud.com>
2018-02-15 20:00:33 -0500

    Update documentation and export isRooted const

--- src/mobile/util/device.js
+++ src/mobile/util/device.js
@@ -1,8 +1,11 @@
 import { Platform } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
+import RNIsDeviceRooted from 'react-native-is-device-rooted';
 
 const device = DeviceInfo.getModel();
 
 export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
 export const isIPhoneX = device.includes('iPhone X');
+
+export const isRooted = RNIsDeviceRooted.isDeviceRooted();
