c6475cd2498fec607d369c550002e7c8747f1bac
iotadance <merch.iota.dance@gmail.com>
2018-02-26 19:38:14 +0200

    Icons update and generate script

--- src/desktop/src/ui/components/Icon.js
+++ src/desktop/src/ui/components/Icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { icons } from 'icons/icons';
+import icons from 'icons/icons';
 
 import css from './icon.css';
 
