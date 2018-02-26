52a48b1c9d0bf363a0c18ad652f21a718b0881c5
Rajiv Shah <rajivshah1@icloud.com>
2018-02-26 12:01:21 -0500

    Fix root detection

--- src/mobile/util/device.js
+++ src/mobile/util/device.js
@@ -1,19 +1,8 @@
 import { Platform } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
-import RNIsDeviceRooted from 'react-native-is-device-rooted';
 
 const device = DeviceInfo.getModel();
 
 export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
 export const isIPhoneX = device.includes('iPhone X');
-
-export const isRooted = async function() {
-    try {
-        var r = await RNIsDeviceRooted.isDeviceRooted();
-
-        return r;
-    } catch (e) {
-        console.log('Error');
-    }
-};
