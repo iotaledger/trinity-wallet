a9aa63530c98fed2816c7e0ad36a86e0fd5abdae
Rajiv Shah <rajivshah1@icloud.com>
2018-02-26 20:41:12 -0500

    Finish modal setup

--- src/mobile/components/rootDetectionModal.js
+++ src/mobile/components/rootDetectionModal.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { translate } from 'react-i18next';
+import { translate, Trans } from 'react-i18next';
 import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 import OnboardingButtons from '../components/onboardingButtons';
 import GENERAL from '../theme/general';
+import COLORS from '../theme/Colors';
 import { width, height } from '../util/dimensions';
 
 const styles = StyleSheet.create({
@@ -13,13 +14,22 @@ const styles = StyleSheet.create({
         borderRadius: GENERAL.borderRadius,
         borderWidth: 2,
         paddingVertical: height / 18,
-        width: width / 1.2,
+        width: width / 1.1,
+    },
+    warningText: {
+        backgroundColor: 'transparent',
+        fontFamily: 'Lato-Regular',
+        fontSize: width / 22,
+        textAlign: 'center',
+        color: COLORS.redLight,
+        paddingBottom: height / 30,
     },
     questionText: {
         backgroundColor: 'transparent',
         fontFamily: 'Lato-Regular',
-        fontSize: width / 27.6,
-        paddingBottom: height / 16,
+        fontSize: width / 24,
+        paddingBottom: height / 60,
+        textAlign: 'center',
     },
 });
 
@@ -30,10 +40,19 @@ class RootDetectionModal extends Component {
         return (
             <View style={{ width: width / 1.2, alignItems: 'center', backgroundColor: backgroundColor }}>
                 <View style={[styles.modalContent, borderColor]}>
-                    <Text style={[styles.questionText, textColor]}>{t('warning')}</Text>
+                    <Text style={styles.warningText}>{t('warning')}</Text>
+                    <Trans i18nKey="rootDetected">
+                        <Text style={[styles.questionText, textColor]}>
+                            Your device appears to be rooted/jailbroken.
+                        </Text>
+                        <Text style={[styles.questionText, textColor]}>
+                            This can pose a significant risk to the security of your wallet.
+                        </Text>
+                        <Text style={[styles.questionText, textColor]}>Do you wish to continue despite this risk?</Text>
+                    </Trans>
                     <OnboardingButtons
-                        onLeftButtonPress={() => this.props.hideModal()}
-                        onRightButtonPress={() => this.props.logout()}
+                        onLeftButtonPress={() => this.props.closeApp()}
+                        onRightButtonPress={() => this.props.hideModal()}
                         leftText={t('global:no')}
                         rightText={t('global:yes')}
                         buttonWidth={{ width: width / 3.2 }}
