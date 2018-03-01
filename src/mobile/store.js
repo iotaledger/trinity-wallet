13f114c76433f83af99f87d3ceb668f65daeecc1
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-03-01 17:42:16 +0000

    Fix duplicate declaration

--- src/mobile/store.js
+++ src/mobile/store.js
@@ -1,4 +1,3 @@
-import { AsyncStorage } from 'react-native';
 import get from 'lodash/get';
 import { getVersion, getBuildNumber } from 'react-native-device-info';
 import { AsyncStorage } from 'react-native';
