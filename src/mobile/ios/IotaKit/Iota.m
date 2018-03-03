047726c97460c59fdbd9cf1d36b8898dcb0fac98
Rajiv Shah <rajivshah1@icloud.com>
2018-03-03 15:15:49 -0500

    Add doPoW function

--- src/mobile/ios/IotaKit/Iota.m
+++ src/mobile/ios/IotaKit/Iota.m
@@ -3,6 +3,7 @@
 @interface RCT_EXTERN_MODULE(Iota, NSObject)
 RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location callback: (RCTResponseSenderBlock)callback);
 RCT_EXTERN_METHOD(address:(NSString *)seed index:(int)index security:(int)security checksum:(BOOL)checksum multithreaded:(BOOL)multithreaded resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(doPoW:(NSString *)trytes minWeightMagnitude:(int)minWeightMagnitude resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject);
 @end
 
 //RCT_EXTERN_METHOD(address:(NSString *)seed index:(int)index security:(int)security checksum:(BOOL)checksum);
