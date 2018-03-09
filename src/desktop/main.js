32e7fae552ae785514e27f2500c0e0596aa3825e
iotadance <merch.iota.dance@gmail.com>
2018-03-09 12:08:56 +0200

    Build bugfixes: - Enable Electron asar and iota:// file protocol - Add missing electron-settings package

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -1,10 +1,9 @@
-const { ipcMain: ipc } = require('electron');
+const { ipcMain: ipc, app, protocol } = require('electron');
 const electron = require('electron');
 const initMenu = require('./lib/Menu.js');
 const path = require('path');
 const settings = require('electron-settings');
 
-const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const devMode = process.env.NODE_ENV === 'development';
 
@@ -12,7 +11,19 @@ const windows = {
     main: null,
 };
 
+if (!devMode) {
+    protocol.registerStandardSchemes(['iota']);
+}
+
 function createWindow() {
+    protocol.registerFileProtocol('iota', (request, callback) => {
+        callback(
+            request.url
+                .replace('iota:/', app.getAppPath())
+                .split('?')[0]
+                .split('#')[0],
+        );
+    });
 
     windows.main = new BrowserWindow({
         width: 1024,
@@ -22,14 +33,14 @@ function createWindow() {
         minWidth: 440,
         minHeight: 720,
         titleBarStyle: 'hidden',
-        backgroundColor: settings.get('backgroundColor') ? settings.get('backgroundColor') : '#1A373E',
+        backgroundColor: settings.get('backgroundColor') ? settings.get('backgroundColor') : '#1a373e',
         webPreferences: {
             nodeIntegration: false,
-            preload: path.join(__dirname, 'lib/window.js'),
+            preload: path.resolve(__dirname, 'lib/Window.js'),
         },
     });
 
-    const url = devMode ? 'http://localhost:1074/' : 'file://' + __dirname + '/dist/index.html';
+    const url = devMode ? 'http://localhost:1074/' : 'iota://dist/index.html';
 
     windows.main.loadURL(url);
 
