0e9e6519c0fcd997170359aa84525049a1cece45
Umair Sarfraz <aquadestructor@icloud.com>
2018-03-11 03:03:03 +0500

    Tweak with argument naming

--- src/shared/libs/errors.js
+++ src/shared/libs/errors.js
@@ -2,5 +2,5 @@ export default {
     KEY_REUSE: 'Key reuse detected.',
     NOT_ENOUGH_BALANCE: 'Not enough balance.',
     ADDRESS_HAS_PENDING_TRANSFERS: 'Input addresses already used in a pending transfer.',
-    CANNOT_SEND_TO_OWN_ADDRESS: 'Cannot send to an own address.'
+    CANNOT_SEND_TO_OWN_ADDRESS: 'Cannot send to an own address.',
 };
