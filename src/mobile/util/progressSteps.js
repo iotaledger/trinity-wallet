ed8b27645287991626b7d061216b8716c78c9405
Umair Sarfraz <aquadestructor@icloud.com>
2018-03-10 16:36:49 +0500

    Simplify reducer composition for tracking progress

---
+++ src/mobile/util/progressSteps.js
@@ -0,0 +1,30 @@
+export default {
+    valueTransactionWithLocalPow: [
+        'Preparing inputs',
+        'Preparing transfers',
+        'Getting transactions to approve',
+        'Proof of work',
+        'Broadcasting',
+    ],
+    valueTransactionWithRemotePow: [
+        'Preparing inputs',
+        'Preparing transfers',
+        'Getting transactions to approve',
+        'Proof of work',
+        'Broadcasting',
+    ],
+    zeroValueTransactionWithLocalPow: [
+        'Preparing inputs',
+        'Preparing transfers',
+        'Getting transactions to approve',
+        'Proof of work',
+        'Broadcasting',
+    ],
+    zeroValueTransactionWithRemotePow: [
+        'Preparing inputs',
+        'Preparing transfers',
+        'Getting transactions to approve',
+        'Proof of work',
+        'Broadcasting',
+    ],
+};
