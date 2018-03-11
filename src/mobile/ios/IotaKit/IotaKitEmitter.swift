3857647b49cd67196e4a93b90447b8d54662de19
Rajiv Shah <rajivshah1@icloud.com>
2018-03-11 15:04:41 -0400

    Bump IotaKit and add multiAddress

--- src/mobile/ios/IotaKit/IotaKitEmitter.swift
+++ src/mobile/ios/IotaKit/IotaKitEmitter.swift
@@ -10,10 +10,21 @@ class Iota: RCTEventEmitter {
     let result = "\(name) \(location)"
     callback([result])
   }
+  
   @objc func address(_ seed: String, index: Int, security: Int, checksum: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    let address = IotaAPIUtils.newAddress(seed: seed, index: index, checksum: checksum, security: security, multithreaded: false)
+    let address = IotaAPIUtils.newAddress(seed: seed, index: index, checksum: checksum, security: security, multithreaded: true)
     resolve(address)
   }
+  
+  @objc func multiAddress(_ seed: String, index: Int,  total: Int, security: Int, checksum: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+    var addresses: [String] = []
+    for i in index..<index+total {
+      let address = IotaAPIUtils.newAddress(seed: seed, index: i, checksum: checksum, security: security, multithreaded: true)
+      addresses.append(address)
+    }
+    resolve(addresses)
+  }
+  
   @objc func doPoW(_ trytes: String, minWeightMagnitude: Int, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
     let result = PearlDiverLocalPoW().performPoW(trytes: trytes, minWeightMagnitude: minWeightMagnitude)
     let resultNonce = nonce(fromTrytes: result)
@@ -23,7 +34,7 @@ class Iota: RCTEventEmitter {
     let start = trytes.index(trytes.startIndex, offsetBy: 2646)
     let end = trytes.index(start, offsetBy: 27)
     let nonce = String(trytes[start ..< end])
-    return nonce!
+    return nonce
   }
   override func supportedEvents() -> [String]! {
     return ["Hello"]
@@ -35,4 +46,3 @@ class Iota: RCTEventEmitter {
     ];
   }
 }
-
