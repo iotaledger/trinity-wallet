49e6076288406cc3195ee6b7b0cf2d71554f36ba
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-03-15 15:39:22 +0000

    Update light and dark themes

--- src/shared/themes/themes/Ionic.js
+++ src/shared/themes/themes/Ionic.js
@@ -6,7 +6,7 @@ export default {
     positive: { color: '#196E56', hover: '#185B48', body: '#FFFFFF' },
     negative: { color: '#B4000F', hover: '#9B000D', body: '#FFFFFF' },
     highlight: { color: '#D78D0E', hover: '#BA7909', body: '#FFFFFF' },
-    extra: { color: '#88D4FF', hover: '#669FBF', body: '#1E1E1E' },
+    extra: { color: '#008CE0', hover: '#669FBF', body: '#1E1E1E' },
     label: { color: '#000000', hover: '#8C31A4' },
     input: { color: '#1E1E1E', bg: '#FFFFFF', alt: '#727272' },
     inputOptional: { color: '#1E1E1E', bg: '#FFFFFF', alt: '#727272' },
