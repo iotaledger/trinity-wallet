552c80ba9fc89061eef0f5102a6ab70d5b68aa79
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-03-16 17:03:27 +0000

    Update themes

--- src/shared/themes/themes/Dark.js
+++ src/shared/themes/themes/Dark.js
@@ -2,7 +2,7 @@ export default {
     body: { color: '#FFFFFF', bg: '#000000', alt: '#000000', hover: '#DEE0E6' },
     bar: { color: '#FFFFFF', bg: '#000000', alt: '#000000' },
     primary: { color: '#F7D002', hover: '#F7D002', body: '#000000' },
-    secondary: { color: '#88D4FF', hover: '#532D8F', body: '#FFFFFF' },
+    secondary: { color: '#88D4FF', hover: '#532D8F', body: '#000000' },
     positive: { color: '#44C27F', hover: '#185B48', body: '#FFFFFF' },
     negative: { color: '#CF6666', hover: '#9B000D', body: '#FFFFFF' },
     highlight: { color: '#D78D0E', hover: '#BA7909', body: '#FFFFFF' },
