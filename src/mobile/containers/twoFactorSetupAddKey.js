d78032917ed816630ec85f687fdef46ceb290489
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-03-19 16:25:06 +0000

    Remove unnecessary code

--- src/mobile/containers/twoFactorSetupAddKey.js
+++ src/mobile/containers/twoFactorSetupAddKey.js
@@ -154,7 +154,7 @@ export class TwoFactorSetupAddKey extends Component {
 
         return (
             <View style={[styles.container, backgroundColor]}>
-                <DynamicStatusBar textColor={body.color} backgroundColor={body.bg} />
+                <DynamicStatusBar backgroundColor={body.bg} />
                 <View style={styles.topWrapper}>
                     <Icon name="iota" size={width / 8} color={body.color} />
                 </View>
