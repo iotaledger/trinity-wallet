66029c444327746d0a600690adaeaac8b1708505
iotadance <merch.iota.dance@gmail.com>
2018-03-19 22:03:14 +0200

    State update bugfixes

--- src/desktop/src/ui/views/account/Name.js
+++ src/desktop/src/ui/views/account/Name.js
@@ -22,7 +22,7 @@ class AccountName extends PureComponent {
         accountName: PropTypes.string.isRequired,
         /** Change current account name
          * @param {Object} AccountInfo - updated account info
-         * @param {Object} SeedNames - updated seed names
+         * @param {Object} accountNames - updated seed names
          */
         changeAccountName: PropTypes.func.isRequired,
         /** Create a notification message
@@ -88,7 +88,7 @@ class AccountName extends PureComponent {
 
 const mapStateToProps = (state) => ({
     accountInfo: state.account.accountInfo,
-    accountName: getSelectedAccountNameViaSeedIndex(state.tempAccount.seedIndex, state.account.seedNames),
+    accountName: getSelectedAccountNameViaSeedIndex(state.tempAccount.seedIndex, state.account.accountNames),
 });
 
 const mapDispatchToProps = {
