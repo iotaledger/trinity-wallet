bf3919267c8fd7371ee790cfbfd41f49063faecb
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-03-19 16:25:06 +0000

    Remove unnecessary code

--- src/mobile/components/deleteAccount.js
+++ src/mobile/components/deleteAccount.js
@@ -212,7 +212,7 @@ class DeleteAccount extends Component {
                                 <CustomTextInput
                                     label={t('global:password')}
                                     onChangeText={(password) => this.setState({ password })}
-                                    containerStyle={{ width: width / 1.36 }}
+                                    containerStyle={{ width: width / 1.2 }}
                                     autoCapitalize="none"
                                     autoCorrect={false}
                                     enablesReturnKeyAutomatically
