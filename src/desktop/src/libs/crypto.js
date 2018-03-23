c9abd18728c8c5c01132c5ca29c8e1ef2a40f8f4
iotadance <merch.iota.dance@gmail.com>
2018-03-23 16:15:42 +0200

    - Assets update - Crypto.js rollback

--- src/desktop/src/libs/crypto.js
+++ src/desktop/src/libs/crypto.js
@@ -106,4 +106,7 @@ export const removeKey = (password, key) => {
  * @param {String} code - Target activation code
  * @param {String} uuid - UUID of the machine
  */
-export const checkActivationCode = true;
+export const checkActivationCode = (code, uuid) => {
+    const key = 'LURGzCPEHqhjvYLwAJXRv5Fc';
+    return code === sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(key + uuid));
+};
