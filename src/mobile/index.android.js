c3e938ca96e883ae4c4dd8020660ce1945f2c089
Rajiv Shah <rajivshah1@icloud.com>
2018-03-28 16:12:43 -0400

    Implement Bugsnag error reports

--- src/mobile/index.android.js
+++ src/mobile/index.android.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
+const bugsnag = new Client();
--- src/mobile/index.ios.js
+++ src/mobile/index.ios.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
 console.disableYellowBox = true;
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
+const bugsnag = new Client();
