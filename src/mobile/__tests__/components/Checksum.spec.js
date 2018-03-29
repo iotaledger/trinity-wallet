6bf09391494a321e990141e09030bb42ed2a4d60
Umair Sarfraz <aquadestructor@icloud.com>
2018-03-29 14:33:50 +0500

    Add coverage for Checksum component

---
+++ src/mobile/__tests__/components/Checksum.spec.js
@@ -0,0 +1,84 @@
+import assign from 'lodash/assign';
+import noop from 'lodash/noop';
+import React from 'react';
+import { Clipboard } from 'react-native';
+import PropTypes from 'prop-types';
+import { shallow } from 'enzyme';
+import Checksum from '../../components/Checksum';
+
+const getProps = (overrides) =>
+    assign(
+        {},
+        {
+            seed: '9'.repeat(81),
+            theme: { input: {}, body: {}, primary: {} },
+        },
+        overrides,
+    );
+
+describe('Testing Checksum component', () => {
+    describe('propTypes', () => {
+        it('should require a seed string as a prop', () => {
+            expect(Checksum.propTypes.seed).toEqual(PropTypes.string.isRequired);
+        });
+
+        it('should require a theme object as a prop', () => {
+            expect(Checksum.propTypes.theme).toEqual(PropTypes.object.isRequired);
+        });
+    });
+
+    describe('when renders', () => {
+        it('should not explode', () => {
+            const props = getProps();
+
+            const wrapper = shallow(<Checksum {...props} />);
+            expect(wrapper.name()).toEqual('View');
+        });
+
+        it('should return a Text component', () => {
+            const props = getProps();
+
+            const wrapper = shallow(<Checksum {...props} />);
+            expect(wrapper.find('Text').length).toEqual(1);
+        });
+    });
+
+    describe('instance methods', () => {
+        describe('when called', () => {
+            describe('#getChecksumValue', () => {
+                describe('when seed length is not zero and seed contains any character other than (A-Z, 9)', () => {
+                    it('should return "!"', () => {
+                        const props = getProps({ seed: '-!' });
+
+                        const instance = shallow(<Checksum {...props} />).instance();
+                        const checksum = instance.getChecksumValue();
+
+                        expect(checksum).toEqual('!');
+                    });
+                });
+
+                describe('when seed length is not zero and seed length is less than 81', () => {
+                    it('should return "< 81"', () => {
+                        const props = getProps({ seed: 'A'.repeat(80) });
+
+                        const instance = shallow(<Checksum {...props} />).instance();
+                        const checksum = instance.getChecksumValue();
+
+                        expect(checksum).toEqual('< 81');
+                    });
+                });
+
+                describe('when seed length is 81 and seed contains valid characters (A-Z, 9)', () => {
+                    it('should return computed checksum of seed', () => {
+                        const props = getProps({ seed: '9'.repeat(81) });
+
+                        const instance = shallow(<Checksum {...props} />).instance();
+                        const checksum = instance.getChecksumValue();
+
+                        expect(checksum).toEqual('KZW');
+                    });
+                });
+            });
+        });
+    });
+});
