229e757e0aae3f977813b76a3d929a2e1a8478c3
Rajiv Shah <rajivshah1@icloud.com>
2018-03-29 11:52:24 -0400

    Bugsnag implementation

--- src/desktop/src/index.js
+++ src/desktop/src/index.js
@@ -1,3 +1,4 @@
+import bugsnag from 'bugsnag-js';
 import React from 'react';
 import { render } from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
@@ -5,8 +6,12 @@ import { Provider as Redux } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router';
 import i18next from 'libs/i18next';
 import store, { persistStore } from 'store';
+import createPlugin from 'bugsnag-react';
 
 import Index from 'ui/Index';
+
+export const bugsnagClient = bugsnag('53981ba998df346f6377ebbeb1da46d3');
+const ErrorBoundary = bugsnagClient.use(createPlugin(React));
 
 const persistConfig = {
     blacklist: ['tempAccount', 'polling', 'ui', 'seeds'],
@@ -15,12 +20,14 @@ const persistConfig = {
 persistStore(store, persistConfig);
 
 render(
-    <Redux store={store}>
-        <I18nextProvider i18n={i18next}>
-            <Router>
-                <Index />
-            </Router>
-        </I18nextProvider>
-    </Redux>,
+    <ErrorBoundary>
+        <Redux store={store}>
+            <I18nextProvider i18n={i18next}>
+                <Router>
+                    <Index />
+                </Router>
+            </I18nextProvider>
+        </Redux>,
+    </ErrorBoundary>,
     document.getElementById('root'),
 );
