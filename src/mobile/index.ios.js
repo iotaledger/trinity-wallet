cdb9bcf58feb49d8eef4a5263a297511b70a0500
Rajiv Shah <rajivshah1@icloud.com>
2018-04-02 09:42:25 -0400

    Disable breadcrumbs

--- src/mobile/index.ios.js
+++ src/mobile/index.ios.js
@@ -1,7 +1,9 @@
 import { AppRegistry } from 'react-native';
-import { Client } from 'bugsnag-react-native';
+import { Client, Configuration } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
 console.disableYellowBox = true;
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
+const configuration = new Configuration();
+configuration.automaticallyCollectBreadcrumbs = false;
 const bugsnag = new Client();
