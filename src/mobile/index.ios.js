86d53b0a70a8b06a0a22d7cec7c44234726c41d2
Rajiv Shah <rajivshah1@icloud.com>
2018-04-02 12:24:30 -0400

    Ignore linting errors on index.android.js and index.ios.js

--- src/mobile/index.ios.js
+++ src/mobile/index.ios.js
@@ -2,6 +2,8 @@ import { AppRegistry } from 'react-native';
 import { Client } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
+/*eslint-disable no-unused-vars*/
+/*eslint-disable no-console*/
 console.disableYellowBox = true;
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
 const bugsnag = new Client();
