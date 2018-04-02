65579e4871ca794c5f5356b41bca3316be713370
Rajiv Shah <rajivshah1@icloud.com>
2018-04-02 12:20:26 -0400

    Reenable breadcrumbs

--- src/mobile/index.android.js
+++ src/mobile/index.android.js
@@ -1,8 +1,6 @@
 import { AppRegistry } from 'react-native';
-import { Client, Configuration } from 'bugsnag-react-native';
+import { Client } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
-const configuration = new Configuration();
-configuration.automaticallyCollectBreadcrumbs = false;
 const bugsnag = new Client();
