03ae41857dd15f548ea104e0ec2df0e74e2b0259
Rajiv Shah <rajivshah1@icloud.com>
2018-04-06 11:46:02 -0400

    mobile/ios: add Entangled bindings and expose methods to RN

---
+++ src/mobile/ios/Entangled/Entangled.h
@@ -0,0 +1,13 @@
+//
+//  Entangled.h
+//  iotaWallet
+//
+//  Created by Rajiv Shah on 4/6/18.
+//  Copyright © 2018 IOTA Foundation. All rights reserved.
+//
+#import <React/RCTBridgeModule.h>
+#import "bindings.h"
+
+@interface Entangled: NSObject <RCTBridgeModule>
+
+@end
