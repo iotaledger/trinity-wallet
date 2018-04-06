641589e1e6f49c6112ea8fbaa3182ccf6171bf58
Rajiv Shah <rajivshah1@icloud.com>
2018-04-06 15:02:00 -0400

    Shared: Localize strings

--- src/shared/containers/settings/Node.js
+++ src/shared/containers/settings/Node.js
@@ -69,7 +69,7 @@ export default function withNodeData(NodeComponent) {
                     addCustomPoWNode(nodeSelected);
                 }
 
-                generateAlert('success', 'Node changed', 'The node has been changed successfully!');
+                generateAlert('success', t('nodeChanged'), t('nodeChangedExpalantion'));
 
                 if (typeof backPress === 'function') {
                     backPress();
