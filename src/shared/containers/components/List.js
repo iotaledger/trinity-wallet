4839cad64b17389a6db128ced523e493cdbbf339
Umair Sarfraz <aquadestructor@icloud.com>
2018-04-10 05:46:24 +0500

    Map new transfers state structure to desktop

--- src/shared/containers/components/List.js
+++ src/shared/containers/components/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
-import { getDeduplicatedTransfersForSelectedAccount, selectAccountInfo } from '../../selectors/accounts';
+import { getTransfersForSelectedAccount } from '../../selectors/accounts';
 
 /**
  * List component container
@@ -11,8 +11,7 @@ import { getDeduplicatedTransfersForSelectedAccount, selectAccountInfo } from '.
 export default function withListData(ListComponent) {
     class ListData extends React.PureComponent {
         static propTypes = {
-            accountInfo: PropTypes.object.isRequired,
-            transfers: PropTypes.array.isRequired,
+            transfers: PropTypes.object.isRequired,
             wallet: PropTypes.object.isRequired,
             limit: PropTypes.number,
             filter: PropTypes.string,
@@ -25,7 +24,6 @@ export default function withListData(ListComponent) {
         };
         render() {
             const {
-                accountInfo,
                 updateAccount,
                 transfers,
                 limit,
@@ -42,7 +40,6 @@ export default function withListData(ListComponent) {
                 wallet.isSyncing || wallet.isSendingTransfer || wallet.isAttachingToTangle || wallet.isTransitioning;
 
             const ListProps = {
-                addresses: Object.keys(accountInfo.addresses),
                 transfers,
                 updateAccount,
                 setItem,
@@ -64,8 +61,7 @@ export default function withListData(ListComponent) {
 
     const mapStateToProps = (state) => ({
         accounts: state.accounts,
-        accountInfo: selectAccountInfo(state),
-        transfers: getDeduplicatedTransfersForSelectedAccount(state),
+        transfers: getTransfersForSelectedAccount(state),
         theme: state.settings.theme,
         wallet: state.wallet,
     });
