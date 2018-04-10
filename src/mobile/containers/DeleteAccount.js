d12dac65e1417eed5898ef8c9549b0a8d1271ce4
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-04-10 16:00:19 +0100

    Mobile: Revert Android modal animations

--- src/mobile/containers/DeleteAccount.js
+++ src/mobile/containers/DeleteAccount.js
@@ -290,11 +290,11 @@ class DeleteAccount extends Component {
                         </TouchableOpacity>
                     </View>
                     <Modal
-                        animationIn="zoomIn"
-                        animationOut="zoomOut"
-                        animationInTiming={300}
+                        animationIn={isAndroid ? 'bounceInUp' : 'zoomIn'}
+                        animationOut={isAndroid ? 'bounceOut' : 'zoomOut'}
+                        animationInTiming={isAndroid ? 1000 : 300}
                         animationOutTiming={200}
-                        backdropTransitionInTiming={300}
+                        backdropTransitionInTiming={isAndroid ? 500 : 300}
                         backdropTransitionOutTiming={200}
                         backdropColor={backgroundColor}
                         backdropOpacity={0.6}
