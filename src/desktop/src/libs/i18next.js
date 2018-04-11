7bb0f3034ab08e9d24d5e9ea63ecd8ae6072b75a
Rajiv Shah <rajivshah1@icloud.com>
2018-04-11 01:12:23 -0400

    More localization

--- src/desktop/src/libs/i18next.js
+++ src/desktop/src/libs/i18next.js
@@ -7,7 +7,6 @@ export default i18next.init({
     fallbackLng: 'en',
     defaultNS: 'global',
     parseMissingKeyHandler: (value) => `Translation not available for ${value}`,
-    debug: true,
     resources: {
         en: require('locales/en/translation.json'),
         ar: require('locales/ar/translation.json'),
