8e44624436165df15daf22bf71fdbee74cb12433
Umair Sarfraz <aquadestructor@icloud.com>
2018-04-19 00:57:58 +0500

    Return immutable state from alerts reducer

--- src/shared/reducers/alerts.js
+++ src/shared/reducers/alerts.js
@@ -20,6 +20,7 @@ export default (state = initialState, action) => {
             };
         case ActionTypes.HIDE:
             return {
+                ...state,
                 category: '',
                 title: '',
                 message: '',
