789b0d6e60b779a8b61946ea4df8e498854d422d
Rajiv Shah <rajivshah1@icloud.com>
2018-04-19 21:53:13 -0400

    Add new nodes

--- src/shared/config.js
+++ src/shared/config.js
@@ -7,6 +7,14 @@ export const nodes = [
     'https://nodes.iota.fm:443',
     'https://field.carriota.com:443',
     'https://iotanode.us:443',
+    'https://peanut.iotasalad.org:14265',
+    'https://beta.tangle-nodes.com:443',
+    'https://alpha.tangle-nodes.com:443',
+    'https://potato.iotasalad.org:14265',
+    'https://tuna.iotasalad.org:14265',
+    'https://durian.iotasalad.org:14265',
+    'https://nodes.iota.cafe:443',
+    'https://nodes.thetangle.org:443',
 ];
 
 export const UPDATE_URL = 'https://trinity-alpha.iota.org/release';
