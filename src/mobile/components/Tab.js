0abacd6074326b22aa1faa0eb082030ebaf0fd7a
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-04-30 16:38:47 +0100

    Mobile: Improve Topbar UX

--- src/mobile/components/Tab.js
+++ src/mobile/components/Tab.js
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderTopColor: 'transparent',
-        borderTopWidth: parseInt(height / 100),
+        borderTopWidth: parseInt(height / 160),
     },
     iconTitle: {
         fontWeight: 'bold',
