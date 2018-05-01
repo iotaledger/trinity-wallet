6e0234b3acc86d4bdf62cd54a6b61141d07c97d5
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-05-01 14:23:43 +0100

    Mobile: Clear clipboard whenever navigating after copying seed

--- src/mobile/routes/entry.js
+++ src/mobile/routes/entry.js
@@ -37,7 +37,7 @@ const renderInitialScreen = (store) => {
 
     i18next.changeLanguage(getLocaleFromLabel(state.settings.language));
 
-    const initialScreen = state.accounts.onboardingComplete ? 'login' : 'languageSetup';
+    const initialScreen = state.accounts.onboardingComplete ? 'newSeedSetup' : 'languageSetup';
 
     Navigation.startSingleScreenApp({
         screen: {
