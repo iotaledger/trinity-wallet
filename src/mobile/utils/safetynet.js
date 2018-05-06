93aa65b7ffdbf7d680ebfedbe813dfe89dfc1bc5
Umair Sarfraz <aquadestructor@icloud.com>
2018-05-07 02:28:14 +0500

    Mobile: Simpilify device integrity checks

--- src/mobile/utils/safetynet.js
+++ src/mobile/utils/safetynet.js
@@ -2,22 +2,14 @@ import { isPlayServicesAvailable, generateNonce, sendAndVerifyAttestation } from
 
 const API_KEY = 'AIzaSyCG6XTyOM1Od7jW9fucVdbep0x6BcL57Ko';
 
-export const sendAndVerify = () => {
-  return isPlayServicesAvailable()
+export const doAttestationFromSafetyNet = () => {
+    return isPlayServicesAvailable()
         .then((result) => {
             if (result === true) {
-                Promise.resolve();
-            } else {
-                Promise.reject(result);
+                return generateNonce(24);
             }
+
+            throw new Error('play services not available.');
         })
-        .then(() => generateNonce(24))
-        .then((nonce) => sendAndVerifyAttestation(nonce, API_KEY))
-        .then((isRooted) => {
-            return Promise.resolve(isRooted);
-        })
-        .catch((e) => {
-            console.log(e);
-            return e;
-        });
+        .then((nonce) => sendAndVerifyAttestation(nonce, API_KEY));
 };
