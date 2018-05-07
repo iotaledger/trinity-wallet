a08f2e00b9b6a9d34973d82f3bb000bda9908f13
iotadance <merch.iota.dance@gmail.com>
2018-05-07 09:11:58 +0100

    Theme color update

--- src/shared/themes/themes/Default.js
+++ src/shared/themes/themes/Default.js
@@ -1,12 +1,15 @@
 export default {
     body: { color: '#ffffff', bg: '#1a373e', altBg: '#173036', alt: '#264147' },
     bar: { color: '#ffffff', bg: '#181C2B', alt: '#000000' },
-    primary: { color: '#F7D002', hover: '#D3B100', body: '#1a373e' },
-    secondary: { color: '#88D4FF', hover: '#64A2C6', body: '#1a373e' },
+    primary: { color: '#f7d100', hover: '#d2b200', body: '#1a373e' },
+    secondary: { color: '#89D3FE', hover: '#74b4d9', body: '#1a373e' },
     positive: { color: '#44C27F', hover: '#409568', body: '#000000' },
     negative: { color: '#CF6666', hover: '#B94006', body: '#000000' },
     extra: { color: '#88D4FF', hover: '#669FBF', body: '#ffffff' },
+    dark: { color: '#162f35', hover: '#13282d', body: '#ffffff' },
     label: { color: '#ffffff', hover: '#F7D002' },
     input: { color: '#ffffff', bg: '#2a4a51', alt: '#94A8AC' },
     chart: { color: '#FFA25B' },
+    wave: { primary: 'rgba(137, 211, 254, 0.15)', secondary: 'rgba(0, 0, 0, 0.15)' },
+    infobox: { bg: '#244149', border: '#3b555b', body: '#ffffff' },
 };
