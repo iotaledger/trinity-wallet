05c1451f018be1cc5cc6de3575222296506bbc8b
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-05-11 23:16:18 +0100

    Mobile: Fix notification alert on no connection

--- src/mobile/components/NotificationLog.js
+++ src/mobile/components/NotificationLog.js
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        width
     },
     modalContent: {
         borderRadius: GENERAL.borderRadius,
