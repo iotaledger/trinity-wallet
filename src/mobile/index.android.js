d852d9ab89ebce0406f15bf086ebc63a8a83fd68
Rajiv Shah <rajivshah1@icloud.com>
2018-05-12 22:23:20 -0400

    Use package.json info for Bugsnag configuration

--- src/mobile/index.android.js
+++ src/mobile/index.android.js
@@ -1,8 +1,12 @@
 import { AppRegistry } from 'react-native';
-import { Client } from 'bugsnag-react-native';
+import { Client, Configuration } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
+
+const packageJson = require('./package.json');
 
 /*eslint-disable no-unused-vars*/
 /*eslint-disable no-console*/
-const bugsnag = new Client();
+const configuration = new Configuration();
+configuration.appVersion = packageJson.version;
+const bugsnag = new Client(configuration);
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
