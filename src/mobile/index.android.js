c21e926f7fadc71bdb589fa6627e8828b1353ddc
Rajiv Shah <rajivshah1@icloud.com>
2018-05-12 22:17:35 -0400

    Fix release lane and remove bugsnag configuration

--- src/mobile/index.android.js
+++ src/mobile/index.android.js
@@ -1,10 +1,8 @@
 import { AppRegistry } from 'react-native';
-import { Client, Configuration } from 'bugsnag-react-native';
+import { Client } from 'bugsnag-react-native';
 import Wallet from './routes/entry';
 
 /*eslint-disable no-unused-vars*/
 /*eslint-disable no-console*/
-const configuration = new Configuration();
-configuration.appVersion = '0.1.34';
-const bugsnag = new Client(configuration);
+const bugsnag = new Client();
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
