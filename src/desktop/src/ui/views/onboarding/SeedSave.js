0c71c1ffea7a6da19937ab9b71b95f0b1e9787f1
iotadance <merch.iota.dance@gmail.com>
2018-05-16 15:24:58 +0300

    Bugfixes and updates:
    - Update filled/empty paper wallet template
    - Layout Write seed down to match paper wallet

--- src/desktop/src/ui/views/onboarding/SeedSave.js
+++ src/desktop/src/ui/views/onboarding/SeedSave.js
@@ -1,14 +1,25 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { translate, Trans } from 'react-i18next';
 import { connect } from 'react-redux';
+
+import { getChecksum } from 'libs/iota/utils';
 
 import Modal from 'ui/components/modal/Modal';
 import Button from 'ui/components/Button';
 import Icon from 'ui/components/Icon';
 import Clipboard from 'ui/components/Clipboard';
 
+import paperWallet from 'themes/paper-wallet.svg';
+import paperWalletFilled from 'themes/paper-wallet-filled.svg';
+
 import css from './index.scss';
+
+const wallets = {
+    paperWallet: paperWallet,
+    paperWalletFilled: paperWalletFilled,
+};
 
 /**
  * Onboarding, Seed backup step
@@ -84,15 +95,18 @@ class SeedSave extends PureComponent {
                     <section>
                         <h1>{t('saveYourSeed:letsWriteDownYourSeed')}</h1>
                         <p>{t('saveYourSeed:youCanHighlightCharacters')}</p>
-                        <div className={css.seed}>
+                        <div className={classNames(css.seed, css.narrow)}>
                             <div>
                                 {seed &&
                                     seed.match(/.{1,3}/g).map((letter, index) => {
                                         return (
                                             <span
-                                                className={writeIndex !== Math.floor(index / 3) + 1 ? css.disabled : null}
+                                                className={
+                                                    writeIndex !== Math.floor(index / 3) + 1 ? css.disabled : null
+                                                }
                                                 key={`${index}${letter}`}
                                             >
+                                                {index % 3 === 0 ? <em>{index / 3 + 1}</em> : null}
                                                 {letter}
                                             </span>
                                         );
@@ -118,7 +132,7 @@ class SeedSave extends PureComponent {
                         </nav>
                     </section>
                     <footer>
-                        <Button to="/onboarding/seed-generate" variant="secondary">
+                        <Button onClick={() => window.print()} variant="secondary">
                             {t('saveYourSeed:printBlankWallet')}
                         </Button>
                         <Button
@@ -126,13 +140,39 @@ class SeedSave extends PureComponent {
                                 e.preventDefault();
                                 this.setState({ writeVisible: false });
                             }}
-                            to="/onboarding/seed-verify"
                             variant="primary"
                         >
                             {t('done')}
                         </Button>
                     </footer>
                 </Modal>
+                <div className={css.print} onClick={() => window.print()}>
+                    <div>
+                        {writeVisible ? (
+                            <svg viewBox="0 0 595 841" xmlns="http://www.w3.org/2000/svg">
+                                {seed &&
+                                    seed.split('').map((letter, index) => {
+                                        const space = index % 9 > 5 ? 38 : index % 9 > 2 ? 19 : 0;
+                                        const x = 168 + (index % 9) * 26 + space;
+                                        const y = 405 + Math.floor(index / 9) * 32.8;
+                                        return (
+                                            <text x={x} y={y} key={`${index}${letter}`}>
+                                                {letter}
+                                            </text>
+                                        );
+                                    })}
+                                <text x="278" y="730">
+                                    {getChecksum(seed)}
+                                </text>
+                            </svg>
+                        ) : null}
+                    </div>
+                    <img
+                        width="100%"
+                        height="100%"
+                        src={writeVisible ? wallets.paperWalletFilled : wallets.paperWallet}
+                    />
+                </div>
             </form>
         );
     }
