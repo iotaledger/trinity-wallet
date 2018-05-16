cd03cfbf9140c74cbdf5f8ffa7f7f3ca3dc1920e
Charlie Varley <charlie@192.168.1.106>
2018-05-16 22:17:32 +0100

    Mobile: Update PoW on node change if node does not support remote PoW. Add spinner while checking node

--- src/shared/libs/iota/extendedApi.js
+++ src/shared/libs/iota/extendedApi.js
@@ -208,6 +208,11 @@ const storeAndBroadcastAsync = (trytes) => {
     });
 };
 
+const checkAttachToTangleAsync = (node) => {
+    return fetch(node, { method: 'POST', body: JSON.stringify({ command: 'attachToTangle'}), headers: { 'X-IOTA-API-Version': '1' } })
+        .then((res) => res.json())
+};
+
 const attachToTangleAsync = (
     trunkTransaction,
     branchTransaction,
@@ -245,4 +250,5 @@ export {
     prepareTransfersAsync,
     storeAndBroadcastAsync,
     attachToTangleAsync,
+    checkAttachToTangleAsync
 };
