6c74ac926d1b5ee7428142690191fef93aada1d3
iotadance <merch.iota.dance@gmail.com>
2018-05-18 11:22:13 +0300

    Check Capitalize helper params

--- src/desktop/src/libs/helpers.js
+++ src/desktop/src/libs/helpers.js
@@ -3,5 +3,5 @@
  * @param {String} input - The target string
  */
 export const capitalize = (input) => {
-    return input[0].toUpperCase() + input.substr(1).toLowerCase();
+    return typeof input === 'string' && input.length > 0 ? input[0].toUpperCase() + input.substr(1).toLowerCase() : '';
 };
