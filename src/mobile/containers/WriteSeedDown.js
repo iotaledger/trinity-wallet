964befb81fe4679db8950f25f11f8f4eec5cf472
Charlie Varley <charlie@charlies-mbp-2.mynet>
2018-05-21 20:50:42 +0100

    Mobile: Increase button width

--- src/mobile/containers/WriteSeedDown.js
+++ src/mobile/containers/WriteSeedDown.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
     doneButton: {
         borderWidth: 1.2,
         borderRadius: GENERAL.borderRadius,
-        width: width / 3,
+        width: width / 2.7,
         height: height / 14,
         alignItems: 'center',
         justifyContent: 'space-around',
