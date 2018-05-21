20f18098e95269025ed44331905edb40fc156973
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-05-21 12:19:44 +0100

    Mobile: Fix modal status bar colour on Android

--- src/mobile/android/app/src/main/java/com/iota/wallet/MainApplication.java
+++ src/mobile/android/app/src/main/java/com/iota/wallet/MainApplication.java
@@ -26,6 +26,7 @@ import com.bugsnag.BugsnagReactNative;
 import com.rajivshah.safetynet.RNGoogleSafetyNetPackage;
 import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
 import com.rndetectnavbarandroid.RNDetectNavbarAndroidPackage;
+import me.listenzz.modal.TranslucentModalReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -56,6 +57,7 @@ public class MainApplication extends NavigationApplication {
             // eg. new VectorIconsPackage()
             new SvgPackage(),
             new MainReactPackage(),
+            new TranslucentModalReactPackage(),
             new RNDetectNavbarAndroidPackage(),
             new RNDeviceInfo(),
             new RNOSModule(),
