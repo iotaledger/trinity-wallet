b7adca125ec0bf7206d22eace0b2afc873e7cebb
Charlie Varley <charlie@Charlies-MacBook-Pro-2.local>
2018-05-28 14:20:47 +0300

    Mobile: Display alert when changing auto-promotion settings. Block auto-promote setting update when node doesnt support remote PoW

--- src/shared/actions/settings.js
+++ src/shared/actions/settings.js
@@ -306,7 +306,7 @@ export function changePowSettings() {
                         ),
                     );
                 }
-                dispatch(setRemotePoW(true));
+                dispatch(setRemotePoW(!settings.remotePoW));
                 dispatch(generateAlert('success', i18next.t('pow:powUpdated'), i18next.t('pow:powUpdatedExplanation')));
             });
         } else {
@@ -319,7 +319,37 @@ export function changePowSettings() {
 export function changeAutoPromotionSettings() {
     return (dispatch, getState) => {
         const settings = getState().settings;
-        dispatch(setAutoPromotion(!settings.autoPromotion));
+        if (!settings.autoPromotion){
+              checkAttachToTangleAsync(settings.node).then((res) => {
+                  if (res.error.includes(Errors.ATTACH_TO_TANGLE_UNAVAILABLE)) {
+                      return dispatch(
+                          generateAlert(
+                              'error',
+                              i18next.t('global:attachToTangleUnavailable'),
+                              i18next.t('global:attachToTangleUnavailableExplanationShort'),
+                              10000,
+                          ),
+                      );
+                  }
+                  dispatch(setAutoPromotion(!settings.autoPromotion));
+                  dispatch(
+                      generateAlert(
+                          'success',
+                          i18next.t('autoPromotion:autoPromotionUpdated'),
+                          i18next.t('autoPromotion:autoPromotionUpdatedExplanation'),
+                      ),
+                  );
+              });
+        } else {
+            dispatch(setAutoPromotion(!settings.autoPromotion));
+            dispatch(
+                generateAlert(
+                    'success',
+                    i18next.t('autoPromotion:autoPromotionUpdated'),
+                    i18next.t('autoPromotion:autoPromotionUpdatedExplanation'),
+                ),
+            );
+        }
     };
 }
 
