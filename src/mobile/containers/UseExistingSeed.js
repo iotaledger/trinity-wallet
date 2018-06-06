c82eea9020d47ee840ecba3b04e6da2a3274365d
cvarley100 <cvarley100@gmail.com>
2018-06-06 18:30:32 +0100

    Mobile: Block entry of non A-Z9 chars on seed text inputs

--- src/mobile/containers/UseExistingSeed.js
+++ src/mobile/containers/UseExistingSeed.js
@@ -322,7 +322,11 @@ class UseExistingSeed extends Component {
                         <View style={{ flex: 0.4 }} />
                         <CustomTextInput
                             label={t('global:seed')}
-                            onChangeText={(value) => this.setState({ seed: value.toUpperCase() })}
+                            onChangeText={(text) => {
+                                if (text.match(VALID_SEED_REGEX) || text.length === 0) {
+                                    this.setState({ seed: text.toUpperCase() });
+                                }
+                            }}
                             containerStyle={{ width: width / 1.15 }}
                             autoCapitalize="characters"
                             maxLength={MAX_SEED_LENGTH}
