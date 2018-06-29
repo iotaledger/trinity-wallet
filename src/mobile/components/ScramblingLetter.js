818a8aef88edaf5a3782e5689deb05ccbcd35498
cvarley100 <cvarley100@gmail.com>
2018-06-29 18:52:23 +0100

    Mobile: Add letter animation when generating receive address

---
+++ src/mobile/components/ScramblingLetter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+const ScramblingLetter = (props) => {
+    const { textStyle, index, scramble, letter, scramblingLetters } = props;
+    return <Text style={textStyle}>{scramble ? scramblingLetters[index] : letter}</Text>;
+};
+
+ScramblingLetter.propTypes = {
+    /** Letter text styles */
+    textStyle: PropTypes.array,
+    /** Index of current receive address character */
+    index: PropTypes.number,
+    /** Determines whether text should scramble */
+    scramble: PropTypes.bool,
+    /** Current receive address character */
+    letter: PropTypes.string,
+    /** Array of letters that change at random intervals */
+    scramblingLetters: PropTypes.array,
+};
+
+export default ScramblingLetter;
