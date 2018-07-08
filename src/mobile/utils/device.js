17ec39dbb64822db8429ae5f07c9de8f7b120257
Rajiv Shah <rajivshah1@icloud.com>
2018-07-08 16:32:02 -0400

    Check device locale against moment's supported locales before requiring it

--- src/mobile/utils/device.js
+++ src/mobile/utils/device.js
@@ -1,10 +1,9 @@
 import { Platform } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
-import { detectLocale } from 'iota-wallet-shared-modules/libs/locale';
 
 const device = DeviceInfo.getModel();
 
-export const locale = detectLocale(DeviceInfo.getDeviceLocale());
+export const locale = DeviceInfo.getDeviceLocale();
 
 export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
