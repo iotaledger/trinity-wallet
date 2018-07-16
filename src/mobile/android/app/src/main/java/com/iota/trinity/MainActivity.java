ef6b794e0e0ac35b4ea6393c9eeae75114fe12d6
Rajiv Shah <rajivshah1@icloud.com>
2018-07-16 16:49:44 -0400

    Fix Android linking

--- src/mobile/android/app/src/main/java/com/iota/trinity/MainActivity.java
+++ src/mobile/android/app/src/main/java/com/iota/trinity/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.support.v4.content.ContextCompat;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
-import com.jamesisaac.rnbackgroundtask;
+import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
 
 public class MainActivity extends SplashActivity {
     @Override
@@ -23,7 +23,7 @@ public class MainActivity extends SplashActivity {
         // FORCE LTR
         I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
         sharedI18nUtilInstance.allowRTL(getApplicationContext(), false);
-        BackgroundTaskPackage.useContext(this);
+        BackgroundTaskPackage.useContext(getApplication());
     }
 
     @Override
