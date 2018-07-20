a0cadefc9683a521473d09d878f458b0a0b83bcc
rihardsgravis <rihards.gravis@iota.org>
2018-07-20 17:12:38 +0300

    Bugfixes and updates:
    - Remove the outdated Styleguide
    - Add Puppeteer screenshot command line task

--- src/desktop/README.md
+++ src/desktop/README.md
@@ -1,39 +1,52 @@
 # IOTA Trinity Desktop Wallet
 
-This is the repository for the IOTA Trinity Desktop Wallet. The application is based on [React](https://reactjs.org) and built on [Electron](https://electronjs.org/). 
+This is the repository for the IOTA Trinity Desktop Wallet. The application is based on [React](https://reactjs.org) and built on [Electron](https://electronjs.org/).
 
 ## Building the application
 
 To build the application locally from source, follow these steps:
 
 ### 1. Install Node.JS and Electron
+
 First you need to install Node.JS if you haven’t done that already.
 Then run the following command to install electron globally.
+
 ```
 npm install -g electron
 ```
 
 ### 2. Clone or download the Trinity repo from GitLab.
+
 Clone the GitLab repo by running this command:
+
 ```
 git clone git@gitlab.com:iota-community/wallet.git
 ```
+
 Or [download](https://gitlab.com/iota-community/wallet/repository/archive.zip) the repo and extract the archive.
 
 After cloning or downloading and extracting the application run:
+
 ```
 cd src/desktop/
 ```
+
 ### 3. Install dependencies
+
 Now we need to install dependencies, such as the electron installer or the React code packager. Do this by running:
+
 ```
 npm install
 ```
+
 ### 4. Build Trinity desktop appplication
+
 When the npm install is done you can build the wallet application by running:
+
 ```
 npm run compile:mac
 ```
+
 Change `mac` to your operating system - `mac`, `win` or `linux`.
 
 This will start the building process for the Trinity desktop application and could take a couple of minutes to finish.
@@ -41,7 +54,9 @@ This will start the building process for the Trinity desktop application and cou
 After the building is finished, the application executable and installation files will be located in the directory `src/desktop/out/`.
 
 ### 4. Run Trinity desktop app in development mode
+
 To start the application in development mode, run
+
 ```
 npm start
 ```
@@ -49,10 +64,12 @@ npm start
 Note: you might have to rebuild the `keytar` node module under Windows with `.\node_modules\.bin\electron-rebuild.cmd`
 as there's a bug with it ([link](https://github.com/atom/node-keytar/issues/51)).
 
-## Trinity UI styleguide
+## Trinity theming
 
-To start the browser based UI styleguide and theme editor, run 
+To create proof checking screenshots of key wallet views for all Trinity themes, run
+
 ```
-npm run styleguide
+npm run style:shots
 ```
-After the command finished to load, open `http://localhost:1074` in a browser. Chrome is preferred as it's used in the Trinity application itself.
+
+After the command finished, the screenshots will be located in the directory `/shots/`
--- src/desktop/src/ui/global/Theme.js
+++ src/desktop/src/ui/global/Theme.js
@@ -1,6 +1,10 @@
+/* global Electron */
 import { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+import { updateTheme } from 'actions/settings';
+import themes from 'themes/themes';
 
 /**
  * Theming style provider component
@@ -8,20 +12,81 @@ import PropTypes from 'prop-types';
  */
 class Theme extends PureComponent {
     static propTypes = {
-        /** Theme definitions object */
+        location: PropTypes.object,
         theme: PropTypes.object.isRequired,
-        /** Unique yheme name */
         themeName: PropTypes.string.isRequired,
+        updateTheme: PropTypes.func.isRequired,
+    };
+
+    state = {
+        themeIndex: 0,
+        originalTheme: 'Default',
     };
 
     componentDidMount() {
         this.updateTheme(this.props.theme);
+
+        if (Electron.mode === 'dev') {
+            this.onKey = this.onKeyDown.bind(this);
+            window.addEventListener('keydown', this.onKey);
+        }
+        if (Electron.mode === 'puppeteer') {
+            this.timeout = setTimeout(this.takeShot.bind(this), 4000);
+        }
     }
 
     componentWillReceiveProps(nextProps) {
         if (this.props.themeName !== nextProps.themeName) {
             this.updateTheme(nextProps.theme);
         }
+    }
+
+    componentWillUnmount() {
+        if (Electron.mode === 'dev') {
+            window.removeEventListener('keydown', this.onKey);
+        }
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+    }
+
+    onKeyDown(e) {
+        if ((e.metaKey || e.ctrlKey) && e.key === 'p') {
+            this.setState({
+                themeIndex: 0,
+                originalTheme: this.props.themeName,
+            });
+            this.takeShot();
+        }
+    }
+
+    takeShot() {
+        const { originalTheme, themeIndex } = this.state;
+
+        const themeNames = Object.keys(themes);
+
+        if (themeIndex >= themeNames.length) {
+            this.props.updateTheme(themes[originalTheme], originalTheme);
+            Electron.screenshotsDone();
+            return;
+        }
+
+        const themeName = themeNames[themeIndex];
+        const fileName = `wallet${this.props.location.pathname.replace(/\//g, '-')}_${themeName
+            .replace(/ /g, '_')
+            .toLowerCase()}`;
+
+        this.setState({
+            themeIndex: themeIndex + 1,
+        });
+
+        this.props.updateTheme(themes[themeName], themeName);
+
+        setTimeout(() => {
+            Electron.screenshot(fileName);
+        }, 300);
+
+        setTimeout(this.takeShot.bind(this), 900);
     }
 
     updateTheme(theme) {
@@ -50,4 +115,11 @@ const mapStateToProps = (state) => ({
     themeName: state.settings.themeName,
 });
 
-export default connect(mapStateToProps)(Theme);
+const mapDispatchToProps = {
+    updateTheme,
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Theme);
