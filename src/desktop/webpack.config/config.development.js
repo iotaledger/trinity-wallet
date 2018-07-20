9617a849ae5a9da8b706c0ce253c51af2e219b3a
rihardsgravis <rihards.gravis@iota.org>
2018-07-20 17:12:38 +0300

    Bugfixes and updates:
    - Remove the outdated Styleguide
    - Add Puppeteer screenshot command line task

--- src/desktop/webpack.config/config.development.js
+++ src/desktop/webpack.config/config.development.js
@@ -4,11 +4,7 @@ const config = require('./config.base');
 config.devtool = 'eval-source-map';
 config.mode = 'development';
 
-const buildTarget = process.env.BUILD_TARGET || 'main';
-
-const buildEntry = buildTarget === 'styleguide' ? './src/guide/index.js' : config.entry;
-
-config.entry = ['webpack-hot-middleware/client?reload=true', 'react-error-overlay'].concat(buildEntry);
+config.entry = ['webpack-hot-middleware/client?reload=true', 'react-error-overlay'].concat(config.entry);
 
 config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(config.plugins);
 
