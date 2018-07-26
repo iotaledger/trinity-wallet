6a7a11c134f2cd7494e62802a8edceb115c26ece
Umair Sarfraz <aquadestructor@icloud.com>
2018-07-26 10:53:45 +0500

    Add code documentation for actions/alerts

--- src/shared/actions/alerts.js
+++ src/shared/actions/alerts.js
@@ -8,6 +8,18 @@ export const ActionTypes = {
     CLEAR_LOG: 'IOTA/ALERTS/CLEAR_LOG',
 };
 
+/**
+ * Dispatch to generate an alert
+ *
+ * @method generate
+ * @param {string} category
+ * @param {string} title
+ * @param {string} message
+ * @param {string} category
+ * @param {number} closeInterval
+ *
+ * @returns {{ type: {string}, category: {string}, title: {string}, message: {string}, category: {string}, closeInterval: {number} }}
+ */
 const generate = (category, title, message, closeInterval = 5500) => ({
     type: ActionTypes.SHOW,
     category,
@@ -16,8 +28,28 @@ const generate = (category, title, message, closeInterval = 5500) => ({
     closeInterval,
 });
 
+/**
+ * Dispatch to hide an alert
+ *
+ * @method dispose
+ *
+ * @returns {{type: {string} }}
+ */
 const dispose = () => ({ type: ActionTypes.HIDE });
 
+/**
+ * Generates an alert. If an error string is passed then it also updates notification log in state
+ *
+ * @method generateAlert
+ * @param {string} category
+ * @param {string} title
+ * @param {string} message
+ * @param {string} category
+ * @param {number} closeInterval
+ * @param {object} err
+ *
+ * @returns {function} dispatch
+ */
 export const generateAlert = (category, title, message, closeInterval, err) => (dispatch) => {
     dispatch(generate(category, title, message, closeInterval));
     if (err) {
@@ -25,6 +57,14 @@ export const generateAlert = (category, title, message, closeInterval, err) => (
     }
 };
 
+/**
+ * Generates an error alert when account info network call fails
+ *
+ * @method generateAccountInfoErrorAlert
+ * @param {object} err
+ *
+ * @returns {function} dispatch
+ */
 export const generateAccountInfoErrorAlert = (err) => (dispatch) => {
     dispatch(
         generateAlert(
@@ -37,10 +77,25 @@ export const generateAccountInfoErrorAlert = (err) => (dispatch) => {
     dispatch(prepareLogUpdate(err));
 };
 
+/**
+ * Generates an error alert if a network call is made to an out of sync node
+ *
+ * @method generateNodeOutOfSyncErrorAlert
+ *
+ * @returns {function} dispatch
+ */
 export const generateNodeOutOfSyncErrorAlert = () => (dispatch) => {
     dispatch(generateAlert('error', i18next.t('global:nodeOutOfSync'), i18next.t('global:nodeOutOfSyncExplanation')));
 };
 
+/**
+ * Generates an error alert if a something goes wrong during snapshot transition
+ *
+ * @method generateTransitionErrorAlert
+ * @param {object} err
+ *
+ * @returns {function} dispatch
+ */
 export const generateTransitionErrorAlert = (err) => (dispatch) => {
     if (err.message.includes(Errors.ATTACH_TO_TANGLE_UNAVAILABLE)) {
         dispatch(
@@ -73,6 +128,13 @@ export const generateTransitionErrorAlert = (err) => (dispatch) => {
     dispatch(prepareLogUpdate(err));
 };
 
+/**
+ * Generates a success alert manual sync is completed
+ *
+ * @method generateSyncingCompleteAlert
+ *
+ * @returns {function} dispatch
+ */
 export const generateSyncingCompleteAlert = () => (dispatch) => {
     dispatch(
         generateAlert(
@@ -84,11 +146,26 @@ export const generateSyncingCompleteAlert = () => (dispatch) => {
     );
 };
 
+/**
+ * Generates a success when an account is deleted
+ *
+ * @method generateAccountDeletedAlert
+ *
+ * @returns {function} dispatch
+ */
 export const generateAccountDeletedAlert = () => (dispatch) =>
     dispatch(
         generateAlert('success', i18next.t('settings:accountDeleted'), i18next.t('settings:accountDeletedExplanation')),
     );
 
+/**
+ * Generates an error alert if something goes wrong during manual sync
+ *
+ * @method generateSyncingErrorAlert
+ * @param {object} err
+ *
+ * @returns {function} dispatch
+ */
 export const generateSyncingErrorAlert = (err) => (dispatch) => {
     dispatch(
         generateAlert('error', i18next.t('settings:invalidResponse'), i18next.t('settings:invalidResponseExplanation')),
@@ -96,6 +173,14 @@ export const generateSyncingErrorAlert = (err) => (dispatch) => {
     dispatch(prepareLogUpdate(err));
 };
 
+/**
+ * Generates an error alert if something goes wrong during transaction
+ *
+ * @method generateTransferErrorAlert
+ * @param {object} error
+ *
+ * @returns {function} dispatch
+ */
 export const generateTransferErrorAlert = (error) => (dispatch) =>
     dispatch(
         generateAlert(
@@ -107,6 +192,14 @@ export const generateTransferErrorAlert = (error) => (dispatch) =>
         ),
     );
 
+/**
+ * Generates an error alert if something goes wrong during transaction promotion
+ *
+ * @method generatePromotionErrorAlert
+ * @param {object} error
+ *
+ * @returns {function} dispatch
+ */
 export const generatePromotionErrorAlert = (error) => (dispatch) =>
     dispatch(
         generateAlert(
@@ -118,6 +211,14 @@ export const generatePromotionErrorAlert = (error) => (dispatch) =>
         ),
     );
 
+/**
+ * Generates a success alert on successful transaction. Generates different alerts based on value/non-value transaction
+ *
+ * @method generateTransactionSuccessAlert
+ * @param {boolean} isZeroValue
+ *
+ * @returns {function} dispatch
+ */
 export const generateTransactionSuccessAlert = (isZeroValue = false) => (dispatch) => {
     if (isZeroValue) {
         dispatch(
@@ -130,19 +231,50 @@ export const generateTransactionSuccessAlert = (isZeroValue = false) => (dispatc
     }
 };
 
+/**
+ * Hides an active alert
+ *
+ * @method disposeOffAlert
+ *
+ * @returns {function} dispatch
+ */
 export const disposeOffAlert = () => (dispatch) => dispatch(dispose());
 
+/**
+ * Formats error object, assigns error receive time and update notification logs with the newly received error
+ *
+ * @method prepareLogUpdate
+ * @param {object} err
+ *
+ * @returns {function} dispatch
+ */
 export const prepareLogUpdate = (err) => (dispatch) => {
     const time = Date.now();
     const error = { error: err.toString(), time: time };
     dispatch(updateLog(error));
 };
 
+/**
+ * Dispatch to update notification log in state
+ *
+ * @method updateLog
+ * @param {object} logItem
+ *
+ * @returns {{type: {string}, logItem: {object} }}
+ */
 export const updateLog = (logItem) => ({
     type: ActionTypes.UPDATE_LOG,
     logItem,
 });
 
+/**
+ * Dispatch to clear notification log in state
+ *
+ *
+ * @method updateLog
+ *
+ * @returns {{type: {string} }}
+ */
 export const clearLog = () => ({
     type: ActionTypes.CLEAR_LOG,
 });
