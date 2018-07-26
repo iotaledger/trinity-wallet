56a61f6cd1aab058e2dae0e1c2a5afcd5c9bb4d1
Umair Sarfraz <aquadestructor@icloud.com>
2018-07-26 11:08:44 +0500

    Add code documentation
    - Add code documentation for actions/app
    - Remove unused "migrate" function from actions/app
    - Add code documentation for actions/home
    - Add code documentation for actios/keychain

--- src/shared/actions/home.js
+++ src/shared/actions/home.js
@@ -1,14 +1,28 @@
 export const ActionTypes = {
     CHANGE_HOME_SCREEN_CHILD_ROUTE: 'IOTA/HOME/ROUTE/CHANGE',
     TOGGLE_TOP_BAR_DISPLAY: 'IOTA/HOME/TOP_BAR/TOGGLE',
-    CLOSE_TOP_BAR: 'IOTA/HOME/TOP_BAR/CLOSE',
 };
 
+/**
+ * Dispatch to set active home screen route for mobile
+ *
+ * @method changeHomeScreenRoute
+ * @param {string} payload
+ *
+ * @returns {{type: {string}, payload: {string} }}
+ */
 export const changeHomeScreenRoute = (payload) => ({
     type: ActionTypes.CHANGE_HOME_SCREEN_CHILD_ROUTE,
     payload,
 });
 
+/**
+ * Dispatch to show/hide dashboard top bar on mobile
+ *
+ * @method toggleTopBarDisplay
+ *
+ * @returns {{type: {string} }}
+ */
 export const toggleTopBarDisplay = () => ({
     type: ActionTypes.TOGGLE_TOP_BAR_DISPLAY,
 });
