6b15820b9092cab2460d96a2518460191c92956a
Umair Sarfraz <aquadestructor@icloud.com>
2018-07-26 11:08:44 +0500

    Add code documentation
    - Add code documentation for actions/app
    - Remove unused "migrate" function from actions/app
    - Add code documentation for actions/home
    - Add code documentation for actios/keychain

--- src/shared/actions/keychain.js
+++ src/shared/actions/keychain.js
@@ -4,18 +4,45 @@ export const ActionTypes = {
     IS_GETTING_SENSITIVE_INFO_ERROR: 'IOTA/KEYCHAIN/IS_GETTING_SENSITIVE_INFO_ERROR',
 };
 
+/**
+ * Dispatch when a request to access keychain is about to be made on mobile
+ *
+ * @method getFromKeychainRequest
+ * @param {string} screen
+ * @param {string} purpose
+ *
+ * @returns {{type: {string}, screen: {string}, purpose: {string} }}
+ */
 export const getFromKeychainRequest = (screen, purpose) => ({
     type: ActionTypes.IS_GETTING_SENSITIVE_INFO_REQUEST,
     screen,
     purpose,
 });
 
+/**
+ * Dispatch when keychain data is successfully accessed on mobile
+ *
+ * @method getFromKeychainSuccess
+ * @param {string} screen
+ * @param {string} purpose
+ *
+ * @returns {{type: {string}, screen: {string}, purpose: {string} }}
+ */
 export const getFromKeychainSuccess = (screen, purpose) => ({
     type: ActionTypes.IS_GETTING_SENSITIVE_INFO_SUCCESS,
     screen,
     purpose,
 });
 
+/**
+ * Dispatch when an error occurs during keychain access on mobile
+ *
+ * @method getFromKeychainError
+ * @param {string} screen
+ * @param {string} purpose
+ *
+ * @returns {{type: {string}, screen: {string}, purpose: {string} }}
+ */
 export const getFromKeychainError = (screen, purpose) => ({
     type: ActionTypes.IS_GETTING_SENSITIVE_INFO_ERROR,
     screen,
