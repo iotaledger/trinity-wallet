3ccadffa857614083d34b471634c3487fea20d46
Rajiv Shah <rajivshah1@icloud.com>
2018-07-31 13:30:35 -0400

    Mobile/iOS: Write core functions for Argon2 implementation

--- src/mobile/ios/Argon2/Argon2.swift
+++ src/mobile/ios/Argon2/Argon2.swift
@@ -8,3 +8,55 @@
 
 import Foundation
 import CatCrypto
+
+// MARK: Core Functions
+
+/// Initializes Argon2
+///
+/// - Parameters:
+///   - iterations: Desired number of iterations
+///   - salt: Desired string to use as a salt
+///   - hashLength: Desired hash length
+///   - parallelism: Number of threads to use
+/// - Returns: Initialized argon2Crypto instance
+func initialize(iterations: Int, salt: String, hashLength: Int, parallelism: Int) -> CatArgon2Crypto {
+  let argon2Crypto = CatArgon2Crypto()
+  // Set mode to Argon2id
+  argon2Crypto.context.mode = .argon2id
+  // Set parameters
+  argon2Crypto.context.iterations = iterations
+  argon2Crypto.context.salt = salt
+  argon2Crypto.context.hashLength = hashLength
+  argon2Crypto.context.parallelism = parallelism
+
+  return argon2Crypto
+}
+
+/// Hashes a given password
+///
+/// - Parameters:
+///   - params: Parameters to initialize Argon2 with
+///   - password: Password to hash
+/// - Returns: Hashed password
+func hash(params: [String: Any], password: String) -> String {
+  // Initialize Argon2
+  let argon2Crypto = initialize(iterations: params["iterations"] as! Int, salt: params["salt"] as! String, hashLength: params["hashLength"] as! Int, parallelism: params["parallelism"] as! Int)
+  // Return the hash of the password
+  let hash = argon2Crypto.hash(password: password)
+  return hash.value!
+}
+
+/// Verifies a hash and password
+///
+/// - Parameters:
+///   - params: Parameters to initialize Argon2 with
+///   - hash: Password to verify
+///   - password: Hash to verify
+/// - Returns: Result of verification
+func verify(params: [String: Any], hash: String, password: String) -> Bool {
+  // Initialize Argon2
+  let argon2Crypto = initialize(iterations: params["iterations"] as! Int, salt: params["salt"] as! String, hashLength: params["hashLength"] as! Int, parallelism: params["parallelism"] as! Int)
+  // Verify the hash and password, then return the result
+  let result = argon2Crypto.verify(hash: hash, password: password)
+  return result.value
+}
