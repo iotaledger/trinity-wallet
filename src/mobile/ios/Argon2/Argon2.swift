d72ea3ea702afd6fbaf7f135f362050c0035c7d6
Rajiv Shah <rajivshah1@icloud.com>
2018-07-31 12:00:08 -0400

    Mobile/iOS: Add CatCrypto Argon2 implementation

---
+++ src/mobile/ios/Argon2/Argon2.swift
@@ -0,0 +1,10 @@
+//
+//  Argon2.swift
+//  iotaWallet
+//
+//  Created by Rajiv Shah on 7/31/18.
+//  Copyright © 2018 IOTA Foundation. All rights reserved.
+//
+
+import Foundation
+import CatCrypto
