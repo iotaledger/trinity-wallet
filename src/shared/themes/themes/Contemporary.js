312845acaa5b6134b4634d1378aea60a630cc0dc
rihardsgravis <rihards.gravis@iota.org>
2018-08-13 15:00:34 +0300

    Theming sync

--- src/shared/themes/themes/Contemporary.js
+++ src/shared/themes/themes/Contemporary.js
@@ -10,5 +10,5 @@ dark: { color: 'rgb(209, 212, 217)',hover: 'rgb(192, 195, 200)',body: 'rgb(0, 0,
 secondary: { color: 'rgb(85, 20, 180)',hover: 'rgb(69, 16, 146)',body: 'rgb(255, 255, 255)', },
 primary: { color: 'rgb(142, 18, 144)',hover: 'rgb(124, 16, 125)',body: 'rgb(255, 255, 255)', },
 body: { bg: 'rgb(234, 237, 243)',color: 'rgb(0, 0, 0)',alt: 'rgba(0, 0, 0, 0.25)', },
-wave: { primary: 'rgba(85, 20, 180, 0.5)',secondary: 'rgba(0, 0, 0, 0.25)', },
+wave: { primary: 'rgba(85, 20, 180, 0.5)',secondary: 'rgba(63, 89, 108, 0.15)', },
 };
