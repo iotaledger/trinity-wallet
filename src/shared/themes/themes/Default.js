335699220310c48a6589d22158fff5d058683dbe
rihardsgravis <rihards.gravis@iota.org>
2018-08-13 15:00:34 +0300

    Theming sync

--- src/shared/themes/themes/Default.js
+++ src/shared/themes/themes/Default.js
@@ -10,5 +10,5 @@ dark: { color: 'rgb(6, 35, 50)',hover: 'rgb(7, 30, 39)',body: 'rgb(240, 242, 246
 secondary: { color: 'rgb(208, 220, 238)',hover: 'rgb(192, 205, 225)',body: 'rgb(30, 30, 30)', },
 primary: { color: 'rgb(65, 220, 243)',hover: 'rgb(0, 205, 235)',body: 'rgb(0, 50, 65)', },
 body: { bg: 'rgb(3, 41, 62)',color: 'rgb(240, 242, 246)',alt: 'rgba(240, 240, 240, 0.25)', },
-wave: { primary: 'rgba(49, 94, 119, 0.12)',secondary: 'rgba(11, 27, 31, 0.6)', },
+wave: { primary: 'rgba(49, 94, 119, 0.3)',secondary: 'rgba(11, 27, 31, 0.6)', },
 };
