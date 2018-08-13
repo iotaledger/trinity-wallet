8459265f531ba68ff4e63d9d6af11350b90cd823
rihardsgravis <rihards.gravis@iota.org>
2018-08-13 15:06:23 +0300

    Add `Lucky` theme

---
+++ src/shared/themes/themes/Lucky.js
@@ -0,0 +1,14 @@
+export default { 
+chart: { color: 'rgb(250, 206, 73)', },
+box: { bg: 'rgb(255, 255, 255)',alt: 'rgb(231, 231, 231)',body: 'rgb(0, 0, 0)', },
+bar: { bg: 'rgb(177, 0, 0)',hover: 'rgb(140, 0, 0)',alt: 'rgb(154, 1, 0)',color: 'rgb(255, 255, 255)', },
+label: { color: 'rgb(0, 0, 0)',hover: 'rgb(0, 0, 0)', },
+input: { bg: 'rgb(255, 255, 255)',border: 'rgb(210, 210, 210)',alt: 'rgb(0, 0, 0)',hover: 'rgba(0, 0, 0, 0.5)',color: 'rgb(30, 30, 30)', },
+negative: { color: 'rgb(242, 159, 0)',hover: 'rgb(197, 85, 30)',body: 'rgb(255, 255, 255)', },
+positive: { color: 'rgb(42, 153, 51)',hover: 'rgb(31, 129, 39)',body: 'rgb(255, 255, 255)', },
+dark: { color: 'rgb(242, 242, 242)',hover: 'rgb(212, 212, 212)',body: 'rgb(0, 0, 0)', },
+secondary: { color: 'rgb(177, 0, 0)',hover: 'rgb(140, 0, 0)',body: 'rgb(242, 242, 242)', },
+primary: { color: 'rgb(250, 206, 73)',hover: 'rgb(233, 190, 58)',body: 'rgb(0, 0, 0)', },
+body: { bg: 'rgb(237, 237, 237)',color: 'rgb(0, 0, 0)',alt: 'rgba(237, 237, 237, 0.15)', },
+wave: { primary: 'rgba(246, 66, 66, 0.2)',secondary: 'rgb(246, 246, 246)', },
+};
