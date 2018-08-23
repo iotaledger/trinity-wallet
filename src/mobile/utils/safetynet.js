3f192a6e9e3a3649b8776f838dca778267cfc8dc
Rajiv Shah <rajivshah1@icloud.com>
2018-08-23 13:30:20 -0400

    Mobile: Add docs (#249)

--- src/mobile/utils/safetynet.js
+++ src/mobile/utils/safetynet.js
@@ -1,7 +1,13 @@
 import { isPlayServicesAvailable, generateNonce, sendAndVerifyAttestation } from 'react-native-google-safetynet';
 
+// NOTE: This is a fake API key, thus SafetyNet attestation may fail even if the device is not rooted
+// It is replaced by the correct API key during the deployment process
 const API_KEY = 'fakeAPIkey';
 
+/**
+ * Send an attestation request to SafetyNet
+ * @return {Promise<boolean>}
+ */
 export const doAttestationFromSafetyNet = () => {
     return isPlayServicesAvailable()
         .then((result) => {
