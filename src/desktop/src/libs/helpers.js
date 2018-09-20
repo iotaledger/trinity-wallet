5777f9de81b738f6706dba01c9c874b9e3dac06b
Rihards gravis <rihards.gravis@iota.org>
2018-09-20 19:22:39 +0300

    Fix trit/byte array conversion

--- src/desktop/src/libs/helpers.js
+++ src/desktop/src/libs/helpers.js
@@ -50,6 +50,8 @@ const trytesTrits = [
     [-1, 0, 0],
 ];
 
+const tritStrings = trytesTrits.map((trit) => trit.toString());
+
 /**
  * Convert trit to an ASCII character
  * @param {trit} trit - raw Trit input
@@ -67,8 +69,44 @@ const charToByte = (char) => {
     return '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(char.toUpperCase());
 };
 
+/**
+ * Convert single byte to trit array
+ * @param {number} byte - Input byte
+ * @returns {array} Output trit array
+ */
 const byteToTrit = (byte) => {
     return trytesTrits[byte % 27];
+};
+
+/**
+ * Convert byte array to trit array
+ * @param {array} bytes - Input byte array
+ * @returns {array} Output trit array
+ */
+const bytesToTrits = (bytes) => {
+    let trits = [];
+    for (let i = 0; i < bytes.length; i++) {
+        trits = trits.concat(byteToTrit(bytes[i]));
+    }
+    return trits;
+};
+
+/**
+ * Convert trit array to string
+ * @param {array} trits - Input trit array
+ * @returns {string} Output string
+ */
+const tritsToChars = (trits) => {
+    let seed = '';
+    for (let i = 0; i < trits.length; i += 3) {
+        const trit = trits.slice(i, i + 3).toString();
+        for (let x = 0; x < tritStrings.length; x++) {
+            if (tritStrings[x] === trit) {
+                seed += '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(x);
+            }
+        }
+    }
+    return seed;
 };
 
 module.exports = {
@@ -76,5 +114,7 @@ module.exports = {
     shorten,
     byteToChar,
     byteToTrit,
+    bytesToTrits,
+    tritsToChars,
     charToByte,
 };
