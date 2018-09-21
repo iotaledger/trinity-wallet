637c5a83fad33d89e12c27fffd7fb774f74c4998
Umair Sarfraz <aquadestructor@icloud.com>
2018-09-21 17:44:40 +0500

    Handle read permissions on runtime when seedvault is imported (Android) (#393)
    Fixes https://github.com/iotaledger/trinity-wallet/issues/376

--- src/mobile/src/ui/components/SeedVaultImportComponent.js
+++ src/mobile/src/ui/components/SeedVaultImportComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, PermissionsAndroid } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { DocumentPicker } from 'react-native-document-picker';
@@ -82,6 +82,22 @@ export class SeedVaultImportComponent extends Component {
     }
 
     /**
+     * Grant storage read permissions for android
+     *
+     * @method grantPermissions
+     * @returns {Promise}
+     */
+    grantPermissions() {
+        return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((granted) => {
+            if (granted === true || granted === PermissionsAndroid.RESULTS.GRANTED) {
+                return Promise.resolve(granted);
+            }
+
+            throw new Error('Read permissions not granted.');
+        });
+    }
+
+    /**
      * Opens document picker, reads chosen file and opens password validation modal
      * @method importSeedVault
      */
@@ -105,8 +121,9 @@ export class SeedVaultImportComponent extends Component {
                 if (path.startsWith('file://')) {
                     path = path.slice(7);
                 }
-                RNFetchBlob.fs
-                    .readFile(path, 'ascii')
+
+                (isAndroid ? this.grantPermissions() : Promise.resolve())
+                    .then(() => RNFetchBlob.fs.readFile(path, 'ascii'))
                     .then((data) => {
                         this.setState({ seedVault: data });
                         this.props.openPasswordValidationModal();
