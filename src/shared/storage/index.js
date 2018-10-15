eb55424912cd7a6bc035f515867efa603b0c8885
Umair Sarfraz <aquadestructor@icloud.com>
2018-10-15 17:04:31 +0500

    Update schema
    - Add Wallet schema
    - Add Account schema
    - Update Transaction schema
    - Update Address schema

--- src/shared/storage/index.js
+++ src/shared/storage/index.js
@@ -1,30 +1,95 @@
+import assign from 'lodash/assign';
+import isEmpty from 'lodash/isEmpty';
+import map from 'lodash/map';
+import values from 'lodash/values';
 import Realm from 'realm';
-import { TransactionSchema } from '../schema';
+import {
+    TransactionSchema,
+    AddressSchema,
+    AccountSchema,
+    AddressSpendStatusSchema,
+    WalletSchema
+} from '../schema';
 
 const SCHEMA_VERSION = 0;
 
-class Transaction {
-    static schema = TransactionSchema;
+class Account {
+    static schema = AccountSchema;
 
-    static getTransactions() {
-        return realm.objects('Transaction');
+    static getAccountData(name) {
+        const account = realm.objectForPrimaryKey('Account', name);
+
+        return assign({}, account, {
+            addresses: values(account.addresses),
+            transactions: values(account.transactions)
+        });
     }
 
-    static addTransaction() {
-        realm.write(() =>
-            realm.create('Transaction', {
-                hash: Math.random().toString(),
-                value: Math.random(),
-                tag: 'TAG',
-                message: 'MESSAGE',
-            }),
+    static getAccountsData() {
+        const accounts = realm.objects('Account');
+
+        return map(accounts, (account) => assign({}, account, {
+            addresses: values(account.addresses),
+            transactions: values(account.transactions)
+        })
         );
+    }
+
+    static create({ accountName, addresses, transactions, type }) {
+        realm.write(() => realm.create('Account', { name: accountName, addresses, transactions, type }));
+    }
+}
+
+class Transaction {
+    static schema = TransactionSchema;
+}
+
+class Address {
+    static schema = AddressSchema;
+}
+
+class AddressSpendStatus {
+    static schema = AddressSpendStatusSchema;
+}
+
+class Wallet {
+    static schema = WalletSchema;
+
+     static get() {
+        const data = realm.objects('Wallet');
+        const shouldCreate = isEmpty(data);
+
+        if (shouldCreate) {
+            realm.write(() => realm.create('Wallet', {}));
+        }
+
+        return shouldCreate ? realm.objects('Wallet')[0] : data[0];
+    }
+
+    static setOnboardingComplete() {
+         realm.write(() => {
+             Wallet.get().onboardingComplete = true;
+         });
     }
 }
 
 const realm = new Realm({
-    schema: [Transaction],
+    path: '~/Desktop/Trinity',
+    schema: [
+        Account,
+        Address,
+        Transaction,
+        AddressSpendStatus,
+        Wallet
+    ],
     schemaVersion: SCHEMA_VERSION,
 });
 
-export { realm, Transaction };
+export {
+    realm,
+    Account,
+    Transaction,
+    Address,
+    AddressSpendStatus,
+    Wallet
+};
