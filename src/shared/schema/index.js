b6ebe2dcb977e626b5817fd13135d81ab79dd73b
Umair Sarfraz <aquadestructor@icloud.com>
2018-10-31 10:43:32 +0500

    Remove ChartDataSchema, DataForTimeframeSchema, DataPointSchema

--- src/shared/schema/index.js
+++ src/shared/schema/index.js
@@ -256,35 +256,6 @@ export const WalletSettingsSchema = {
     },
 };
 
-export const ChartDataSchema = {
-    name: 'ChartData',
-    primaryKey: 'currency',
-    properties: {
-        currency: 'string',
-        data: 'DataForTimeframe[]',
-    },
-};
-
-export const DataForTimeframeSchema = {
-    name: 'DataForTimeframe',
-    primaryKey: 'timeframe',
-    properties: {
-        timeframe: 'string',
-        data: 'DataPoint[]',
-    },
-};
-
-export const DataPointSchema = {
-    name: 'DataPoint',
-    primaryKey: 'id',
-    properties: {
-        id: 'string',
-        x: 'int',
-        y: 'float',
-        time: 'int',
-    },
-};
-
 export const NodeSchema = {
     name: 'Node',
     primaryKey: 'url',
