f96fdf2f1eacfea08b29331a1930b9a248b5b75d
Rajiv Shah <rajivshah1@icloud.com>
2018-11-27 23:51:28 -0500

    Work on Haul config

--- src/mobile/haul.config.js
+++ src/mobile/haul.config.js
@@ -1,10 +1,39 @@
 import { createWebpackConfig } from 'haul';
+import path from 'path';
 
 export default {
     webpack: (env) => {
         const config = createWebpackConfig({
             entry: './index.js',
         })(env);
+
+        config.module.rules = [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: require.resolve('sjcl'),
+                use: [
+                    {
+                        loader: 'imports-loader?sjcl',
+                    },
+                ],
+            },
+        ];
+
+        config.resolve.modules = [
+            'node_modules',
+            path.resolve(__dirname, '.', 'src'),
+            path.resolve(__dirname, '..', 'shared'),
+        ];
+
+        config.module.noParse = [/sjcl\.js$/];
 
         return config;
     },
