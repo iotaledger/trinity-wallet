db2e5408f91c1a31c9d42a20a8ac202686971a13
Rajiv Shah <rajivshah1@icloud.com>
2018-12-26 00:22:41 -0500

    Mobile: Fix progress step translations (#843)

--- src/mobile/src/ui/views/wallet/Send.js
+++ src/mobile/src/ui/views/wallet/Send.js
@@ -388,10 +388,10 @@ export class Send extends Component {
     }
 
     getProgressBarText() {
-        const { t, activeStepIndex, activeSteps } = this.props;
+        const { activeStepIndex, activeSteps } = this.props;
         const totalSteps = size(activeSteps);
         if (activeStepIndex === totalSteps) {
-            return t('progressSteps:transferComplete');
+            return 'progressSteps:transferComplete';
         }
         return activeSteps[activeStepIndex] ? activeSteps[activeStepIndex] : '';
     }
@@ -833,6 +833,7 @@ export class Send extends Component {
                                         this.blurTextFields();
                                     }
                                 }}
+                                t={t}
                             />
                         </View>
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
