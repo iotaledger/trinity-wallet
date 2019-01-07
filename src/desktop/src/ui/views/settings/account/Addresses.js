4145dac5f8c3534b8cb4f0c8d94627518c3c9ef6
Rihards Gravis <rihards.gravis@iota.org>
2019-01-07 13:52:28 +0200

    - Fix Settings narrow layout - Fix account settings address list layout - Fix Node settings "Remove custom node" button style; Fixes #846

--- src/desktop/src/ui/views/settings/account/Addresses.js
+++ src/desktop/src/ui/views/settings/account/Addresses.js
@@ -7,6 +7,7 @@ import Scrollbar from 'ui/components/Scrollbar';
 import Clipboard from 'ui/components/Clipboard';
 
 import css from './addresses.scss';
+import settingsCSS from '../index.scss';
 
 /**
  * Account addresses component
@@ -24,39 +25,37 @@ class Addresses extends PureComponent {
         const isSpent = ({ spent: { local, remote } }) => local || remote;
 
         return (
-            <form>
-                <fieldset>
+            <div className={settingsCSS.scroll}>
+                <Scrollbar>
                     <ul className={css.addresses}>
-                        <Scrollbar>
-                            {Object.keys(account.addresses)
-                                .reverse()
-                                .map((item) => {
-                                    const address = item + account.addresses[item].checksum;
-                                    return (
-                                        <li key={address}>
-                                            <p className={isSpent(account.addresses[item]) ? css.spent : null}>
-                                                <Clipboard
-                                                    text={address}
-                                                    title={t('receive:addressCopied')}
-                                                    success={t('receive:addressCopiedExplanation')}
-                                                >
-                                                    {item.match(/.{1,3}/g).join(' ')}{' '}
-                                                    <mark>
-                                                        {account.addresses[item].checksum.match(/.{1,3}/g).join(' ')}
-                                                    </mark>
-                                                </Clipboard>
-                                            </p>
-                                            <strong>
-                                                {formatValue(account.addresses[item].balance)}
-                                                {formatUnit(account.addresses[item].balance)}
-                                            </strong>
-                                        </li>
-                                    );
-                                })}
-                        </Scrollbar>
+                        {Object.keys(account.addresses)
+                            .reverse()
+                            .map((item) => {
+                                const address = item + account.addresses[item].checksum;
+                                return (
+                                    <li key={address}>
+                                        <p className={isSpent(account.addresses[item]) ? css.spent : null}>
+                                            <Clipboard
+                                                text={address}
+                                                title={t('receive:addressCopied')}
+                                                success={t('receive:addressCopiedExplanation')}
+                                            >
+                                                {item.match(/.{1,3}/g).join(' ')}{' '}
+                                                <mark>
+                                                    {account.addresses[item].checksum.match(/.{1,3}/g).join(' ')}
+                                                </mark>
+                                            </Clipboard>
+                                        </p>
+                                        <strong>
+                                            {formatValue(account.addresses[item].balance)}
+                                            {formatUnit(account.addresses[item].balance)}
+                                        </strong>
+                                    </li>
+                                );
+                            })}
                     </ul>
-                </fieldset>
-            </form>
+                </Scrollbar>
+            </div>
         );
     }
 }
