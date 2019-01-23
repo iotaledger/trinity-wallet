e2ccf0a9dfb28bc35294dc7c440040c6d402eabc
Umair Sarfraz <aquadestructor@icloud.com>
2019-01-24 03:22:49 +0500

    Blacklist nodejs-assets and platform folders for react native packager (#986)

--- src/mobile/rn-cli.config.js
+++ src/mobile/rn-cli.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const metroBlacklist = require('metro-config/src/defaults/blacklist');
 
 const alternateRoots = [path.join(__dirname, '../shared')];
-const blacklist = metroBlacklist([/nodejs-project\/.*/]);
+const blacklist = metroBlacklist([/nodejs-assets\/.*/, /android\/.*/, /ios\/.*/]);
 
 module.exports = {
     watchFolders: alternateRoots,
