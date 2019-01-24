b1ee00e0e25d15f701748a957287d7b51e8475f2
Umair Sarfraz <aquadestructor@icloud.com>
2019-01-24 07:17:00 +0500

    Only blacklist (iOS & android) build directories for react-native packager (#988)
    * Blacklist nodejs-assets and platform folders for react native packager
    
    * Only blacklist (iOS & android) build directories for react-native packager
    
    Blacklisting complete iOS and android directories leads to a build failure. This commit fixes the problem by blacklisting only iOS and android build paths.
    
    * Revert changes to Podfile.lock

--- src/mobile/rn-cli.config.js
+++ src/mobile/rn-cli.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const metroBlacklist = require('metro-config/src/defaults/blacklist');
 
 const alternateRoots = [path.join(__dirname, '../shared')];
-const blacklist = metroBlacklist([/nodejs-assets\/.*/, /android\/.*/, /ios\/.*/]);
+const blacklist = metroBlacklist([
+    /nodejs-assets\/.*/,
+    /android\/build\/.*/,
+    /android\/app\/build\/.*/,
+    /ios\/build\/.*/,
+]);
 
 module.exports = {
     watchFolders: alternateRoots,
