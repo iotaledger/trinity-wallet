b9488e04e213f9fa1444e3b2b3131976fb9ba305
Rajiv Shah <rajivshah1@icloud.com>
2019-02-15 14:54:31 -0500

    Mobile/iOS: Rebuild Entangled with new bindings
    Ref: rajivshah3/entangled@5a11ee1

--- src/mobile/ios/EntangledIOS/EntangledKit.framework/Headers/Interface.h
+++ src/mobile/ios/EntangledIOS/EntangledKit.framework/Headers/Interface.h
@@ -3,21 +3,31 @@
 //  EntangledKit
 //
 //
-//  Copyright © 2018 IOTA Foundation. All rights reserved.
+//  Copyright © 2019 IOTA Foundation. All rights reserved.
 //
 
 #ifndef __MOBILE__IOS_INTERFACE_H_
 #define __MOBILE__IOS_INTERFACE_H_
 
+#import <Foundation/Foundation.h>
 #include <stddef.h>
 
-char* iota_checksum(const char* input, const size_t input_length,
-                    const size_t checksum_length);
-char* iota_digest(char const* const trytes);
-char* iota_pow(const char* trytes, int mwm);
-char* iota_sign_address_gen(const char* seed, const int index,
-                            const int security);
-char* iota_sign_signature_gen(const char* seed, const int index,
-                              const int security, const char* bundleHash);
+@interface EntangledIOSBindings : NSObject
+
++ (NSString*)iota_ios_digest:(NSString*)trytes;
++ (NSString*)iota_ios_pow_trytes:(NSString*)trytes mwm:(int)mwm;
++ (int8_t*)iota_ios_sign_address_gen_trits:(int8_t*)seed
+                                     index:(const int)index
+                                  security:(const int)security;
++ (int8_t*)iota_ios_sign_signature_gen_trits:(int8_t*)seed
+                                       index:(const int)index
+                                    security:(const int)security
+                                  bundleHash:(int8_t*)bundleHash;
++ (NSArray*)iota_ios_pow_bundle:(NSArray*)txsTrytes
+                          trunk:(NSString*)trunk
+                         branch:(NSString*)branch
+                            mwm:(int)mwm;
+
+@end
 
 #endif  // __MOBILE__IOS_INTERFACE_H_
