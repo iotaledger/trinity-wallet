10b85c17c6ab9cbd0c90d088cdcf457059cd4fc3
cvarley100 <cvarley100@gmail.com>
2019-02-22 03:54:30 +0000

    Mobile: Fix main application

--- src/mobile/android/app/src/main/java/com/iota/trinity/MainApplication.java
+++ src/mobile/android/app/src/main/java/com/iota/trinity/MainApplication.java
@@ -95,7 +95,7 @@ public class MainApplication extends NavigationApplication implements ShareAppli
                 new RNSharePackage(),
                 new RNViewShotPackage(),
                 new RealmReactPackage(),
-                new RNFetchBlobPackage(),
+                new RNFetchBlobPackage()
         );
     }
 
