5395db53425dacdc2cdbaffa5752bf155024b5a6
Umair Sarfraz <aquadestructor@icloud.com>
2019-04-04 01:03:41 +0500

    Update realm schema to include deeplinking props

--- src/shared/schemas/index.js
+++ src/shared/schemas/index.js
@@ -1,6 +1,7 @@
 /* global Electron */
 import v0Schema from './v0';
 import v1Schema, { migration as v1Migration } from './v1';
+import v2Schema, { migration as v2Migration } from './v2';
 import { __MOBILE__, __TEST__, __DEV__ } from '../config';
 
 const STORAGE_PATH =
@@ -31,6 +32,12 @@ export default [
         schemaVersion: 1,
         migration: v1Migration,
         path: STORAGE_PATH,
+    },
+    {
+        schema: v2Schema,
+        schemaVersion: 2,
+        path: STORAGE_PATH,
+        migration: v2Migration,
     },
 ];
 
