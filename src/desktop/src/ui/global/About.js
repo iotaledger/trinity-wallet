cf474e6584824e883d4a66ca9c8b3868725fc045
rihardsgravis <rihards.gravis@iota.org>
2019-05-03 12:00:28 +0300

    Version bump to 0.5.0-RC2

--- src/desktop/src/ui/global/About.js
+++ src/desktop/src/ui/global/About.js
@@ -59,6 +59,55 @@ class About extends React.PureComponent {
 
                     <article>
                         <Scrollbar>
+                            <h5>0.5.0-RC2</h5>
+                            <ul>
+                                <li>- Fix: Use local time for SeedVault export file</li>
+                                <li>
+                                    - Fix: Manually assign account index as a fallback strategy during Realm migration
+                                </li>
+                                <li>- Fix: Disable quorum on login</li>
+                                <li>- Update: Add polling service for automatically retrying failed transactions</li>
+                                <li>- Fix: Update dependencies</li>
+                                <li>- Fix: Reduce quorum size</li>
+                                <li>- Fix: Realm undefined at launch on some Linux versions</li>
+                                <li>- Fix: Bugs related to local PoW</li>
+                                <li>- Fix: Minor bugs in Realm and polling</li>
+                                <li>- Fix: Snapshot transition showing 0 balance</li>
+                                <li>- Fix: Colon in 2FA QR code scan</li>
+                                <li>- Update: Deep link support</li>
+                                <li>- Fix: Add missing domains to external link whitelist</li>
+                                <li>- Fix: Disable quorum on initial account load, manual sync, and migration</li>
+                                <li>- Fix: Increase timeout for getTransactionsToApprove requests</li>
+                                <li>- Fix: Improve performance by not reconstructing bundles unnecessarily</li>
+                                <li>- Update: Remove 2FA</li>
+                                <li>- Update: Show modal for Ledger errors related to udev</li>
+                                <li>- Fix: Linux app icon</li>
+                                <li>- Fix: Only trigger notifications for new transactions</li>
+                                <li>- Update: Validate SeedVault before attemtping to import</li>
+                                <li>- Update: Add seed export unavailable explanation</li>
+                                <li>- Update: Transaction history search</li>
+                                <li>- Fix: Various Realm migration bugs</li>
+                                <li>- Fix: Update account indexes in Realm if an account is deleted</li>
+                                <li>- Fix: Use less strict threshold for out-of-sync checks</li>
+                                <li>- Fix: Align Entangled trunk/branch assignment with IRI</li>
+                                <li>- Update: Add iota.org node cluster</li>
+                            </ul>
+                            <h5>0.5.0-RC1</h5>
+                            <ul>
+                                <li>- Update: Add data migration Retry and Change node functionallity</li>
+                                <li>- Fix: Account transition broken</li>
+                                <li>- Fix: Onboarding unique seed check raises error</li>
+                                <li>- Fix: Additional account onboarding freezes after first unsucsefull try</li>
+                                <li>- Fix: Unable to cancel history refresh without Ledger device</li>
+                                <li>- Fix: Receive closes automatically on Ledger account</li>
+                            </ul>
+                            <h5>0.5.0 alpha</h5>
+                            <ul>
+                                <li>- Update: Add node quorum</li>
+                                <li>- Update: Use Realm for account data persistance</li>
+                                <li>- Fix: Account names containing [] crashes wallet</li>
+                                <li>- Fix: Remove drag region from update bar on Windows</li>
+                            </ul>
                             <h5>0.4.6</h5>
                             <ul>
                                 <li>- Fix: Unexpected wallet behaviour with account names starting with a number</li>
