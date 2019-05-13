a2d4cfe4af47ec3e709506a90c0f97e94f694fbf
rihardsgravis <rihards.gravis@iota.org>
2019-05-13 14:11:55 +0300

    Update Bugsnag API key after building the js app

--- src/desktop/bugsnag.sh
+++ src/desktop/bugsnag.sh
@@ -2,8 +2,8 @@
 # fail if any commands fail
 set -e
 
-# Add Bugsnag API key to src/index.js
-cat src/index.js | sed -e "s/fakeAPIkey/$BUGSNAG_KEY/" | tee src/index.js
+# Add Bugsnag API key to dist/bundle.js
+cat dist/bundle.js | sed -e "s/fakeAPIkey/$BUGSNAG_KEY/" | tee dist/bundle.js
 
 # Add Bugsnag API key to bugsnag.js
 cat bugsnag.js | sed -e "s/fakeAPIkey/$BUGSNAG_KEY/" | tee bugsnag.js
