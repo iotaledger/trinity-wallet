be0c566cc94086b494b353c0cbe0e39135e25097
Umair Sarfraz <aquadestructor@icloud.com>
2019-05-14 02:32:12 +0500

    feat: update realm schema for quorum configuration

--- src/shared/schemas/v3/index.js
+++ src/shared/schemas/v3/index.js
@@ -1,30 +1,61 @@
 import merge from 'lodash/merge';
 import map from 'lodash/map';
 import v2Schema from '../v2';
+import { QUORUM_SIZE } from '../../config';
 
 const migration = (_, newRealm) => {
     const walletData = newRealm.objectForPrimaryKey('Wallet', 2);
 
     // Bump wallet version.
     walletData.version = 3;
+    walletData.settings.quorum = {};
 };
 
-export default map(v2Schema, (schema) => {
-    if (schema.name === 'Node') {
-        return merge({}, schema, {
-            properties: {
-                /**
-                 * (Optional) authentication key for node
-                 */
-                authKey: {
-                    type: 'string',
-                    default: '',
-                },
-            },
-        });
-    }
+/**
+ * Schema for quorum config
+ */
+export const QuorumConfigSchema = {
+    name: 'QuorumConfig',
+    properties: {
+        size: {
+            type: 'int',
+            default: QUORUM_SIZE,
+        },
+        enabled: {
+            type: 'bool',
+            default: true,
+        },
+    },
+};
 
-    return schema;
-});
+export default [
+    QuorumConfigSchema,
+    ...map(v2Schema, (schema) => {
+        if (schema.name === 'Node') {
+            return merge({}, schema, {
+                properties: {
+                    /**
+                     * (Optional) authentication key for node
+                     */
+                    authKey: {
+                        type: 'string',
+                        default: '',
+                    },
+                },
+            });
+        } else if (schema.name === 'WalletSettings') {
+            return merge({}, schema, {
+                properties: {
+                    /**
+                     * Quorum configuration
+                     */
+                    quorum: 'QuorumConfig',
+                },
+            });
+        }
+
+        return schema;
+    }),
+];
 
 export { migration };
