cca89f8c002f6355706705a2ed3765ce08709aad
Rihards Gravis <31288628+rihardsgravis@users.noreply.github.com>
2019-05-27 18:36:46 +0300

    Shared: Fix `getSelectedAccountMeta` selector (#1605)
    * Fix `getSelectedAccountMeta` to not raise fatal error  if `typeof account === "undefined"`
    
    * Add tests for `getSelectedAccountMeta`
    
    * Lint fixe

--- src/shared/selectors/accounts.js
+++ src/shared/selectors/accounts.js
@@ -99,7 +99,7 @@ export const selectLatestAddressFromAccountFactory = (withChecksum = true) =>
  **/
 export const getSelectedAccountMeta = createSelector(
     selectAccountInfo,
-    (account) => account.meta || { type: 'keychain' },
+    (account) => get(account, 'meta'),
 );
 
 /**
