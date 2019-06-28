8a09af44eea8ab6f244089cb8699c09cb3e928ed
Thoralf-M <thoralf.mue@gmail.com>
2019-06-29 00:24:21 +0200

    remove unused import

--- src/desktop/src/ui/components/input/Text.js
+++ src/desktop/src/ui/components/input/Text.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import MAX_MESSAGE_LENGTH from 'libs/iota/utils';
 import css from './input.scss';
 
 /**
