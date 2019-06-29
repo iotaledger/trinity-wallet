5210fac61c436ed0fb9e35879920d13343c9d20b
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2019-06-29 02:28:42 +0200

    Specify maxLength for message textboxes (#1884)
    * cantfindanythinginthisscam
    
    * formatting change
    
    * some changes + tag
    
    * exitscam message too long error
    
    * change importorder
    
    * edit text.js
    
    * remove unused import
    
    * change integer to number
    
    * Shared: Revert changes to other languages
    
    These strings will be removed by Crowdin once the PR is merged to develop
    
    * add maxLength
    
    * change prop
    
    * Desktop: Specify maxLength for message field on send

--- src/mobile/src/ui/views/wallet/Send.js
+++ src/mobile/src/ui/views/wallet/Send.js
@@ -12,6 +12,7 @@ import {
     VALID_ADDRESS_WITH_CHECKSUM_REGEX,
     VALID_SEED_REGEX,
     ADDRESS_LENGTH,
+    MAX_MESSAGE_LENGTH,
 } from 'shared-modules/libs/iota/utils';
 import { completeDeepLinkRequest } from 'shared-modules/actions/wallet';
 import { getCurrencySymbol, getIOTAUnitMultiplier } from 'shared-modules/libs/currency';
@@ -790,6 +791,7 @@ export class Send extends Component {
                             onRef={(c) => {
                                 this.messageField = c;
                             }}
+                            maxLength={MAX_MESSAGE_LENGTH}
                             keyboardType="default"
                             label={t('message')}
                             onValidTextChange={(text) => this.props.setSendMessageField(text)}
