56d5d6a97f3fe3985fb28690f71e97b4b5d6e463
Umair Sarfraz <aquadestructor@icloud.com>
2019-07-17 02:55:29 +0500

    fix: polyfill intl for android (#2038)
    Fixes https://github.com/iotaledger/trinity-wallet/issues/2037

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -1,4 +1,13 @@
 import { AppRegistry } from 'react-native';
 import Wallet from './src/ui/routes/entry';
+import { isAndroid } from './src/libs/device';
+
+if (isAndroid) {
+    // This Intl API is not available in android 
+    // It needs to be polyfilled
+    require('intl');
+
+    require('intl/locale-data/jsonp/en-US');
+}
 
 AppRegistry.registerComponent('iotaWallet', () => Wallet);
