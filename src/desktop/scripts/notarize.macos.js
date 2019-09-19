55378b93f4dfeedde7d8b4334c6da32258489941
cvarley100 <cvarley100@gmail.com>
2019-09-19 14:42:52 +0100

    Desktop: Remove references to RC

--- src/desktop/scripts/notarize.macos.js
+++ src/desktop/scripts/notarize.macos.js
@@ -19,7 +19,7 @@ exports.default = async () => {
 
     await notarize({
         appBundleId: 'org.iota.trinity',
-        appPath: path.resolve(__dirname, '../out/mac/Trinity-RC.app'),
+        appPath: path.resolve(__dirname, '../out/mac/Trinity.app'),
         appleId: APPLE_ID,
         appleIdPassword: `@keychain:${APPLE_ID_IDENTITY_NAME}`,
         ascProvider: 'UG77RJKZHH',
