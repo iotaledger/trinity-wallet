412cdcf48e4f8e8588f14ad630e9ecd1b16eeb3b
Umair Sarfraz <aquadestructor@icloud.com>
2019-10-15 09:21:06 +0500

    fix: do minor fixes for MoonPay SetupEmail component

--- src/mobile/src/ui/views/wallet/exchanges/MoonPay/SetupEmail.js
+++ src/mobile/src/ui/views/wallet/exchanges/MoonPay/SetupEmail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import { StyleSheet, View, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import navigator from 'libs/navigation';
 import { getThemeFromState } from 'shared-modules/selectors/global';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { authenticateViaEmail } from 'shared-modules/actions/exchanges/MoonPay';
+import { generateAlert } from 'shared-modules/actions/alerts';
+import { isValidEmail } from 'shared-modules/libs/utils';
 import WithUserActivity from 'ui/components/UserActivity';
 import CustomTextInput from 'ui/components/CustomTextInput';
 import InfoBox from 'ui/components/InfoBox';
@@ -14,8 +16,6 @@ import AnimatedComponent from 'ui/components/AnimatedComponent';
 import { width, height } from 'libs/dimensions';
 import { Styling } from 'ui/theme/general';
 import Header from 'ui/components/Header';
-
-console.ignoredYellowBox = ['Native TextInput']; // eslint-disable-line no-console
 
 const styles = StyleSheet.create({
     container: {
@@ -51,11 +51,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         backgroundColor: 'transparent',
     },
-    seedVaultImportContainer: {
-        flex: 0.5,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
 });
 
 /** MoonPay setup email component */
@@ -71,6 +66,8 @@ class SetupEmail extends React.Component {
         hasErrorAuthenticatingEmail: PropTypes.bool.isRequired,
         /** @ignore */
         authenticateViaEmail: PropTypes.func.isRequired,
+        /** @ignore */
+        generateAlert: PropTypes.func.isRequired,
     };
 
     /**
@@ -100,78 +97,95 @@ class SetupEmail extends React.Component {
         }
     }
 
+    /**
+     * Authenticates user via emai
+     *
+     * @method authenticateViaEmail
+     *
+     * @returns {function}
+     */
+    authenticateViaEmail() {
+        const { email } = this.state;
+        const { t } = this.props;
+
+        if (!email) {
+            return this.props.generateAlert('error', t('moonpay:emptyEmail'), t('moonpay:emptyEmailExplanation'));
+        } else if (!isValidEmail(email)) {
+            return this.props.generateAlert('error', t('moonpay:invalidEmail'), t('moonpay:invalidEmailExplanation'));
+        }
+
+        return this.props.authenticateViaEmail(email);
+    }
+
     render() {
         const { t, theme, isAuthenticatingEmail } = this.props;
 
         return (
-            <TouchableWithoutFeedback style={{ flex: 0.8 }} onPress={Keyboard.dismiss} accessible={false}>
-                <View style={[styles.container, { backgroundColor: theme.body.bg }]}>
-                    <View>
-                        <View style={styles.topContainer}>
-                            <AnimatedComponent
-                                animationInType={['slideInRight', 'fadeIn']}
-                                animationOutType={['slideOutLeft', 'fadeOut']}
-                                delay={400}
-                            >
-                                <Header textColor={theme.body.color} />
-                            </AnimatedComponent>
-                        </View>
-                        <View style={styles.midContainer}>
-                            <AnimatedComponent
-                                animationInType={['slideInRight', 'fadeIn']}
-                                animationOutType={['slideOutLeft', 'fadeOut']}
-                                delay={266}
-                            >
-                                <InfoBox>
-                                    <Text style={[styles.infoText, { color: theme.body.color }]}>
-                                        {t('moonpay:setupEmail')}
-                                    </Text>
-                                    <Text
-                                        style={[
-                                            styles.infoTextRegular,
-                                            { paddingTop: height / 60, color: theme.body.color },
-                                        ]}
-                                    >
-                                        {t('moonpay:setupEmailExplanation')}
-                                    </Text>
-                                </InfoBox>
-                            </AnimatedComponent>
-                            <View style={{ flex: 0.3 }} />
-                            <AnimatedComponent
-                                animationInType={['slideInRight', 'fadeIn']}
-                                animationOutType={['slideOutLeft', 'fadeOut']}
-                                delay={133}
-                            >
-                                <CustomTextInput
-                                    label={t('moonpay:yourEmail')}
-                                    onValidTextChange={(email) => this.setState({ email })}
-                                    theme={theme}
-                                    autoCorrect={false}
-                                    enablesReturnKeyAutomatically
-                                    autoCapitalize="none"
-                                    returnKeyType="done"
-                                    value={this.state.email}
-                                    testID="enterSeed-seedbox"
-                                />
-                            </AnimatedComponent>
-                            <View style={{ flex: 0.6 }} />
-                        </View>
-                        <View style={styles.bottomContainer}>
-                            <AnimatedComponent animationInType={['fadeIn']} animationOutType={['fadeOut']} delay={0}>
-                                <DualFooterButtons
-                                    onLeftButtonPress={() => SetupEmail.redirectToScreen('selectAccount')}
-                                    onRightButtonPress={() => this.props.authenticateViaEmail(this.state.email)}
-                                    isRightButtonLoading={isAuthenticatingEmail}
-                                    leftButtonText={t('global:goBack')}
-                                    rightButtonText={t('global:continue')}
-                                    leftButtonTestID="enterSeed-back"
-                                    rightButtonTestID="enterSeed-next"
-                                />
-                            </AnimatedComponent>
-                        </View>
+            <View style={[styles.container, { backgroundColor: theme.body.bg }]}>
+                <View>
+                    <View style={styles.topContainer}>
+                        <AnimatedComponent
+                            animationInType={['slideInRight', 'fadeIn']}
+                            animationOutType={['slideOutLeft', 'fadeOut']}
+                            delay={400}
+                        >
+                            <Header textColor={theme.body.color} />
+                        </AnimatedComponent>
+                    </View>
+                    <View style={styles.midContainer}>
+                        <AnimatedComponent
+                            animationInType={['slideInRight', 'fadeIn']}
+                            animationOutType={['slideOutLeft', 'fadeOut']}
+                            delay={266}
+                        >
+                            <InfoBox>
+                                <Text style={[styles.infoText, { color: theme.body.color }]}>
+                                    {t('moonpay:setupEmail')}
+                                </Text>
+                                <Text
+                                    style={[
+                                        styles.infoTextRegular,
+                                        { paddingTop: height / 60, color: theme.body.color },
+                                    ]}
+                                >
+                                    {t('moonpay:setupEmailExplanation')}
+                                </Text>
+                            </InfoBox>
+                        </AnimatedComponent>
+                        <View style={{ flex: 0.3 }} />
+                        <AnimatedComponent
+                            animationInType={['slideInRight', 'fadeIn']}
+                            animationOutType={['slideOutLeft', 'fadeOut']}
+                            delay={133}
+                        >
+                            <CustomTextInput
+                                label={t('moonpay:yourEmail')}
+                                onValidTextChange={(email) => this.setState({ email })}
+                                theme={theme}
+                                autoCorrect={false}
+                                enablesReturnKeyAutomatically
+                                autoCapitalize="none"
+                                returnKeyType="done"
+                                value={this.state.email}
+                            />
+                        </AnimatedComponent>
+                        <View style={{ flex: 0.6 }} />
+                    </View>
+                    <View style={styles.bottomContainer}>
+                        <AnimatedComponent animationInType={['fadeIn']} animationOutType={['fadeOut']} delay={0}>
+                            <DualFooterButtons
+                                onLeftButtonPress={() => SetupEmail.redirectToScreen('selectAccount')}
+                                onRightButtonPress={() => this.authenticateViaEmail()}
+                                isRightButtonLoading={isAuthenticatingEmail}
+                                leftButtonText={t('global:goBack')}
+                                rightButtonText={t('global:continue')}
+                                leftButtonTestID="moonpay-back"
+                                rightButtonTestID="moonpay-select-account"
+                            />
+                        </AnimatedComponent>
                     </View>
                 </View>
-            </TouchableWithoutFeedback>
+            </View>
         );
     }
 }
@@ -184,6 +198,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = {
     authenticateViaEmail,
+    generateAlert,
 };
 
 export default WithUserActivity()(
