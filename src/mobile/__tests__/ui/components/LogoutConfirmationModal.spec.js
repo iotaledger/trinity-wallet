b210e8ad29c5c25612959920604a0e62334c31e2
Umair Sarfraz <aquadestructor@icloud.com>
2019-10-21 21:58:28 +0500

    Mobile: Fix proptype warnings and errors (#2300)

--- src/mobile/__tests__/ui/components/LogoutConfirmationModal.spec.js
+++ src/mobile/__tests__/ui/components/LogoutConfirmationModal.spec.js
@@ -14,6 +14,7 @@ const getProps = (overrides) =>
             hideModal: noop,
             logout: noop,
             theme,
+            themeName: 'Default',
         },
         overrides,
     );
