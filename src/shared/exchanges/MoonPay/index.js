fe8f3f9fb269cb459dee9a272ee8d7ad0f5ec523
Umair Sarfraz <aquadestructor@icloud.com>
2019-10-23 14:26:46 +0500

    chore: use test mode

--- src/shared/exchanges/MoonPay/index.js
+++ src/shared/exchanges/MoonPay/index.js
@@ -1,6 +1,6 @@
 import { MoonPayApi } from './api';
 
-export const API_KEY = 'pk_live_1Ls6xJrTtp1UbdXjkD9MwBNg8sQm4aRk';
+export const API_KEY = 'pk_test_W1g4KpNvqWkHEo58O0CTluQz698eOc';
 
 export const IOTA_CURRENCY_CODE = 'miota';
 
