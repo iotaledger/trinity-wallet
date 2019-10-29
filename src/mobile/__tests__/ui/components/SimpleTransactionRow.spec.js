356a4ccca859f9199084b5503af5f935f58962a6
Umair Sarfraz <aquadestructor@icloud.com>
2019-10-29 13:05:11 +0500

    chore: fix failing test

--- src/mobile/__tests__/ui/components/SimpleTransactionRow.spec.js
+++ src/mobile/__tests__/ui/components/SimpleTransactionRow.spec.js
@@ -62,11 +62,11 @@ describe('Testing SimpleTransactionRow component', () => {
             expect(text.children().text()).toEqual('pending');
         });
 
-        it('should return "sign" prop as a child to third Text component', () => {
+        it('should return "sign" prop as a child to first Text component', () => {
             const props = getProps();
 
             const wrapper = shallow(<SimpleTransactionRow {...props} />);
-            const text = wrapper.find('Text').at(2);
+            const text = wrapper.find('Text').at(0);
 
             expect(
                 text
@@ -76,11 +76,11 @@ describe('Testing SimpleTransactionRow component', () => {
             ).toEqual('+');
         });
 
-        it('should return "value" prop as a child to third Text component', () => {
+        it('should return "value" prop as a child to first Text component', () => {
             const props = getProps();
 
             const wrapper = shallow(<SimpleTransactionRow {...props} />);
-            const text = wrapper.find('Text').at(2);
+            const text = wrapper.find('Text').at(0);
 
             expect(
                 text
@@ -90,11 +90,11 @@ describe('Testing SimpleTransactionRow component', () => {
             ).toEqual('100');
         });
 
-        it('should return "unit" prop as a child to third Text component', () => {
+        it('should return "unit" prop as a child to first Text component', () => {
             const props = getProps();
 
             const wrapper = shallow(<SimpleTransactionRow {...props} />);
-            const text = wrapper.find('Text').at(2);
+            const text = wrapper.find('Text').at(0);
 
             expect(
                 text
