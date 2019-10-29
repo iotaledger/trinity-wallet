25421f83e88b0457092bb67d971a4b8cc80467c8
cvarley100 <cvarley100@gmail.com>
2019-10-29 14:53:32 +0000

    Mobile: Update various dependencies

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -1,9 +1,10 @@
 import { AppRegistry } from 'react-native';
+import process from 'process'; // eslint-disable-line no-unused-vars
 import Wallet from './src/ui/routes/entry';
 import { isAndroid } from './src/libs/device';
 
 if (isAndroid) {
-    // This Intl API is not available in android 
+    // This Intl API is not available in android
     // It needs to be polyfilled
     require('intl');
 
