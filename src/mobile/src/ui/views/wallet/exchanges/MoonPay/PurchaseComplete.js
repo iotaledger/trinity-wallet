167b3df37baa0f30c09c3bbb8b0cd9df75882936
cvarley100 <cvarley100@gmail.com>
2019-10-31 17:47:45 +0000

    Mobile: Fix moonpay purchase redirect

--- src/mobile/src/ui/views/wallet/exchanges/MoonPay/PurchaseComplete.js
+++ src/mobile/src/ui/views/wallet/exchanges/MoonPay/PurchaseComplete.js
@@ -42,7 +42,7 @@ class PurchaseComplete extends Component {
      *
      * @method redirectToScreen
      */
-    redirectToScreen() {
+    redirectToHome() {
         navigator.setStackRoot('home');
     }
 
