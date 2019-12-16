2bacc787f65f76b399fbb7b0e6044c7b408dab99
Charlie Varley <charlie.varley@iota.org>
2019-12-16 21:07:56 +0000

    fix/desktop: filter jwt token when checking seed uniqueness

--- src/desktop/src/libs/SeedStore/Keychain.js
+++ src/desktop/src/libs/SeedStore/Keychain.js
@@ -1,6 +1,6 @@
 /* global Electron */
 import { sha256, encrypt, decrypt } from 'libs/crypto';
-import { ALIAS_REALM, ALIAS_MAIN, ALIAS_ACCOUNT } from 'libs/constants';
+import { ALIAS_REALM, ALIAS_MAIN, ALIAS_ACCOUNT, ALIAS_MOONPAY_CREDENTIALS } from 'libs/constants';
 import { tritsToChars, byteToTrit } from 'libs/iota/converter';
 import { prepareTransfersAsync } from 'libs/iota/extendedApi';
 
@@ -222,7 +222,7 @@ class Keychain extends SeedStoreCore {
         }
         try {
             const accounts = vault.filter(
-                (acc) => acc.account !== ALIAS_MAIN && acc.account !== `${ALIAS_MAIN}-salt` && acc.account !== ALIAS_REALM,
+                (acc) => acc.account !== ALIAS_MOONPAY_CREDENTIALS && acc.account !== ALIAS_MAIN && acc.account !== `${ALIAS_MAIN}-salt` && acc.account !== ALIAS_REALM,
             );
 
             for (let i = 0; i < accounts.length; i++) {
