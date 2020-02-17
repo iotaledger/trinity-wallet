c22880778de06e6710eb978d3e0f0bff4ddef115
Charlie Varley <charlie.varley@iota.org>
2020-02-17 00:02:27 +0000

    chore: fix eol alert

--- src/desktop/src/ui/global/Alerts.js
+++ src/desktop/src/ui/global/Alerts.js
@@ -103,7 +103,8 @@ export class AlertsComponent extends React.PureComponent {
 
         return (
             <div className={css.wrapper}>
-                {shouldUpdate &&
+                {!isUpdating &&
+                    !dismissUpdate && shouldUpdate &&
                     this.renderFullWidthAlert(
                         t('global:shouldUpdate'),
                         t('global:shouldUpdateExplanation'),
