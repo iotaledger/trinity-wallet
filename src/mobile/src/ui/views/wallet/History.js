10bdd91b0e0c2bae80141322d368078a0715deba
Charlie Varley <charlie.varley@iota.org>
2020-04-03 18:20:58 +0100

    fix/mobile: add keys to searchOptions map

--- src/mobile/src/ui/views/wallet/History.js
+++ src/mobile/src/ui/views/wallet/History.js
@@ -393,8 +393,8 @@ class History extends Component {
             };
             return (
                 <View style={[styles.searchHelpContainer, { backgroundColor: input.bg }]}>
-                    {map(keys(searchOptions), (item) => (
-                        <View style={styles.searchHelpRow}>
+                    {map(keys(searchOptions), (item, index) => (
+                        <View keys={index} style={styles.searchHelpRow}>
                             <Text style={[styles.searchHelpItemText, { color: primary.color }]}>{item}</Text>
                             <View style={styles.searchHelpInformationContainer}>
                                 <Text style={[styles.searchHelpInformationText, { color: body.color }]}>
