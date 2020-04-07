5145f0c2076a9604d96961280d84a22c065366f8
Rajiv Shah <rajivshah1@icloud.com>
2020-04-07 16:19:48 -0400

    feature(mobile/ios): Add event emitter for system clock change

---
+++ src/mobile/ios/NativeUtils/ClockEventEmitterIOS.m
@@ -0,0 +1,15 @@
+//
+//  ClockEventEmitterIOS.m
+//  iotaWallet
+//
+//  Created by Rajiv Shah on 4/7/20.
+//  Copyright © 2020 IOTA Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_MODULE(ClockEventEmitterIOS, RCTEventEmitter)
+
+@end
