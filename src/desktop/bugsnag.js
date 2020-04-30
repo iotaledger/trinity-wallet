192af214b7ffa5e8003817d14968971c26c9b89d
Rajiv Shah <rajivshah1@icloud.com>
2020-04-30 16:01:17 -0400

    chore(desktop): Upload sourcemaps to Bugsnag

--- src/desktop/bugsnag.js
+++ src/desktop/bugsnag.js
@@ -1,5 +1,6 @@
 /*eslint-disable no-console*/
 const reportBuild = require('bugsnag-build-reporter');
+const { upload } = require('bugsnag-sourcemaps');
 const { resolve } = require('path');
 const { version } = require('./package.json');
 
@@ -16,4 +17,16 @@ reportBuild(
     },
 )
     .then(() => console.log('Build successfully reported to Bugsnag'))
-    .catch((err) => console.log('Build could not be reported to Bugsnag: ', err.messsage));
+    .catch((err) => console.log('Build could not be reported to Bugsnag: ', err));
+
+upload({
+    apiKey: API_KEY,
+    appVersion: version,
+    publicPath: 'iota://dist/',
+    minifiedUrl: 'iota://dist/bundle.js',
+    sourceMap: resolve(__dirname, 'dist/bundle.js.map'),
+    minifiedFile: resolve(__dirname, 'dist/bundle.js'),
+    overwrite: true,
+})
+    .then(() => console.log('Sourcemaps successfully uploaded to Bugsnag'))
+    .catch((err) => console.log('Sourcemaps could not uploaded to Bugsnag: ', err));
