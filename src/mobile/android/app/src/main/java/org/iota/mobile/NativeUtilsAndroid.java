e23002bf0ee75bb6fd11148b84f6ac47cf3fbf8b
Umair Sarfraz <aquadestructor@icloud.com>
2020-05-04 15:42:20 +0500

    chore: fix android build

--- src/mobile/android/app/src/main/java/org/iota/mobile/NativeUtilsAndroid.java
+++ src/mobile/android/app/src/main/java/org/iota/mobile/NativeUtilsAndroid.java
@@ -3,6 +3,8 @@ package org.iota.mobile;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactMethod;
 
 import android.os.SystemClock;
 
@@ -15,12 +17,15 @@ public class NativeUtilsAndroid extends ReactContextBaseJavaModule {
   }
 
   @Override
-  String getName() {
+  public String getName() {
     return "NativeUtilsAndroid";
   }
 
   @ReactMethod
   public void getSystemUptime(Promise promise) {
-    promise.resolve(SystemClock.elapsedRealtime());
+    long time = SystemClock.elapsedRealtime();
+    String timeAsString = Long.toString(time);
+
+    promise.resolve(timeAsString);
   }
 }
