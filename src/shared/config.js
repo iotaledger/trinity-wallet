ac8b09d5e615ea498f70dee562e6a5801c0dee6b
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2020-06-03 19:17:39 +0200

    Change default depth to 1
    Hornet has 3 as max default depth so 4 could cause problems and 1 should even be faster and help that transactions have a longer period where they can get referenced

--- src/shared/config.js
+++ src/shared/config.js
@@ -53,7 +53,7 @@ export const MARKETDATA_ENDPOINTS = [
 export const VERSIONS_URL =
     'https://raw.githubusercontent.com/iotaledger/trinity-wallet/develop/src/shared/libs/versions.json';
 
-export const DEFAULT_DEPTH = 4;
+export const DEFAULT_DEPTH = 1;
 export const DEFAULT_MIN_WEIGHT_MAGNITUDE = 14;
 export const DEFAULT_TAG = 'TRINITY';
 export const DEFAULT_SECURITY = 2;
