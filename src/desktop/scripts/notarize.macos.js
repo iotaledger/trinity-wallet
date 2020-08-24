af98fe97efb8c8bd2893c6d66d9133ecc5c1ae8a
Sadjy SADJAN <sadjy@iota.org>
2020-08-25 08:49:49 +0900

    Moving from Buildkite to GHA (#2949)
    * Moving from Buildkite to GHA
    
    * adding package.json and notarize.macos.js changes
    
    * fix(ci): Avoid unnecessarily running tests on Crowdin PRs
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- src/desktop/scripts/notarize.macos.js
+++ src/desktop/scripts/notarize.macos.js
@@ -7,21 +7,21 @@ exports.default = async () => {
     }
 
     const APPLE_ID = process.env.TRINITY_APPLE_ID;
-    const APPLE_ID_IDENTITY_NAME = process.env.TRINITY_APPLE_ID_IDENTITY;
+    const APPLE_ID_PASSWORD = process.env.TRINITY_APPLE_ID_PASSWORD;
 
     if (!APPLE_ID) {
         throw Error('Notarization failed: Environment variable "TRINITY_APPLE_ID" is not defined');
     }
 
-    if (!APPLE_ID_IDENTITY_NAME) {
-        throw Error('Notarization failed: Environment variable "TRINITY_APPLE_ID_IDENTITY" is not defined');
+    if (!APPLE_ID_PASSWORD) {
+        throw Error('Notarization failed: Environment variable "TRINITY_APPLE_ID_PASSWORD" is not defined');
     }
 
     await notarize({
         appBundleId: 'org.iota.trinity',
         appPath: path.resolve(__dirname, '../out/mac/Trinity.app'),
         appleId: APPLE_ID,
-        appleIdPassword: `@keychain:${APPLE_ID_IDENTITY_NAME}`,
+        appleIdPassword: APPLE_ID_PASSWORD,
         ascProvider: 'UG77RJKZHH',
     });
 };
